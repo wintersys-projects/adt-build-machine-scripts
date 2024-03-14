4a961205bcb44881646bd1904c4acc6f8bcebb78
wintersys-projects <160187405+wintersys-projects@users.noreply.github.com>
2024-03-14 17:50:34 +0000

    Update InstallJQ.sh

--- installscripts/InstallJQ.sh
+++ installscripts/InstallJQ.sh
@@ -37,16 +37,3 @@ then
         DEBIAN_FRONTEND=noninteractive /usr/bin/apt-get -o DPkg::Lock::Timeout=-1 -qq -y install jq
     fi
 fi
-
-if ( [ "`/bin/grep "^PACKAGEMANAGER:*" ${BUILD_HOME}/builddescriptors/buildstylesscp.dat | /usr/bin/awk -F':' '{print $NF}'`" = "nala" ] )
-then
-    if ( [ "${buildos}" = "ubuntu" ] )
-    then
-        /usr/bin/yes | /usr/bin/nala install jq
-    fi
-
-    if ( [ "${buildos}" = "debian" ] )
-    then
-        /usr/bin/yes | /usr/bin/nala install jq
-    fi
-fi
