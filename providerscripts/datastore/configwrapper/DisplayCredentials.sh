711b41d46f42942bab77b935a30d51da97f373fb
wintersys-projects <160187405+wintersys-projects@users.noreply.github.com>
2024-03-16 18:16:09 +0000

    Update DisplayCredentials.sh

--- providerscripts/datastore/configwrapper/DisplayCredentials.sh
+++ providerscripts/datastore/configwrapper/DisplayCredentials.sh
@@ -30,7 +30,7 @@ then
     if ( [ "`/usr/bin/s3cmd ls s3://${config_bucket}`" != "" ] )
     then
         /usr/bin/s3cmd --force get s3://${config_bucket}/credentials/shit
-        if ( [ "${DATASTORE_CHOICE}" = "amazonS3" ] || [ "${DATASTORE_CHOICE}" = "digitalocean" ] || [ "${DATASTORE_CHOICE}" = "exoscale" ] || [ "${DATASTORE_CHOICE}" = "linode" ] || [ "${DATASTORE_CHOICE}" = "vultr" ] )
+        if ( [ "${DATASTORE_CHOICE}" = "digitalocean" ] || [ "${DATASTORE_CHOICE}" = "exoscale" ] || [ "${DATASTORE_CHOICE}" = "linode" ] || [ "${DATASTORE_CHOICE}" = "vultr" ] )
         then
             config_bucket="`/bin/echo ${WEBSITE_URL} | /usr/bin/awk -F'.' '{ for(i = 1; i <= NF; i++) { print $i; } }' | /usr/bin/cut -c1-3 | /usr/bin/tr '\n' '-' | /bin/sed 's/-//g'`-config"
     
