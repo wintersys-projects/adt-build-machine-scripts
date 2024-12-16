46d9f6e4d2c723ba078e801d4dcd23894b90e7f2
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-16 17:29:18 +0000

    Update GetVariableValue.sh

--- helperscripts/GetVariableValue.sh
+++ helperscripts/GetVariableValue.sh
@@ -8,4 +8,4 @@ then
         exit
 fi
 
-/bin/grep ${1} ${BUILD_ENVIRONMENT} | /bin/sed 's/"//g' | /usr/bin/awk -F'=' '{print $NF}'
+/bin/grep "^${1}=" ${BUILD_ENVIRONMENT} | /bin/sed 's/"//g' | /usr/bin/awk -F'=' '{print $NF}'
