fc9c1b72f4a11d437145719a5f21eafbfc2c1622
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-16 20:19:06 +0000

    Update SetApplicationConfig.sh

--- providerscripts/application/SetApplicationConfig.sh
+++ providerscripts/application/SetApplicationConfig.sh
@@ -26,8 +26,6 @@
 #####################################################################################
 #set -x
      
-#. ${BUILD_HOME}/providerscripts/application/${APPLICATION}/GetApplicationDefaultConfig.sh
-
 if ( [ -f ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/DBaaS_HOSTNAME ] )
 then
         DB_HOSTNAME="`/bin/cat ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/DBaaS_HOSTNAME`"
