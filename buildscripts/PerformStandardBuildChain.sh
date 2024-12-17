0a3d4c29815c51821278f69f9b03e4976fdf2cb7
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-17 15:12:53 +0000

    Update PerformStandardBuildChain.sh

--- buildscripts/PerformStandardBuildChain.sh
+++ buildscripts/PerformStandardBuildChain.sh
@@ -175,12 +175,10 @@ then
 fi
 
 #. ${BUILD_HOME}/providerscripts/security/firewall/TightenDBaaSFirewall.sh
-if ( [ "${AUTOSCALER_IMAGE_ID}" = "" ] && [ "${WEBSERVER_IMAGE_ID}" = "" ] && [ "${DATABASE_IMAGE_ID}" = "" ] )
-then
-        /bin/touch ${BUILD_HOME}/runtimedata//PRIME_FIREWALL
-        cloudhost_holder="${CLOUDHOST}"
-        . ${BUILD_HOME}/providerscripts/security/firewall/TightenBuildMachineFirewall.sh
-fi
+/bin/touch ${BUILD_HOME}/runtimedata//PRIME_FIREWALL
+cloudhost_holder="${CLOUDHOST}"
+. ${BUILD_HOME}/providerscripts/security/firewall/TightenBuildMachineFirewall.sh
+
 export CLOUDHOST="${cloudhost_holder}"
 
 if ( [ "${BUILD_ARCHIVE_CHOICE}" != "virgin" ] )
