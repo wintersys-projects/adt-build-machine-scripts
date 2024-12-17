c7db61c0b0e9d14bf79d5911feaf1102ba3a30b6
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-17 12:20:37 +0000

    Update ExpeditedAgileDeploymentToolkit.sh

--- ExpeditedAgileDeploymentToolkit.sh
+++ ExpeditedAgileDeploymentToolkit.sh
@@ -244,8 +244,10 @@ fi
 . ${BUILD_HOME}/providerscripts/security/firewall/TightenDBaaSFirewall.sh
 
 export PRE_BUILD="0"
-. ${BUILD_HOME}/providerscripts/security/firewall/SetupNativeFirewall.sh
-
+if ( [ "${AUTOSCALER_IMAGE_ID}" = "" ] && [ "${WEBSERVER_IMAGE_ID}" = "" ] && [ "${DATABASE_IMAGE_ID}" = "" ] )
+then
+         . ${BUILD_HOME}/providerscripts/security/firewall/SetupNativeFirewall.sh
+fi
 #If we have any messages to put out to the user post build, we add them to this script
 . ${BUILD_HOME}/processingscripts/PostProcessingMessages.sh
 
