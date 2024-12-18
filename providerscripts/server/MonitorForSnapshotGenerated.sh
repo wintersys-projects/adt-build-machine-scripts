0055b6a15731686a126cc3594258635fa12ed406
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-18 19:33:02 +0000

    Update MonitorForSnapshotGenerated.sh

--- providerscripts/server/MonitorForSnapshotGenerated.sh
+++ providerscripts/server/MonitorForSnapshotGenerated.sh
@@ -3,6 +3,20 @@
 rnd="`/bin/cat ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/credentials/SERVERUSER | /usr/bin/fold -w 4 | /usr/bin/head -n 1`"
 prefixes="as- ws- db-"
 
+
+if ( [ "${CLOUDHOST}" = "digitalocean" ] )
+then
+  creation_time="`/usr/local/bin/doctl compute snapshot list -o json | /usr/bin/jq -r '.[] | select ( .name | contains ("'${REGION}-${BUILD_IDENTIFIER}-${rnd}'")).created_at' | /usr/bin/head -1`"
+  creation_time_seconds="`/usr/bin/date -d "${creation_time}" +"%s"`"
+  current_time_seconds="`/usr/bin/date  +"%s"`"
+  
+  while ( [ "`/usr/bin/expr ${current_time_seconds} - ${creation_time_seconds}`" -lt "120" ] )
+  do
+          /bin/sleep 10
+          current_time_seconds="`/usr/bin/date  +"%s"`"
+  done
+fi
+
 if ( [ "${CLOUDHOST}" = "exoscale" ] )
 then
   while ( [ "`/bin/echo ${prefixes} | /bin/sed 's/ //g'`" != "" ] )
