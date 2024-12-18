96ec5a68f88d47ee09bfda7e3bf0c6c3ce993ef1
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-18 09:13:22 +0000

    Update MonitorForSnapshotGenerated.sh

--- providerscripts/server/MonitorForSnapshotGenerated.sh
+++ providerscripts/server/MonitorForSnapshotGenerated.sh
@@ -5,29 +5,32 @@ prefixes="as- ws- db-"
 
 if ( [ "${CLOUDHOST}" = "exoscale" ] )
 then
-  if ( [ "`/bin/echo ${prefixes} | /bin/sed 's/ //g'`" != "" ] )
-  then
-        for prefix in ${prefixes}
-        do
-                result="`/usr/bin/exo compute instance snapshot list -O json | /usr/bin/jq -r '.[] | select ( .instance | contains ("'${prefix}${REGION}-${BUILD_IDENTIFIER}-${rnd}'")).state'`" 
-                if ( [ "${result}" = "exported" ] )
-                then
-                        prefixes="`/bin/echo ${prefixes} | /bin/sed "s/${prefix}//g"`"
-                fi
-        done
-  fi
+  while ( [ "`/bin/echo ${prefixes} | /bin/sed 's/ //g'`" != "" ] )
+  do
+    for prefix in ${prefixes}
+    do
+      result="`/usr/bin/exo compute instance snapshot list -O json | /usr/bin/jq -r '.[] | select ( .instance | contains ("'${prefix}${REGION}-${BUILD_IDENTIFIER}-${rnd}'")).state'`" 
+      if ( [ "${result}" = "exported" ] )
+      then
+        prefixes="`/bin/echo ${prefixes} | /bin/sed "s/${prefix}//g"`"
+      fi
+    done
+  done
   status "All snapshots generated"
 fi
 
 if ( [ "${CLOUDHOST}" = "linode" ] )
 then
-  for prefix in ${prefixes}
+  while ( [ "`/bin/echo ${prefixes} | /bin/sed 's/ //g'`" != "" ] )
   do
-    result="`/usr/local/bin/linode-cli images list --json | /usr/bin/jq -r '.[] | select ( .label | contains ("'${prefix}${REGION}-${BUILD_IDENTIFIER}-${rnd}'")).status'`" 
-    if ( [ "${result}" = "available" ] )
-    then
-      prefixes="`/bin/echo ${prefixes} | /bin/sed "s/${prefix}//g"`"
-    fi
+    for prefix in ${prefixes}
+    do
+      result="`/usr/local/bin/linode-cli images list --json | /usr/bin/jq -r '.[] | select ( .label | contains ("'${prefix}${REGION}-${BUILD_IDENTIFIER}-${rnd}'")).status'`" 
+      if ( [ "${result}" = "available" ] )
+      then
+        prefixes="`/bin/echo ${prefixes} | /bin/sed "s/${prefix}//g"`"
+      fi
+    done
   done
   status "All snapshots generated"
 fi
