178a084f195395109ea3753a437b8f2f04aa783b
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-27 12:40:01 +0000

    Update SetApplicationConfig.sh

--- providerscripts/application/wordpress/SetApplicationConfig.sh
+++ providerscripts/application/wordpress/SetApplicationConfig.sh
@@ -29,19 +29,19 @@ then
 fi
 
 dbprefix="`/bin/cat ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/dbp.dat`"
-/bin/sed -i "/DB_HOST/c\ define('DB_HOST', \"${database_identifier}:${DB_PORT}\");" ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config-sample.php
-/bin/sed -i "/DB_USER/c\ define('DB_USER', \"${database_username}\");" ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config-sample.php
-/bin/sed -i "/DB_PASSWORD/c\ define('DB_PASSWORD', \"${database_password}\");" ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config-sample.php
-/bin/sed -i "/DB_NAME/c\ define('DB_NAME', \"${database_name}\");" ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config-sample.php
-/bin/sed -i "/\$table_prefix/c\ \$table_prefix=\"${dbprefix}\";" ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config-sample.php
+/bin/sed -i "/DB_HOST/c\ define('DB_HOST', \"${database_identifier}:${DB_PORT}\");" ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config.php.default
+/bin/sed -i "/DB_USER/c\ define('DB_USER', \"${database_username}\");" ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config.php.default
+/bin/sed -i "/DB_PASSWORD/c\ define('DB_PASSWORD', \"${database_password}\");" ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config.php.default
+/bin/sed -i "/DB_NAME/c\ define('DB_NAME', \"${database_name}\");" ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config.php.default
+/bin/sed -i "/\$table_prefix/c\ \$table_prefix=\"${dbprefix}\";" ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config.php.default
 
-if ( [ "`/bin/grep SALTEDALREADY ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config-sample.php`" = "" ] )
+if ( [ "`/bin/grep SALTEDALREADY ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config.php.default`" = "" ] )
 then
-    /bin/sed -i "/'AUTH_KEY'/i XXYYZZ" ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config-sample.php
-    /bin/sed -i '/AUTH_KEY/,+7d' ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config-sample.php
+    /bin/sed -i "/'AUTH_KEY'/i XXYYZZ" ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config.php.default
+    /bin/sed -i '/AUTH_KEY/,+7d' ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config.php.default
     salts="`/usr/bin/curl https://api.wordpress.org/secret-key/1.1/salt`"
-    /bin/sed -n '/XXYYZZ/q;p' ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config-sample.php > /tmp/firsthalf
-    /bin/sed '0,/^XXYYZZ$/d' ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config-sample.php > /tmp/secondhalf
+    /bin/sed -n '/XXYYZZ/q;p' ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config.php.default > /tmp/firsthalf
+    /bin/sed '0,/^XXYYZZ$/d' ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config.php.default > /tmp/secondhalf
     /bin/cat /tmp/firsthalf > /tmp/fullfile
     /bin/echo ${salts} >> /tmp/fullfile
     /bin/echo "/* SALTEDALREADY */" >> /tmp/fullfile
@@ -54,7 +54,7 @@ then
     /bin/echo "define('DISABLE_WP_CRON', true);" >> /tmp/fullfile
     /bin/cat /tmp/secondhalf >> /tmp/fullfile
     /bin/rm /tmp/firsthalf /tmp/secondhalf
-    /bin/mv /tmp/fullfile ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config-sample.php
+    /bin/mv /tmp/fullfile ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config.php.default
 fi
 
 ${BUILD_HOME}/providerscripts/datastore/configwrapper/PutToConfigDatastore.sh ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config-sample.php wordpress_config.php
