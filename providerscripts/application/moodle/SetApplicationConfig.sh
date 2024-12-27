0a22bc75ac61d44ec23631dee3456196951b9627
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-27 16:13:34 +0000

    Update SetApplicationConfig.sh

--- providerscripts/application/moodle/SetApplicationConfig.sh
+++ providerscripts/application/moodle/SetApplicationConfig.sh
@@ -53,3 +53,11 @@ dbprefix="`/bin/cat ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/d
         /bin/sed -i "0,/\$CFG->dataroot/ s/\$CFG->dataroot.*/\$CFG->dataroot = \'\/var\/www\/html\/moodledata\';/" ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/config.php.default 
 
 ${BUILD_HOME}/providerscripts/datastore/configwrapper/PutToConfigDatastore.sh ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/config.php.default moodle_config.php
+
+if ( [ "`${BUILD_HOME}/providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh moodle_config.php`" = "" ] )
+then
+ status "Didn't generate the moodle configuration file in the config datastore, this will cause trouble later"
+fi
+
+
+
