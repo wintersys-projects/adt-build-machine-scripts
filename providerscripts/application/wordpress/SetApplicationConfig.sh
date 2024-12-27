de4e349d50e42df274785e2396a5fec6bd7586f5
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-27 12:00:32 +0000

    Update SetApplicationConfig.sh

--- providerscripts/application/wordpress/SetApplicationConfig.sh
+++ providerscripts/application/wordpress/SetApplicationConfig.sh
@@ -35,24 +35,27 @@ dbprefix="`/bin/cat ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/d
 /bin/sed -i "/DB_NAME/c\ define('DB_NAME', \"${database_name}\");" ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config-sample.php
 /bin/sed -i "/\$table_prefix/c\ \$table_prefix=\"${dbprefix}\";" ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config-sample.php
 
-/bin/sed -i "/'AUTH_KEY'/i XXYYZZ" ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config-sample.php
-/bin/sed -i '/AUTH_KEY/,+7d' ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config-sample.php
-salts="`/usr/bin/curl https://api.wordpress.org/secret-key/1.1/salt`"
-/bin/sed -n '/XXYYZZ/q;p' ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config-sample.php > /tmp/firsthalf
-/bin/sed '0,/^XXYYZZ$/d' ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config-sample.php > /tmp/secondhalf
-/bin/cat /tmp/firsthalf > /tmp/fullfile
-/bin/echo ${salts} >> /tmp/fullfile
-/bin/echo "/* SALTEDALREADY */" >> /tmp/fullfile
-/bin/echo "define( 'DISALLOW_FILE_EDIT', true );" >> /tmp/fullfile
-/bin/echo "define( 'WP_DEBUG', false );" >> /tmp/fullfile
-/bin/echo "define('WP_CACHE', false);" >> /tmp/fullfile
-/bin/echo "define('CONCATENATE_SCRIPTS', true);" >> /tmp/fullfile
-/bin/echo "define('COMPRESS_SCRIPTS', true);" >> /tmp/fullfile
-/bin/echo "define('COMPRESS_CSS', true);" >> /tmp/fullfile
-/bin/echo "define('DISABLE_WP_CRON', true);" >> /tmp/fullfile
-/bin/cat /tmp/secondhalf >> /tmp/fullfile
-/bin/rm /tmp/firsthalf /tmp/secondhalf
-/bin/mv /tmp/fullfile ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config-sample.php
+if ( [ "`/bin/grep SALTEDALREADY ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config-sample.php`" = "" ] )
+then
+    /bin/sed -i "/'AUTH_KEY'/i XXYYZZ" ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config-sample.php
+    /bin/sed -i '/AUTH_KEY/,+7d' ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config-sample.php
+    salts="`/usr/bin/curl https://api.wordpress.org/secret-key/1.1/salt`"
+    /bin/sed -n '/XXYYZZ/q;p' ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config-sample.php > /tmp/firsthalf
+    /bin/sed '0,/^XXYYZZ$/d' ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config-sample.php > /tmp/secondhalf
+    /bin/cat /tmp/firsthalf > /tmp/fullfile
+    /bin/echo ${salts} >> /tmp/fullfile
+    /bin/echo "/* SALTEDALREADY */" >> /tmp/fullfile
+    /bin/echo "define( 'DISALLOW_FILE_EDIT', true );" >> /tmp/fullfile
+    /bin/echo "define( 'WP_DEBUG', false );" >> /tmp/fullfile
+    /bin/echo "define('WP_CACHE', false);" >> /tmp/fullfile
+    /bin/echo "define('CONCATENATE_SCRIPTS', true);" >> /tmp/fullfile
+    /bin/echo "define('COMPRESS_SCRIPTS', true);" >> /tmp/fullfile
+    /bin/echo "define('COMPRESS_CSS', true);" >> /tmp/fullfile
+    /bin/echo "define('DISABLE_WP_CRON', true);" >> /tmp/fullfile
+    /bin/cat /tmp/secondhalf >> /tmp/fullfile
+    /bin/rm /tmp/firsthalf /tmp/secondhalf
+    /bin/mv /tmp/fullfile ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config-sample.php
+fi
 
 ${BUILD_HOME}/providerscripts/datastore/configwrapper/PutToConfigDatastore.sh ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config-sample.php wordpress_config.php
 
