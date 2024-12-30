0b707ba6ce2ae27cc37985d12a8caa7e18515c5f
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-30 09:58:16 +0000

    Update InitialiseScalingProfile.sh

--- initscripts/InitialiseScalingProfile.sh
+++ initscripts/InitialiseScalingProfile.sh
@@ -1,8 +1,10 @@
 if ( [ "${PRODUCTION}" = "1" ] && [ "${DEVELOPMENT}" = "0" ] )
 then
-NUMBER_WS
-
-${BUILD_HOME}/providerscripts/datastore/configwrapper/PutToConfigDatastore.sh INSTALLEDSUCCESSFULLY INSTALLEDSUCCESSFULLY
-
+  if ( [ -f ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/STATIC_SCALE:* ] )
+  then
+    /bin/rm ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/STATIC_SCALE:*
+  fi
+  /bin/touch ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/STATIC_SCALE:${NUMBER_WS}
+  ${BUILD_HOME}/providerscripts/datastore/configwrapper/PutToConfigDatastore.sh ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/STATIC_SCALE:${NUMBER_WS}
 fi
 
