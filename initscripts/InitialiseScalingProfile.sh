1c796d21d064594e60222bf28dccfc2d69cb586f
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-30 09:54:03 +0000

    Update InitialiseScalingProfile.sh

--- initscripts/InitialiseScalingProfile.sh
+++ initscripts/InitialiseScalingProfile.sh
@@ -1,2 +1,7 @@
+if ( [ "${PRODUCTION}" = "1" ] && [ "${DEVELOPMENT}" = "0" ] )
+then
 
+${BUILD_HOME}/providerscripts/datastore/configwrapper/PutToConfigDatastore.sh INSTALLEDSUCCESSFULLY INSTALLEDSUCCESSFULLY
+
+fi
 
