ad91a18356963b1b0f728f775c95de5b8aed9d4d
Winty <160187405+wintersys-projects@users.noreply.github.com>
2024-12-30 10:08:11 +0000

    Update InitialiseScalingProfile.sh

--- initscripts/InitialiseScalingProfile.sh
+++ initscripts/InitialiseScalingProfile.sh
@@ -4,6 +4,7 @@ then
   then
     /bin/rm ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/STATIC_SCALE:*
   fi
+  status "Setting scaling value for number of webservers to ${NUMBER_WS}"
   /bin/touch ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/STATIC_SCALE:${NUMBER_WS}
   ${BUILD_HOME}/providerscripts/datastore/configwrapper/PutToConfigDatastore.sh ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/STATIC_SCALE:${NUMBER_WS}
 fi
