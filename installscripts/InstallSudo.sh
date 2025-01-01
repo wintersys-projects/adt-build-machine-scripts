0c25b08370af176177233e0af3c1c4ded995bd02
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-01 21:29:52 +0000

    Update InstallSudo.sh

--- installscripts/InstallSudo.sh
+++ installscripts/InstallSudo.sh
@@ -34,16 +34,18 @@ then
 	apt="/usr/sbin/apt-fast"
 fi
 
+install_command="DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=-1 --o=Dpkg::Use-Pty=0 -qq -y install " 
+
 if ( [ "${apt}" != "" ] )
 then
 	if ( [ "${buildos}" = "ubuntu" ] )
 	then
-		DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=-1 -qq -y install sudo
+		${install_command} sudo
 	fi
 
 	if ( [ "${buildos}" = "debian" ] )
 	then
-		DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=-1 -qq -y install sudo
+		${install_command} sudo
 	fi
 fi
 
