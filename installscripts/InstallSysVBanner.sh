301b9e8c84b9e40eb6d5ffface9f0670ace10af9
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-01 21:30:33 +0000

    Update InstallSysVBanner.sh

--- installscripts/InstallSysVBanner.sh
+++ installscripts/InstallSysVBanner.sh
@@ -34,16 +34,19 @@ then
 	apt="/usr/sbin/apt-fast"
 fi
 
+install_command="DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=-1 --o=Dpkg::Use-Pty=0 -qq -y install " 
+
+
 if ( [ "${apt}" != "" ] )
 then
 	if ( [ "${buildos}" = "ubuntu" ] )
 	then
-		DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=-1 -qq -y install sysvbanner
+		${install_command} sysvbanner
 	fi
 
 	if ( [ "${buildos}" = "debian" ] )
 	then
-		DEBIAN_FRONTEND=noninteractive ${apt} -o DPkg::Lock::Timeout=-1 -qq -y install sysvbanner
+		${install_command} sysvbanner
 	fi
 fi
 
