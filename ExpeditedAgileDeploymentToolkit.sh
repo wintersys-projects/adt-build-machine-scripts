3ed435f8d0e56c0e31719cac68d46140f6be5b09
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-21 21:43:31 +0000

    Update ExpeditedAgileDeploymentToolkit.sh

--- ExpeditedAgileDeploymentToolkit.sh
+++ ExpeditedAgileDeploymentToolkit.sh
@@ -158,7 +158,7 @@ fi
 ${BUILD_HOME}/initscripts/InitialiseCloudhostConfig.sh
 ${BUILD_HOME}/selectionscripts/SelectSMTPSettings.sh
 ${BUILD_HOME}/initscripts/InitialiseServerUserCredentials.sh
-. ${BUILD_HOME}/initscripts/InitialiseDatastoreConfig.sh
+${BUILD_HOME}/initscripts/InitialiseDatastoreConfig.sh
 . ${BUILD_HOME}/initscripts/PreFlightChecks.sh
 . ${BUILD_HOME}/providerscripts/datastore/PersistBuildClientIP.sh
 
