027ebdf4cf75db7c80d19476ed9a0087548d97dc
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-21 11:24:03 +0000

    Update InstallSudo.sh

--- installscripts/InstallSudo.sh
+++ installscripts/InstallSudo.sh
@@ -41,12 +41,12 @@ if ( [ "${apt}" != "" ] )
 then
 	if ( [ "${buildos}" = "ubuntu" ] )
 	then
-		${install_command} sudo
+		eval ${install_command} sudo
 	fi
 
 	if ( [ "${buildos}" = "debian" ] )
 	then
-		${install_command} sudo
+		eval ${install_command} sudo
 	fi
 fi
 
