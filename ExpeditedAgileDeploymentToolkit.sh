9391fa0674ce6a220d9b20816912498e2bce626b
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-22 13:20:19 +0000

    Update ExpeditedAgileDeploymentToolkit.sh

--- ExpeditedAgileDeploymentToolkit.sh
+++ ExpeditedAgileDeploymentToolkit.sh
@@ -220,7 +220,7 @@ start=`/bin/date +%s`
 #script
 ${BUILD_HOME}/processingscripts/PreProcessingMessages.sh
 ${BUILD_HOME}/initscripts/InitialiseScalingProfile.sh
-. ${BUILD_HOME}/initscripts/InitialiseKeystore.sh
+${BUILD_HOME}/initscripts/InitialiseKeystore.sh
 . ${BUILD_HOME}/buildscripts/BuildAndDeployDBaaS.sh
 . ${BUILD_HOME}/initscripts/InitialiseDatabaseCredentials.sh
 . ${BUILD_HOME}/initscripts/InitialiseNewSSLCertificate.sh
