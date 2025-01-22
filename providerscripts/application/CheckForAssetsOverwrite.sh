4c927314f8b1bcbb6d3a806ca230c08e13402c79
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-22 10:51:16 +0000

    Update CheckForAssetsOverwrite.sh

--- providerscripts/application/CheckForAssetsOverwrite.sh
+++ providerscripts/application/CheckForAssetsOverwrite.sh
@@ -29,6 +29,13 @@
 #####################################################################################
 #set -x
 
+${BUILD_CHOICE}" 
+${WEBSITE_URL}
+${DIRECTORIES_TO_MOUNT}
+${WEBSITE_URL}" 
+${BUILD_HOME}
+${DATASTORE_CHOICE} 
+
 #When we are a baseline, we want to persist all our assets to our datastore. This involves deleting any existing assets from the bucket
 #we are persisting to so we issue a warning here, that the existing assets will be purged
 
