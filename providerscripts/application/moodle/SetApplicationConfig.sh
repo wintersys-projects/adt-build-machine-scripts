a723046c8ddd4dc86ac1b51ae26ddbbec54fcdfa
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-23 11:50:41 +0000

    Update SetApplicationConfig.sh

--- providerscripts/application/moodle/SetApplicationConfig.sh
+++ providerscripts/application/moodle/SetApplicationConfig.sh
@@ -34,23 +34,23 @@ dbprefix="`/bin/cat ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/d
 
         if ( [ "${DATABASE_INSTALLATION_TYPE}" = "Postgres" ] || ( [ "${DATABASE_INSTALLATION_TYPE}" = "DBaaS" ] && [ "`/bin/echo ${DATABASE_DBaaS_INSTALLATION_TYPE} | /bin/grep 'Postgres'`" != "" ] ) )
         then
-                /bin/sed -i "/->dbtype /c\    \$CFG->dbtype    = \"pgsql\";" ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/config.php.default
+                /bin/sed -i '/->dbtype /c\    $CFG->dbtype    = "'pgsql'";' ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/config.php.default
         elif ( [ "${DATABASE_INSTALLATION_TYPE}" = "Maria" ] || ( [ "${DATABASE_INSTALLATION_TYPE}" = "DBaaS" ] && [ "`/bin/echo ${DATABASE_DBaaS_INSTALLATION_TYPE} | /bin/grep 'Maria'`" != "" ] ) )
         then
-                /bin/sed -i "/->dbtype /c\    \$CFG->dbtype    = \"mariadb\";" ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/config.php.default
+                /bin/sed -i '/->dbtype /c\    $CFG->dbtype    = "'mariadb'";' ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/config.php.default
         elif ( [ "${DATABASE_INSTALLATION_TYPE}" = "MySQL" ] || ( [ "${DATABASE_INSTALLATION_TYPE}" = "DBaaS" ] && [ "`/bin/echo ${DATABASE_DBaaS_INSTALLATION_TYPE} | /bin/grep 'MySQL'`" != "" ] ) )
         then
-                /bin/sed -i "/->dbtype /c\    \$CFG->dbtype    = \"mysqli\";" ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/config.php.default
+                /bin/sed -i '/->dbtype /c\    $CFG->dbtype    = "'mysqli'";' ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/config.php.default
         fi
 
-        /bin/sed -i "/->prefix /c\    \$CFG->prefix    = \"${dbprefix}\";" ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/config.php.default
-        /bin/sed -i "/->dbuser /c\    \$CFG->dbuser    = \"${database_username}\";" ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/config.php.default
-        /bin/sed -i "/->dbname /c\    \$CFG->dbname    = \"${database_name}\";" ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/config.php.default
-        /bin/sed -i "/->dbpass /c\    \$CFG->dbpass    = \"${database_password}\";" ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/config.php.default
-        /bin/sed -i "/->dbhost /c\    \$CFG->dbhost    = \"${database_identifier}\";" ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/config.php.default
-        /bin/sed -i "/dbport/c\     \'dbport\' => \'${db_port}\'," ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/config.php.default 
-        /bin/sed -i "0,/\$CFG->wwwroot/ s/\$CFG->wwwroot.*/\$CFG->wwwroot = \"https:\/\/${WEBSITE_URL}\/moodle\";/" ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/config.php.default
-        /bin/sed -i "0,/\$CFG->dataroot/ s/\$CFG->dataroot.*/\$CFG->dataroot = \'\/var\/www\/html\/moodledata\';/" ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/config.php.default 
+        /bin/sed -i '/->prefix /c\    $CFG->prefix    = "'${dbprefix}'";' ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/config.php.default
+        /bin/sed -i '/->dbuser /c\    $CFG->dbuser    = "'${database_username}'";' ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/config.php.default
+        /bin/sed -i '/->dbname /c\    $CFG->dbname    = "'${database_name}'";' ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/config.php.default
+        /bin/sed -i '/->dbpass /c\    $CFG->dbpass    = "'${database_password}'";' ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/config.php.default
+        /bin/sed -i '/->dbhost /c\    $CFG->dbhost    = "'${database_identifier}'";' ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/config.php.default
+        /bin/sed -i '/dbport/c\     "dbport" => "'${db_port}'",' ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/config.php.default 
+        /bin/sed -i '0,/\$CFG->wwwroot/ s/$CFG->wwwroot.*/$CFG->wwwroot = "'https:\/\/${WEBSITE_URL}\/moodle'";/' ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/config.php.default
+        /bin/sed -i '0,/\$CFG->dataroot/ s/$CFG->dataroot.*/$CFG->dataroot = "'\/var\/www\/html\/moodledata'";/' ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/config.php.default 
 
 ${BUILD_HOME}/providerscripts/datastore/configwrapper/PutToConfigDatastore.sh ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/config.php.default moodle_config.php
 
