f37ebc320835c6a87784bce973f61c486ab542cb
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-23 13:28:50 +0000

    Update SetVariableValue.sh

--- helperscripts/SetVariableValue.sh
+++ helperscripts/SetVariableValue.sh
@@ -1,13 +1,15 @@
-BUILD_HOME="`/bin/cat /home/buildhome.dat`" 
+#set -x
+
+BUILD_HOME="`/bin/cat /home/buildhome.dat`"
 CLOUDHOST="`/bin/cat ${BUILD_HOME}/runtimedata/ACTIVE_CLOUDHOST`"
 BUILD_IDENTIFIER="`/bin/cat ${BUILD_HOME}/runtimedata/ACTIVE_BUILD_IDENTIFIER`"
 BUILD_ENVIRONMENT="${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/build_environment"
 
 if ( [ "${1}" != "" ] )
 then
-  key-value="${1}"
+  keyvalue="${1}"
 fi
 
-key="`/bin/echo ${key-value} | /usr/bin/awk -F'=' '{print $1}'`"
+key="`/bin/echo ${keyvalue} | /usr/bin/awk -F'=' '{print $1}'`"
 
-/bin/sed -i "s/${key}=.*/${key-value}/" ${BUILD_ENVIRONMENT}
+/bin/sed -i "s/${key}=.*/${keyvalue}/" ${BUILD_ENVIRONMENT}
