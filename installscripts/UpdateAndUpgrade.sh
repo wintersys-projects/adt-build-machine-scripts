72d5a9252808aaf9d46f8fd2c2cc8bb1de898bcc
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-01-30 22:06:58 +0000

    Create UpdateAndUpgrade.sh

---
+++ installscripts/UpdateAndUpgrade.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+###################################################################################
+# Description: This  will perform a software update and upgrade
+# Date: 18/11/2016
+# Author : Peter Winter
+###################################################################################
+# License Agreement:
+# This file is part of The Agile Deployment Toolkit.
+# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+####################################################################################
+####################################################################################
+
+HOME="`/bin/cat /home/homedir.dat`"
+
+${HOME}/installscripts/Update.sh ${1}
+${HOME}/installscripts/Upgrade.sh ${1}
