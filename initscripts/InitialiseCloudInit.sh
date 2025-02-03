9238ac1a1b3b189b5bd8e98ae05820f79d5fa06d
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-02-03 10:02:39 +0000

    Update InitialiseCloudInit.sh

--- initscripts/InitialiseCloudInit.sh
+++ initscripts/InitialiseCloudInit.sh
@@ -1,1 +1,17 @@
+#!/bin/sh
 
+status () {
+        /bin/echo "$1" | /usr/bin/tee /dev/fd/3 2>/dev/null
+}
+
+BUILD_HOME="`/bin/cat /home/buildhome.dat`"
+CLOUDHOST="`${BUILD_HOME}/helperscripts/GetVariableValue.sh CLOUDHOST`"
+TOKEN="`${BUILD_HOME}/helperscripts/GetVariableValue.sh TOKEN`"
+CLOUDHOST_ACCOUNT_ID="`${BUILD_HOME}/helperscripts/GetVariableValue.sh CLOUDHOST_ACCOUNT_ID`"
+REGION="`${BUILD_HOME}/helperscripts/GetVariableValue.sh REGION`"
+ACCESS_KEY="`${BUILD_HOME}/helperscripts/GetVariableValue.sh ACCESS_KEY`"
+SECRET_KEY="`${BUILD_HOME}/helperscripts/GetVariableValue.sh SECRET_KEY`"
+
+if ( [ "${CLOUDHOST}" = "linode" ] )
+then
+  /bin/cp 
