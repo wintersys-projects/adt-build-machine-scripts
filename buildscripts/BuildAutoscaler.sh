d6406661ce6af8a19ce5de91d4846ad48f8d2c09
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-02-06 12:13:09 +0000

    Update BuildAutoscaler.sh

--- buildscripts/BuildAutoscaler.sh
+++ buildscripts/BuildAutoscaler.sh
@@ -36,7 +36,7 @@ counter="0"
 count="0"
 
 status () {
-        yellow="`/usr/bin/tput setaf 3`"
+        yellow="`/usr/bin/tput setaf 11`"
         norm="`/usr/bin/tput sgr0`"
         /bin/echo "${yellow} ${1} ${norm}" | /usr/bin/tee /dev/fd/3 2>/dev/null
 }
