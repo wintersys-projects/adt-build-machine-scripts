5815fb6c6a1fd5c76cf73856f7aff883d4e48dd2
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-02-12 13:42:56 +0000

    Update BuildAutoscaler.sh

--- buildscripts/BuildAutoscaler.sh
+++ buildscripts/BuildAutoscaler.sh
@@ -65,6 +65,8 @@ INFRASTRUCTURE_REPOSITORY_OWNER="`${BUILD_HOME}/helperscripts/GetVariableValue.s
 NO_AUTOSCALERS="`${BUILD_HOME}/helperscripts/GetVariableValue.sh NO_AUTOSCALERS`"
 AS_SIZE="`${BUILD_HOME}/helperscripts/GetVariableValue.sh AS_SIZE`"
 AS_SERVER_TYPE="`${BUILD_HOME}/helperscripts/GetVariableValue.sh AS_SERVER_TYPE`"
+SSH_PORT="`${BUILD_HOME}/helperscripts/GetVariableValue.sh SSH_PORT`"
+
 
 SERVER_USER="`/bin/cat ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/credentials/SERVERUSER`"
 SERVER_USER_PASSWORD="`/bin/cat ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/credentials/SERVERUSERPASSWORD`"
