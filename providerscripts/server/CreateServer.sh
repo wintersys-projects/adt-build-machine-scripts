290245003289906b000c4d4981d09383b35912f4
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-02-13 17:07:46 +0000

    Update CreateServer.sh

--- providerscripts/server/CreateServer.sh
+++ providerscripts/server/CreateServer.sh
@@ -50,7 +50,7 @@ then
         then
                 /bin/sed -i "s/XXXXAUTOSCALER_HOSTNAMEXXXX/${server_name}/g" ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/cloud-init/autoscaler.yaml
 
-                if ( [ "${CLOUDHOST}" = "linode" ] )
+                if ( [ "${CLOUDHOST}" = "linode" ] || [ "${CLOUDHOST}" = "vultr" ]  )
                 then
                         cloud_config="`/bin/cat ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/cloud-init/autoscaler.yaml | /usr/bin/base64 -w 0`"
                 else 
@@ -62,7 +62,7 @@ then
         if ( [ -f  ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/cloud-init/webserver.yaml ] )
         then
                 /bin/sed -i "s/XXXXWEBSERVER_HOSTNAMEXXXX/${server_name}/g" ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/cloud-init/webserver.yaml
-                if ( [ "${CLOUDHOST}" = "linode" ] )
+                if ( [ "${CLOUDHOST}" = "linode" ] || [ "${CLOUDHOST}" = "vultr" ] )
                 then
                         cloud_config="`/bin/cat ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/cloud-init/webserver.yaml | /usr/bin/base64 -w 0`"
                 else 
@@ -74,7 +74,7 @@ then
         if ( [ -f  ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/cloud-init/database.yaml ] )
         then
                 /bin/sed -i "s/XXXXDATABASE_HOSTNAMEXXXX/${server_name}/g" ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/cloud-init/database.yaml
-                if ( [ "${CLOUDHOST}" = "linode" ] )
+                if ( [ "${CLOUDHOST}" = "linode" ] || [ "${CLOUDHOST}" = "vultr" ]  )
                 then
                         cloud_config="`/bin/cat ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/cloud-init/database.yaml | /usr/bin/base64 -w 0`"
                 else 
