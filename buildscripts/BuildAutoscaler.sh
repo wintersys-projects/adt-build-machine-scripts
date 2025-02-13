891c231a7d453353231b985081cc18413e9bef1f
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-02-13 11:57:43 +0000

    Update BuildAutoscaler.sh

--- buildscripts/BuildAutoscaler.sh
+++ buildscripts/BuildAutoscaler.sh
@@ -209,19 +209,20 @@ do
                         /bin/rm ${AUTOSCALER_PUBLIC_KEYS_NUMBERED}
                 fi
 
-                /usr/bin/ssh-keyscan -T 60 ${as_active_ip} >> ${AUTOSCALER_PUBLIC_KEYS_NUMBERED}
+                /usr/bin/ssh-keyscan ${as_active_ip} >> ${AUTOSCALER_PUBLIC_KEYS_NUMBERED}
 
                 keytry="1"
-                while ( [ "`/usr/bin/diff -s ${AUTOSCALER_PUBLIC_KEYS_NUMBERED} /dev/null | /bin/grep identical`" != "" ] && [ "${keytry}" -lt "15" ] )
+               # while ( [ "`/usr/bin/diff -s ${AUTOSCALER_PUBLIC_KEYS_NUMBERED} /dev/null | /bin/grep identical`" != "" ] && [ "${keytry}" -lt "15" ] )
+                while ( ( [ "`/usr/bin/diff -s /dev/null ${AUTOSCALER_PUBLIC_KEYS_NUMBERED} | /bin/grep identical`" != "" ] || [ "`/bin/grep 'ed25519' ${AUTOSCALER_PUBLIC_KEYS_NUMBERED}`" = "" ) && [ "${keytry}" -lt "15" ] )
                 do
                         status "Couldn't scan for autoscaler ${autoscaler_name} ssh-keys attempt ${keytry} (this is normal and expected) .... trying again"
                         /bin/sleep 10
 
-                        /usr/bin/ssh-keyscan -T 60 ${as_active_ip} >> ${AUTOSCALER_PUBLIC_KEYS_NUMBERED}
+                        /usr/bin/ssh-keyscan ${as_active_ip} >> ${AUTOSCALER_PUBLIC_KEYS_NUMBERED}
 
                         if ( [ "`/usr/bin/diff -s /dev/null ${AUTOSCALER_PUBLIC_KEYS_NUMBERED} | /bin/grep identical`" != "" ] )
                         then
-                                /usr/bin/ssh-keyscan -T 60 -p ${SSH_PORT} ${as_active_ip} >> ${AUTOSCALER_PUBLIC_KEYS_NUMBERED}
+                                /usr/bin/ssh-keyscan -p ${SSH_PORT} ${as_active_ip} >> ${AUTOSCALER_PUBLIC_KEYS_NUMBERED}
                         fi
 
                         keytry="`/usr/bin/expr ${keytry} + 1`"
