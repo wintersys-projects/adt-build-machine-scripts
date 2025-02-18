af2757f2c4ce27e03bc23212168ac59bba318919
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-02-18 23:58:04 +0000

    Update AdjustScaling.sh

--- helperscripts/AdjustScaling.sh
+++ helperscripts/AdjustScaling.sh
@@ -94,17 +94,33 @@ number_autoscalers="`${BUILD_HOME}/providerscripts/server/NumberOfServers.sh "-a
 
 /bin/echo "You are running ${number_autoscalers} and you are asking me to build ${new_scale_value} webservers"
 
+base_number_of_webservers="`/usr/bin/expr ${number_of_webservers} / ${number_of_autoscalers}`"
+total_base_number_of_webservers="`/usr/bin/expr ${base_number_of_webservers} \* ${number_of_autoscalers}`"
+additional_number_of_webservers="`/usr/bin/expr ${number_of_webservers} - ${total_base_number_of_webservers}`"
 
+
+new_scale_values="SCALE_SCALE"
+for autoscaler_no in `printf "%d\n" $(seq 1 ${number_of_autoscalers})`
+do
+        if ( [ "${additional_number_of_webservers}" -gt "0" ] )
+        then
+                new_scale_values="${new_scale_values}:`/usr/bin/expr ${base_number_of_webservers} + 1`"
+                additional_number_of_webservers="`/usr/bin/expr ${additional_number_of_webservers} - 1`"
+        else
+                new_scale_values="${new_scale_values}:${base_number_of_webservers}"
+        fi
+done
 
 ${BUILD_HOME}/providerscripts/datastore/configwrapper/MultiDeleteConfigDatastore.sh STATIC_SCALE:
 if ( [ -f ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/STATIC_SCALE:* ] )
 then
         /bin/rm ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/STATIC_SCALE:*
 fi
-/bin/touch ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/STATIC_SCALE:${new_scale_value}
-${BUILD_HOME}/providerscripts/datastore/configwrapper/PutToConfigDatastore.sh ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/STATIC_SCALE:${new_scale_value} STATIC_SCALE:${new_scale_value}
 
-new_no_webservers="`${BUILD_HOME}/providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh STATIC_SCALE:* | /usr/bin/awk -F':' '{print $NF}'`"
+/bin/touch ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/${new_scale_values}
+${BUILD_HOME}/providerscripts/datastore/configwrapper/PutToConfigDatastore.sh ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/${new_scale_values} ${new_scale_values}
+
+new_no_webservers="`${BUILD_HOME}/providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh STATIC_SCALE:*`"
 
 /bin/echo ""
 /bin/echo "Your number of webservers has been successfully set to: ${new_no_webservers}"
