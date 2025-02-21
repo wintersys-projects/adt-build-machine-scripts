631d2867dfe570dbf5f12b40dad4217514507037
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-02-21 20:32:14 +0000

    Update CreateServer.sh

--- providerscripts/server/CreateServer.sh
+++ providerscripts/server/CreateServer.sh
@@ -172,14 +172,14 @@ then
                 /usr/bin/vultr instance create --label="${server_name}" --region="${REGION}" --plan="${server_size}" --os="${OS_CHOICE}" --ipv6=false -s ${KEY_ID} --ddos=false --userdata="`/bin/cat ${cloud_config}`" --vpc-enable --vpc-ids ${vpc_id}
         fi    
 
-        machine_id=""
-        count="0"
-        while ( [ "${machine_id}" = "" ] && [ "${count}" -lt "10" ] )
-        do
-                machine_id="`/usr/bin/vultr instance list -o json | /usr/bin/jq -r '.instances[] | select (.label == "'"${server_name}"'").id'`"
-                /bin/sleep 5
-                count="`/usr/bin/expr ${count} + 1`"
-        done
+      #  machine_id=""
+       # count="0"
+       # while ( [ "${machine_id}" = "" ] && [ "${count}" -lt "10" ] )
+       # do
+       #         machine_id="`/usr/bin/vultr instance list -o json | /usr/bin/jq -r '.instances[] | select (.label == "'"${server_name}"'").id'`"
+       #         /bin/sleep 5
+       #         count="`/usr/bin/expr ${count} + 1`"
+       # done
 
-        /usr/bin/vultr vpc2 nodes attach ${vpc_id} --nodes="${machine_id}"
+       # /usr/bin/vultr vpc2 nodes attach ${vpc_id} --nodes="${machine_id}"
 fi
