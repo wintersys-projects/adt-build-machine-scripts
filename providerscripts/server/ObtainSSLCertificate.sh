5cdf73859c6236c897725e77d4aa048f3d21c0c7
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-03-17 19:15:43 +0000

    Update ObtainSSLCertificate.sh

--- providerscripts/server/ObtainSSLCertificate.sh
+++ providerscripts/server/ObtainSSLCertificate.sh
@@ -37,7 +37,8 @@
 #set -x
 
 status () {
-        /bin/echo "$1" | /usr/bin/tee /dev/fd/3 2>/dev/null
+        /bin/echo "${1}" | /usr/bin/tee /dev/fd/3 2>/dev/null
+        /bin/echo "${0}: ${1}" >> /dev/fd/4
 }
 
 website_url="${1}"
