03deb0f2d6c94091848c1eec1478545fe85f0213
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-03-17 22:42:41 +0000

    Update TightenBuildMachineFirewall.sh

--- providerscripts/security/firewall/TightenBuildMachineFirewall.sh
+++ providerscripts/security/firewall/TightenBuildMachineFirewall.sh
@@ -27,6 +27,12 @@
 #######################################################################################################
 #set -x
 
+status () {
+        /bin/echo "${1}" | /usr/bin/tee /dev/fd/3 2>/dev/null
+        script_name="`/bin/echo ${0} | /usr/bin/awk -F'/' '{print $NF}'`"
+        /bin/echo "${script_name}: ${1}" >> /dev/fd/4
+}
+
 if ( [ -f /var/spool/cron/crontabs/root ] )
 then
         /bin/sed -i "/^#/d" /var/spool/cron/crontabs/root
