a31d5683a94ea04515452c7abdbfa63c2ef1a785
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-03-17 21:57:18 +0000

    Update BuildDatabase.sh

--- buildscripts/BuildDatabase.sh
+++ buildscripts/BuildDatabase.sh
@@ -30,7 +30,8 @@ status () {
         red="`/usr/bin/tput setaf 7`"
         norm="`/usr/bin/tput sgr0`"
         /bin/echo "${red} ${1} ${norm}" | /usr/bin/tee /dev/fd/3 2>/dev/null
-        /bin/echo "${0}: ${1}" >> /dev/fd/4
+        script_name="`/bin/echo ${0} | /usr/bin/awk -F'/' '{print $NF}'`"
+        /bin/echo "${script_name}: ${1}" >> /dev/fd/4
 }
 
 #If done=1 then we know that we have build a database correctly so we don't need to run again
