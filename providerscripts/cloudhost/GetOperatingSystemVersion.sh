bf059fd87a771ec647739058c707286f503e98b7
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-03-17 19:27:23 +0000

    Update GetOperatingSystemVersion.sh

--- providerscripts/cloudhost/GetOperatingSystemVersion.sh
+++ providerscripts/cloudhost/GetOperatingSystemVersion.sh
@@ -20,6 +20,11 @@
 ############################################################################################
 #set -x
 
+status () {
+        /bin/echo "${1}" | /usr/bin/tee /dev/fd/3 2>/dev/null
+        /bin/echo "${0}: ${1}" >> /dev/fd/4
+}
+
 cloudhost="${1}"
 buildos="${2}"
 buildos_version="${3}"
