41dda91685ba7addd23d05f74bc4823884411ec0
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-03-17 19:24:50 +0000

    Update InitialiseCompatibilityChecks.sh

--- initscripts/InitialiseCompatibilityChecks.sh
+++ initscripts/InitialiseCompatibilityChecks.sh
@@ -21,6 +21,11 @@
 ####################################################################################
 #set -x
 
+status () {
+        /bin/echo "${1}" | /usr/bin/tee /dev/fd/3 2>/dev/null
+        /bin/echo "${0}: ${1}" >> /dev/fd/4
+}
+
 #Check that we are 64 bit
 if ( [ "`/usr/bin/dpkg --print-architecture`" = "i386" ] )
 then
