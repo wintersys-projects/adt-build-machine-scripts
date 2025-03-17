a50b39b6007ab4058d60381541266f55bec7ad32
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-03-17 19:31:58 +0000

    Update GitProviderDomain.sh

--- providerscripts/git/GitProviderDomain.sh
+++ providerscripts/git/GitProviderDomain.sh
@@ -1,3 +1,9 @@
+
+status () {
+        /bin/echo "${1}" | /usr/bin/tee /dev/fd/3 2>/dev/null
+        /bin/echo "${0}: ${1}" >> /dev/fd/4
+}
+
 infrastructure_repository_provider="${1}"
 
 if ( [ "${infrastructure_repository_provider}" = "github" ] )
