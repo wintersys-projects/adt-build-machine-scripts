120f370875179d3312ba3eeb35df6e6f5f9c1aa9
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-03-17 15:08:50 +0000

    Update ExpeditedAgileDeploymentToolkit.sh

--- ExpeditedAgileDeploymentToolkit.sh
+++ ExpeditedAgileDeploymentToolkit.sh
@@ -282,6 +282,7 @@ then
         ${BUILD_HOME}/initscripts/InitialiseNewSSLCertificate.sh ${auth_website_url}
 fi
 ${BUILD_HOME}/initscripts/InitialiseNewSSLCertificate.sh
+exit
 ${BUILD_HOME}/initscripts/InitialiseCloudInit.sh
 
 if ( [ "`${BUILD_HOME}/helperscripts/IsHardcoreBuild.sh`" != "1" ] )
