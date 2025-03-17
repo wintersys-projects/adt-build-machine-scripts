500645b2e8ca2b90c65d9745f6d9d8e2f8bf25ee
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-03-17 21:29:33 +0000

    Update InstallCoreSoftware.sh

--- installscripts/InstallCoreSoftware.sh
+++ installscripts/InstallCoreSoftware.sh
@@ -24,7 +24,8 @@
 
 status () {
         /bin/echo "${1}" | /usr/bin/tee /dev/fd/3 2>/dev/null
-        /bin/echo "${0}: ${1}" >> /dev/fd/4
+        script_name="`/bin/echo ${0} | /usr/bin/awk -F'/' '{print $NF}'`"
+        /bin/echo "${script_name}: ${1}" >> /dev/fd/4
 }
 
 BUILD_HOME="`/bin/cat /home/buildhome.dat`"
