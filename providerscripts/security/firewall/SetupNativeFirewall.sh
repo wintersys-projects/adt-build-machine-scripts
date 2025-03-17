6ba1eff4b8a9d52bbe550e269849fa0b9ae586b4
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-03-17 19:12:34 +0000

    Update SetupNativeFirewall.sh

--- providerscripts/security/firewall/SetupNativeFirewall.sh
+++ providerscripts/security/firewall/SetupNativeFirewall.sh
@@ -21,7 +21,8 @@
 set -x
 
 status () {
-        /bin/echo "$1" | /usr/bin/tee /dev/fd/3 2>/dev/null
+        /bin/echo "${1}" | /usr/bin/tee /dev/fd/3 2>/dev/null
+        /bin/echo "${0}: ${1}" >> /dev/fd/4
 }
 
 pre_build="${1}"
