03b2a3b2c5b33ea0e14d6112a37a1bf9c9580828
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-03-18 19:08:58 +0000

    Update CreateZone.sh

--- providerscripts/dns/CreateZone.sh
+++ providerscripts/dns/CreateZone.sh
@@ -24,7 +24,7 @@
 status () {
         /bin/echo "${1}" | /usr/bin/tee /dev/fd/3 2>/dev/null
         script_name="`/bin/echo ${0} | /usr/bin/awk -F'/' '{print $NF}'`"
-        /bin/echo "${script_name}: ${1}" >> /dev/fd/4
+        /bin/echo "${script_name}: ${1}" >> /dev/fd/4  2>/dev/null
 }
 
 email="${1}"
