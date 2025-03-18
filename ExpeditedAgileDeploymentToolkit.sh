68c1b44b7db8de970c57845ef0ac3d364b96e9e5
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-03-18 19:16:48 +0000

    Update ExpeditedAgileDeploymentToolkit.sh

--- ExpeditedAgileDeploymentToolkit.sh
+++ ExpeditedAgileDeploymentToolkit.sh
@@ -46,7 +46,7 @@ exec 2>>/root/logs/${err_file}
 status () {
         /bin/echo "${1}" | /usr/bin/tee /dev/fd/3 2>/dev/null
         script_name="`/bin/echo ${0} | /usr/bin/awk -F'/' '{print $NF}'`"
-        /bin/echo "${script_name}: ${1}" >> /dev/fd/4
+        /bin/echo "${script_name}: ${1}" >> /dev/fd/4  2>/dev/null
 }
 
 status "The initial output log file is located at /root/logs/${out_file}"
