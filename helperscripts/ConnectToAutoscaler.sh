818d1cfa1c6a0b84d7f1aaf1e43ee26833e45444
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-03-20 13:26:58 +0000

    Update ConnectToAutoscaler.sh

--- helperscripts/ConnectToAutoscaler.sh
+++ helperscripts/ConnectToAutoscaler.sh
@@ -2,7 +2,7 @@
 ######################################################################################################################################################
 # Author: Peter Winter
 # Date  : 13/07/2016
-# Description : This script will connect you to your autoscaler via ssh
+# Description : This script will connect you to your autoscaler machine(s) through its configured port via ssh
 ######################################################################################################################################################
 # License Agreement:
 # This file is part of The Agile Deployment Toolkit.
