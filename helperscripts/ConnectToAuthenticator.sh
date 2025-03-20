042e573f24d31de1fec2874a705787b3e4e4118c
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-03-20 13:26:15 +0000

    Update ConnectToAuthenticator.sh

--- helperscripts/ConnectToAuthenticator.sh
+++ helperscripts/ConnectToAuthenticator.sh
@@ -2,7 +2,7 @@
 ######################################################################################################################################################
 # Author: Peter Winter
 # Date  : 13/07/2016
-# Description : This script will connect you to your webserver(s) via ssh
+# Description : This script will connect you to your authetnicator machine through its configured port via ssh
 ######################################################################################################################################################
 # License Agreement:
 # This file is part of The Agile Deployment Toolkit.
