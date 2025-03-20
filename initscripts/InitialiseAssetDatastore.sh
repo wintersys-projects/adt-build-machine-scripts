d8764a9b6e2b68e1a57abfab521c6335b68c4e92
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-03-20 13:58:58 +0000

    Update InitialiseAssetDatastore.sh

--- initscripts/InitialiseAssetDatastore.sh
+++ initscripts/InitialiseAssetDatastore.sh
@@ -1,4 +1,26 @@
-set -x
+#!/bin/sh
+#################################################################################
+# Author: Peter Winter
+# Date  : 13/07/2016
+# Description : This will create the DNS record for your current domain and is called
+# at the end of building a webserver
+##################################################################################
+# License Agreement:
+# This file is part of The Agile Deployment Toolkit.
+# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+####################################################################################
+####################################################################################
+#set -x
+
 status () {
         /bin/echo "${1}" | /usr/bin/tee /dev/fd/3 2>/dev/null
         script_name="`/bin/echo ${0} | /usr/bin/awk -F'/' '{print $NF}'`"
