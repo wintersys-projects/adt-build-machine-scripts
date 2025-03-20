4972fcee55b3e75b549cd7fe36d63650030d9424
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-03-20 09:22:58 +0000

    Update BuildAuthenticator.sh

--- buildscripts/BuildAuthenticator.sh
+++ buildscripts/BuildAuthenticator.sh
@@ -190,7 +190,7 @@ do
 
             ${BUILD_HOME}/providerscripts/server/DestroyServer.sh ${AUTHIP_PUBLIC} ${CLOUDHOST}
 
-            #Wait until we are sure that the image server(s) are destroyed because of a faulty build
+            #Wait until we are sure that the authentication server is destroyed because of a faulty build
             while ( [ "`${BUILD_HOME}/providerscripts/server/NumberOfServers.sh "auth-${REGION}-${BUILD_IDENTIFIER}" ${CLOUDHOST} 2>/dev/null`" != "0" ] )
             do
                 /bin/sleep 30
