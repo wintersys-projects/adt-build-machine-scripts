f99a1db0f498ed05250194b405ea865ab7097965
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-03-20 18:33:15 +0000

    Update CopyFromDatabase.sh

--- helperscripts/CopyFromDatabase.sh
+++ helperscripts/CopyFromDatabase.sh
@@ -24,8 +24,8 @@ DB_IP=""
 
 if ( [ ! -f  ./CopyFromDatabase.sh ] )
 then
-    /bin/echo "Sorry, this script has to be run from the helperscripts subdirectory"
-    exit
+	/bin/echo "Sorry, this script has to be run from the helperscripts subdirectory"
+	exit
 fi
 
 BUILD_HOME="`/bin/cat /home/buildhome.dat`"
@@ -34,8 +34,8 @@ sourcefile="${1}"
 
 if ( [ "${sourcefile}" = "" ] )
 then
-    /bin/echo "Please tell me the full path to the location of the file you wish to copy from the database server, for example, /tmp/databasedump.tar.gz"
-    read sourcefile
+	/bin/echo "Please tell me the full path to the location of the file you wish to copy from the database server, for example, /tmp/databasedump.tar.gz"
+	read sourcefile
 fi
 
 /bin/echo "Which Cloudhost are you using for this server?"
@@ -44,16 +44,16 @@ read response
 
 if ( [ "${response}" = "1" ] )
 then
-    CLOUDHOST="digitalocean"
+	CLOUDHOST="digitalocean"
 elif ( [ "${response}" = "2" ] )
 then
-    CLOUDHOST="exoscale"
+	CLOUDHOST="exoscale"
 elif ( [ "${response}" = "3" ] )
 then
-    CLOUDHOST="linode"
+	CLOUDHOST="linode"
 elif ( [ "${response}" = "4" ] )
 then
-    CLOUDHOST="vultr"
+	CLOUDHOST="vultr"
 fi
 
 /bin/echo "What is the build identifier you want to connect to?"
@@ -66,7 +66,7 @@ read BUILD_IDENTIFIER
 
 if ( [ "${CLOUDHOST}" = "vultr" ] )
 then
-    export VULTR_API_KEY="`/bin/cat ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/TOKEN`"
+	export VULTR_API_KEY="`/bin/cat ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/TOKEN`"
 fi
 
 token_to_match="db-`${BUILD_HOME}/helperscripts/GetVariableValue.sh REGION`-${BUILD_IDENTIFIER}"
@@ -74,36 +74,36 @@ token_to_match="db-`${BUILD_HOME}/helperscripts/GetVariableValue.sh REGION`-${BU
 
 if ( [ -f ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/VPC-ACTIVE ] )
 then
-    ips="`${BUILD_HOME}/providerscripts/server/GetServerPrivateIPAddresses.sh ${token_to_match} ${CLOUDHOST} ${BUILD_HOME}`"
+	ips="`${BUILD_HOME}/providerscripts/server/GetServerPrivateIPAddresses.sh ${token_to_match} ${CLOUDHOST} ${BUILD_HOME}`"
 else
-    ips="`${BUILD_HOME}/providerscripts/server/GetServerIPAddresses.sh ${token_to_match} ${CLOUDHOST} ${BUILD_HOME}`"
+	ips="`${BUILD_HOME}/providerscripts/server/GetServerIPAddresses.sh ${token_to_match} ${CLOUDHOST} ${BUILD_HOME}`"
 fi
 
 if ( [ "${ips}" = "" ] )
 then
-    /bin/echo "There doesn't seem to be any databases running"
-    exit
+	/bin/echo "There doesn't seem to be any databases running"
+	exit
 fi
 
 /bin/echo "Which database would you like to connect to?"
 count=1
 for ip in ${ips}
 do
-    /bin/echo "${count}:   ${ip}"
-    /bin/echo "Press Y/N to connect..."
-    read response
+	/bin/echo "${count}:   ${ip}"
+	/bin/echo "Press Y/N to connect..."
+	read response
 	
-    if ( [ "${response}" = "Y" ] || [ "${response}" = "y" ] )
-    then
-        DB_IP=${ip}
-        break
-    fi
-    count="`/usr/bin/expr ${count} + 1`"
+	if ( [ "${response}" = "Y" ] || [ "${response}" = "y" ] )
+	then
+		DB_IP=${ip}
+		break
+	fi
+	count="`/usr/bin/expr ${count} + 1`"
 done
 
 if ( [ "${response}" = "N" ] )
 then
-    exit
+	exit
 fi
 
 SERVER_USER="`/bin/cat ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/credentials/SERVERUSER`"
@@ -112,21 +112,21 @@ DATABASE_PUBLIC_KEYS="${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}
 
 if ( [ ! -f ${DATABASE_PUBLIC_KEYS} ] )
 then
-    /usr/bin/ssh-keyscan  -p ${SSH_PORT} ${DB_IP} > ${DATABASE_PUBLIC_KEYS}    
+	/usr/bin/ssh-keyscan  -p ${SSH_PORT} ${DB_IP} > ${DATABASE_PUBLIC_KEYS}    
 fi
 
 if ( [ "`/bin/cat ${DATABASE_PUBLIC_KEYS}`" = "" ] )
 then
-    /bin/echo "Couldn't initiate ssh key scan please try again (make sure the machine is online"
-    /bin/rm ${DATABASE_PUBLIC_KEYS}
-    exit
+	/bin/echo "Couldn't initiate ssh key scan please try again (make sure the machine is online"
+	/bin/rm ${DATABASE_PUBLIC_KEYS}
+	exit
 fi
 
 if ( [ ! -f ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/build_environment ] )
 then
-     ALGORITHM="rsa"
+	ALGORITHM="rsa"
 else
-    ALGORITHM="`${BUILD_HOME}/helperscripts/GetVariableValue.sh ALGORITHM`"
+	ALGORITHM="`${BUILD_HOME}/helperscripts/GetVariableValue.sh ALGORITHM`"
 fi
 
 /bin/echo "Please enter the full path to the directory you would like to copy the file to on this machine for example ${BUILD_HOME}/migrationdirectory"
@@ -139,15 +139,15 @@ runtime="`/usr/bin/expr ${end} - ${start}`"
 
 if ( [ "${runtime}" -lt "3" ] )
 then
-    /bin/echo "#####################################################################################################################################################################"
-    /bin/echo "Do you want to initiate a fresh ssh key scan (might be necessary if you can't connect) or  do you want to use previously generated keys"
-    /bin/echo "You should always use previously generated keys unless you can't connect (an previously used ip address might have been reallocated as part of scaling or redeployment"
-    /bin/echo "#####################################################################################################################################################################"
-    /bin/echo "Enter 'Y' to regenerate your SSH public keys anything else to keep the keys you have got. You should only need to regenerate the keys very occassionally if at all"   
-    read response1
+	/bin/echo "#####################################################################################################################################################################"
+	/bin/echo "Do you want to initiate a fresh ssh key scan (might be necessary if you can't connect) or  do you want to use previously generated keys"
+	/bin/echo "You should always use previously generated keys unless you can't connect (an previously used ip address might have been reallocated as part of scaling or redeployment"
+	/bin/echo "#####################################################################################################################################################################"
+	/bin/echo "Enter 'Y' to regenerate your SSH public keys anything else to keep the keys you have got. You should only need to regenerate the keys very occassionally if at all"   
+	read response1
 	
-    if ( [ "${response1}" = "Y" ] || [ "${response1}" = "y" ] )
-    then
-        /usr/bin/ssh-keyscan  -p ${SSH_PORT} ${DB_IP} > ${DATABASE_PUBLIC_KEYS}
-    fi
+	if ( [ "${response1}" = "Y" ] || [ "${response1}" = "y" ] )
+	then
+		/usr/bin/ssh-keyscan  -p ${SSH_PORT} ${DB_IP} > ${DATABASE_PUBLIC_KEYS}
+	fi
  fi
