045e5904ea6f81c5b78b4c69aa89527342038416
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-03-20 09:11:46 +0000

    Update BuildAuthenticator.sh

--- buildscripts/BuildAuthenticator.sh
+++ buildscripts/BuildAuthenticator.sh
@@ -26,18 +26,17 @@ counter="0"
 count="0"
 
 status () {
-        yellow="`/usr/bin/tput setaf 11`"
-        norm="`/usr/bin/tput sgr0`"
-        /bin/echo "${yellow} ${1} ${norm}" | /usr/bin/tee /dev/fd/3 2>/dev/null
-        script_name="`/bin/echo ${0} | /usr/bin/awk -F'/' '{print $NF}'`"
-        /bin/echo "${script_name}: ${1}" >> /dev/fd/4 2>/dev/null
+    yellow="`/usr/bin/tput setaf 11`"
+    norm="`/usr/bin/tput sgr0`"
+    /bin/echo "${yellow} ${1} ${norm}" | /usr/bin/tee /dev/fd/3 2>/dev/null
+    script_name="`/bin/echo ${0} | /usr/bin/awk -F'/' '{print $NF}'`"
+    /bin/echo "${script_name}: ${1}" >> /dev/fd/4 2>/dev/null
 }
 
 status ""
 status ""
 status ""
 status "#########################AUTHENTICATION SERVER BUILD MESSAGES ARE IN YELLOW#######################"
-
 
 BUILD_HOME="`/bin/cat /home/buildhome.dat`"
 CLOUDHOST="`${BUILD_HOME}/helperscripts/GetVariableValue.sh CLOUDHOST`"
@@ -63,157 +62,157 @@ BUILD_KEY="${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/keys/id_${
 #Try up to 5 times if the authenticator is failing to complete its build
 while ( [ "${done}" != "1" ] && [ "${counter}" -lt "5" ] )
 do
-        counter="`/usr/bin/expr ${counter} + 1`"
-        status "OK... Building an authentication server. This is the ${counter} attempt of 5"
+    counter="`/usr/bin/expr ${counter} + 1`"
+    status "OK... Building an authentication server. This is the ${counter} attempt of 5"
  
-        #Check if there is an authenticator already running. If there is, then skip building the authenticator
-        if ( [ "`${BUILD_HOME}/providerscripts/server/NumberOfServers.sh "auth-${REGION}-${BUILD_IDENTIFIER}" ${CLOUDHOST} 2>/dev/null`" -eq "0" ] )
-        then
-                ip=""
-                #Construct a unique name for this authentication server
-                RND="`/bin/echo ${SERVER_USER} | /usr/bin/fold -w 4 | /usr/bin/head -n 1`"
+    #Check if there is an authenticator already running. If there is, then skip building the authenticator
+    if ( [ "`${BUILD_HOME}/providerscripts/server/NumberOfServers.sh "auth-${REGION}-${BUILD_IDENTIFIER}" ${CLOUDHOST} 2>/dev/null`" -eq "0" ] )
+    then
+        ip=""
+        #Construct a unique name for this authentication server
+        RND="`/bin/echo ${SERVER_USER} | /usr/bin/fold -w 4 | /usr/bin/head -n 1`"
 
-                authenticator_name="auth-${REGION}-${BUILD_IDENTIFIER}-0-${RND}"
+        authenticator_name="auth-${REGION}-${BUILD_IDENTIFIER}-0-${RND}"
 
-                status "Initialising a new server machine, please wait......"
+        status "Initialising a new server machine, please wait......"
 
-                server_started="0"
-                while ( [ "${server_started}" = "0" ] )
-                do
-                        count="0"
-                        #Actually start the server machine. Following this, there will be an active machine instance running on your cloud provider
-                        ${BUILD_HOME}/providerscripts/server/CreateServer.sh "${AUTH_SERVER_TYPE}" "${authenticator_name}" 
+        server_started="0"
+        while ( [ "${server_started}" = "0" ] )
+        do
+            count="0"
+            
+            #Actually start the server machine. Following this, there will be an active machine instance running on your cloud provider
+            ${BUILD_HOME}/providerscripts/server/CreateServer.sh "${AUTH_SERVER_TYPE}" "${authenticator_name}" 
 
-                        #Keep trying if the first time wasn't successful
-                        while ( [ "$?" != "0" ] && [ "${count}" -lt "10" ] )
-                        do
-                                count="`/usr/bin/expr ${count} + 1`"
-                                /bin/sleep 10
-                                ${BUILD_HOME}/providerscripts/server/CreateServer.sh "${AUTH_SERVER_TYPE}" "${authenticator_name}" 
-                        done
+            #Keep trying if the first time wasn't successful
+            while ( [ "$?" != "0" ] && [ "${count}" -lt "10" ] )
+            do
+                count="`/usr/bin/expr ${count} + 1`"
+                /bin/sleep 10
+                ${BUILD_HOME}/providerscripts/server/CreateServer.sh "${AUTH_SERVER_TYPE}" "${authenticator_name}" 
+            done
 
-                        if ( [ "${count}" = "10" ] )
-                        then
-                                status "Could not create authenticator machine"
-                                /usr/bin/kill -9 $PPID                        
-                        fi
+            if ( [ "${count}" = "10" ] )
+            then
+                status "Could not create authenticator machine"
+                /usr/bin/kill -9 $PPID                        
+            fi
 
-                        #Check that the server has been assigned its IP addresses and that they are active
-                        ip=""
-                        private_ip=""
-                        count="0"
+            #Check that the server has been assigned its IP addresses and that they are active
+            ip=""
+            private_ip=""
+            count="0"
    
-                        #Keep trying until we get the ip addresses of our new machine, both public and private ips
-                        while ( ( [ "${ip}" = "" ] || [ "${private_ip}" = "" ] ) || [ "${ip}" = "0.0.0.0" ] && [ "${count}" -lt "20" ] )
-                        do
-                                status "Interrogating for authenticator ip address....."
-                                ip="`${BUILD_HOME}/providerscripts/server/GetServerIPAddresses.sh "${authenticator_name}" ${CLOUDHOST} | /bin/grep -P "^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$"`"
-                                private_ip="`${BUILD_HOME}/providerscripts/server/GetServerPrivateIPAddresses.sh "${authenticator_name}" ${CLOUDHOST} | /bin/grep -P "^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$"`"
-                                /bin/sleep 10
-                                count="`/usr/bin/expr ${count} + 1`"
-                        done
+            #Keep trying until we get the ip addresses of our new machine, both public and private ips
+            while ( ( [ "${ip}" = "" ] || [ "${private_ip}" = "" ] ) || [ "${ip}" = "0.0.0.0" ] && [ "${count}" -lt "20" ] )
+            do
+                status "Interrogating for authenticator ip address....."
+                ip="`${BUILD_HOME}/providerscripts/server/GetServerIPAddresses.sh "${authenticator_name}" ${CLOUDHOST} | /bin/grep -P "^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$"`"
+                private_ip="`${BUILD_HOME}/providerscripts/server/GetServerPrivateIPAddresses.sh "${authenticator_name}" ${CLOUDHOST} | /bin/grep -P "^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$"`"
+                /bin/sleep 10
+                count="`/usr/bin/expr ${count} + 1`"
+            done
 
-                        if ( [ "${ip}" != "" ] && [ "${private_ip}" != "" ] )
-                        then
-                                server_started="1"
-                        elif ( [ "${ip}" != "" ] && [ "${private_ip}" = "" ] )
-                        then
-                                status "Found a public ip address but not a private ip address"
-                                status "This likely means that there is some sort problem with the VPC"
-                        else
-                                status "I haven't been able to start your server, trying again...."
-                        fi
-           done
+            if ( [ "${ip}" != "" ] && [ "${private_ip}" != "" ] )
+            then
+                 server_started="1"
+            elif ( [ "${ip}" != "" ] && [ "${private_ip}" = "" ] )
+            then
+                status "Found a public ip address but not a private ip address"
+                status "This likely means that there is some sort problem with the VPC"
+            else
+                status "I haven't been able to start your server, trying again...."
+            fi
+        done
     
-                AUTHIP_PUBLIC=${ip}
-                AUTHIP_PRIVATE=${private_ip}
+        AUTHIP_PUBLIC=${ip}
+        AUTHIP_PRIVATE=${private_ip}
 
-                ${BUILD_HOME}/providerscripts/datastore/configwrapper/PutToConfigDatastore.sh ${ip} authenticatorpublicip/${ip}
-                ${BUILD_HOME}/providerscripts/datastore/configwrapper/PutToConfigDatastore.sh ${private_ip} authenticatorip/${private_ip}
+        ${BUILD_HOME}/providerscripts/datastore/configwrapper/PutToConfigDatastore.sh ${ip} authenticatorpublicip/${ip}
+        ${BUILD_HOME}/providerscripts/datastore/configwrapper/PutToConfigDatastore.sh ${private_ip} authenticatorip/${private_ip}
 
-                if ( [ "${BUILD_MACHINE_VPC}" = "1" ] )
-                then
-                        auth_active_ip="${AUTHIP_PRIVATE}"
-                elif ( [ "${BUILD_MACHINE_VPC}" = "0" ] )
-                then
-                        auth_active_ip="${AUTHIP_PUBLIC}"
-                fi
-
-                status "Have got the ip addresses for your authenticator (${authenticator_name})"
-                status "Public IP address: ${AUTHIP_PUBLIC}"
-                status "Private IP address: ${AUTHIP_PRIVATE}"
-
-                if ( [ ! -d ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/keys ] )
-                then
-                        /bin/mkdir -p ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/keys
-                fi
-
-                        status "Waiting for the authenticator machine ${authenticator_name} to complete its build. If you are waiting on this for more than 10 minutes, something is likely wrong"
-                        status "This is the current time for your reference `/bin/date`"
-
-
-                        #So, looking good. Now what we have to do is keep monitoring for the build process for our authenticator to complete
-                        done="0"
-                        alive=""
-                        alive="`/usr/bin/ssh -p ${SSH_PORT} -i ${BUILD_KEY} ${OPTIONS} ${SERVER_USER}@${auth_active_ip} "/bin/ls /home/${SERVER_USER}/runtime/AUTHENTICATOR_READY"`"
-
-                        count="0"
-                        while ( [ "${alive}" != "/home/${SERVER_USER}/runtime/AUTHENTICATOR_READY" ] && [ "${count}" -lt "300" ] )
-                        do
-                                count="`/usr/bin/expr ${count} + 1`"
-                                /bin/sleep 2
-                                alive="`/usr/bin/ssh -p ${SSH_PORT} -i ${BUILD_KEY} ${OPTIONS} ${SERVER_USER}@${auth_active_ip} "/bin/ls /home/${SERVER_USER}/runtime/AUTHENTICATOR_READY"`"
-                        done
-
-                        if ( [ "${count}" = "300" ] )
-                        then
-                                done="0"
-                        else
-                                ${BUILD_HOME}/initscripts/InitialiseDNSRecord.sh ${ip} ${WEBSITE_URL}
-                                done="1"
-                        fi
-
-                        #If $done != 1, then the authenticator didn't build properly, so, destroy the machine
-                        if ( [ "${done}" != "1" ] )
-                        then
-                                status "################################################################################################################"
-                                status "Hi, an authenticator server didn't seem to build correctly. I can destroy it and I can try to build a new authentication server for you"
-                                status "################################################################################################################"
-                                status "Press the <enter> key to be continue with the next attempt <ctrl - c> to exit"
-                                if ( [ "`${BUILD_HOME}/helperscripts/IsHardcoreBuild.sh`" != "1" ] )
-                                then
-                                        read response
-                                fi
-
-                                ${HOME}/providerscripts/datastore/configwrapper/DeleteFromConfigDatastore.sh authenticatorpublicip
-                                ${HOME}/providerscripts/datastore/configwrapper/DeleteFromConfigDatastore.sh authenticatorip
-
-                                ${BUILD_HOME}/providerscripts/server/DestroyServer.sh ${AUTHIP_PUBLIC} ${CLOUDHOST}
-
-                                #Wait until we are sure that the image server(s) are destroyed because of a faulty build
-                                while ( [ "`${BUILD_HOME}/providerscripts/server/NumberOfServers.sh "auth-${REGION}-${BUILD_IDENTIFIER}" ${CLOUDHOST} 2>/dev/null`" != "0" ] )
-                                do
-                                        /bin/sleep 30
-                                done
-                        else
-                                status "An authentication server (${authenticator_name}) has built correctly (`/usr/bin/date`) and is accepting connections"
-                                counter="`/usr/bin/expr ${counter} - 1`"
-                        fi
-        else
-                status "An authenticator is already running, using that one"
-                status "Press enter if this is OK with you"
-                if ( [ "`${BUILD_HOME}/helperscripts/IsHardcoreBuild.sh`" != "1" ] )
-                then
-                        read response
-                fi
-                done=1
+        if ( [ "${BUILD_MACHINE_VPC}" = "1" ] )
+        then
+            auth_active_ip="${AUTHIP_PRIVATE}"
+        elif ( [ "${BUILD_MACHINE_VPC}" = "0" ] )
+        then
+            auth_active_ip="${AUTHIP_PUBLIC}"
         fi
+
+        status "Have got the ip addresses for your authenticator (${authenticator_name})"
+        status "Public IP address: ${AUTHIP_PUBLIC}"
+        status "Private IP address: ${AUTHIP_PRIVATE}"
+
+        if ( [ ! -d ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/keys ] )
+        then
+            /bin/mkdir -p ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/keys
+        fi
+
+        status "Waiting for the authenticator machine ${authenticator_name} to complete its build. If you are waiting on this for more than 10 minutes, something is likely wrong"
+        status "This is the current time for your reference `/bin/date`"
+
+        #So, looking good. Now what we have to do is keep monitoring for the build process for our authenticator to complete
+        done="0"
+        alive=""
+        alive="`/usr/bin/ssh -p ${SSH_PORT} -i ${BUILD_KEY} ${OPTIONS} ${SERVER_USER}@${auth_active_ip} "/bin/ls /home/${SERVER_USER}/runtime/AUTHENTICATOR_READY"`"
+
+        count="0"
+        while ( [ "${alive}" != "/home/${SERVER_USER}/runtime/AUTHENTICATOR_READY" ] && [ "${count}" -lt "300" ] )
+        do
+            count="`/usr/bin/expr ${count} + 1`"
+            /bin/sleep 2
+            alive="`/usr/bin/ssh -p ${SSH_PORT} -i ${BUILD_KEY} ${OPTIONS} ${SERVER_USER}@${auth_active_ip} "/bin/ls /home/${SERVER_USER}/runtime/AUTHENTICATOR_READY"`"
+        done
+
+        if ( [ "${count}" = "300" ] )
+        then
+            done="0"
+        else
+            ${BUILD_HOME}/initscripts/InitialiseDNSRecord.sh ${ip} ${WEBSITE_URL}
+            done="1"
+        fi
+
+        #If $done != 1, then the authenticator didn't build properly, so, destroy the machine
+        if ( [ "${done}" != "1" ] )
+        then
+            status "################################################################################################################"
+            status "Hi, an authenticator server didn't seem to build correctly. I can destroy it and I can try to build a new authentication server for you"
+            status "################################################################################################################"
+            status "Press the <enter> key to be continue with the next attempt <ctrl - c> to exit"
+                                
+            if ( [ "`${BUILD_HOME}/helperscripts/IsHardcoreBuild.sh`" != "1" ] )
+            then
+                read response
+            fi
+
+            ${HOME}/providerscripts/datastore/configwrapper/DeleteFromConfigDatastore.sh authenticatorpublicip
+            ${HOME}/providerscripts/datastore/configwrapper/DeleteFromConfigDatastore.sh authenticatorip
+
+            ${BUILD_HOME}/providerscripts/server/DestroyServer.sh ${AUTHIP_PUBLIC} ${CLOUDHOST}
+
+            #Wait until we are sure that the image server(s) are destroyed because of a faulty build
+            while ( [ "`${BUILD_HOME}/providerscripts/server/NumberOfServers.sh "auth-${REGION}-${BUILD_IDENTIFIER}" ${CLOUDHOST} 2>/dev/null`" != "0" ] )
+            do
+                /bin/sleep 30
+            done
+        else
+            status "An authentication server (${authenticator_name}) has built correctly (`/usr/bin/date`) and is accepting connections"
+            counter="`/usr/bin/expr ${counter} - 1`"
+        fi
+    else
+        status "An authenticator is already running, using that one"
+        status "Press enter if this is OK with you"
+        if ( [ "`${BUILD_HOME}/helperscripts/IsHardcoreBuild.sh`" != "1" ] )
+        then
+            read response
+        fi
+        done=1
+    fi
 done
 
 #If we get to here then we know that the authentication server didn't build properly, so report it and exit
-
 if ( [ "${counter}" = "5" ] )
 then
-        status "The infrastructure failed to intialise because of a build problem, please investigate, correct and rebuild"
-        /usr/bin/kill -9 $PPID
+     status "The infrastructure failed to intialise because of a build problem, please investigate, correct and rebuild"
+     /usr/bin/kill -9 $PPID
 fi
