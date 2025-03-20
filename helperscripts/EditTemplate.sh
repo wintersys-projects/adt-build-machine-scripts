6ed89e9b4653bbb0b7794c33e864b5d868d363e2
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-03-20 11:52:41 +0000

    Update EditTemplate.sh

--- helperscripts/EditTemplate.sh
+++ helperscripts/EditTemplate.sh
@@ -24,34 +24,34 @@ BUILD_HOME="`/bin/cat /home/buildhome.dat`"
 
 if ( [ "${1}" != "" ] && [ "${2}" != "" ] )
 then
-        CLOUDHOST="${1}"
-        template="${2}"
+    CLOUDHOST="${1}"
+    template="${2}"
 else
-        /bin/echo "Which cloudhost do you want to view logs for (1)DigitalOcean, (2)Exoscale, (3)Linode or (4)Vultr"
-        read response
+    /bin/echo "Which cloudhost do you want to view logs for (1)DigitalOcean, (2)Exoscale, (3)Linode or (4)Vultr"
+    read response
 
-        if ( [ "${response}" = "1" ] )
-        then
-                CLOUDHOST="digitalocean"
-        fi
-        if ( [ "${response}" = "2" ] )
-        then
-                CLOUDHOST="exoscale"
-        fi
-        if ( [ "${response}" = "3" ] )
-        then
-                CLOUDHOST="linode"
-        fi
-        if ( [ "${response}" = "4" ] )
-        then
-                CLOUDHOST="vultr"
-        fi
+    if ( [ "${response}" = "1" ] )
+    then
+        CLOUDHOST="digitalocean"
+    fi
+    if ( [ "${response}" = "2" ] )
+    then
+        CLOUDHOST="exoscale"
+    fi
+    if ( [ "${response}" = "3" ] )
+    then
+        CLOUDHOST="linode"
+    fi
+    if ( [ "${response}" = "4" ] )
+    then
+        CLOUDHOST="vultr"
+    fi
 
-        /bin/echo "The following templates are available, which one do you want?"
-        /bin/ls ${BUILD_HOME}/templatedconfigurations/templates/${CLOUDHOST}/*tmpl | /usr/bin/awk -F'/' '{print $NF}'
+    /bin/echo "The following templates are available, which one do you want?"
+    /bin/ls ${BUILD_HOME}/templatedconfigurations/templates/${CLOUDHOST}/*tmpl | /usr/bin/awk -F'/' '{print $NF}'
 
-        /bin/echo "Type the name of the template you want from the list above"
-        read template
+    /bin/echo "Type the name of the template you want from the list above"
+    read template
 fi
 
 /usr/bin/vi ${BUILD_HOME}/templatedconfigurations/templates/${CLOUDHOST}/${template}
