4c4969429c511fc4b7623dd65f9feab82cf253ff
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-03-20 13:32:02 +0000

    Update ExecuteOnDatabase.sh

--- helperscripts/ExecuteOnDatabase.sh
+++ helperscripts/ExecuteOnDatabase.sh
@@ -2,7 +2,7 @@
 ######################################################################################################################################################
 # Author: Peter Winter
 # Date  : 13/07/2016
-# Description : This script will connect you to your database(s) via ssh
+# Description : This script will connect you to your database via ssh and run the command passed as a parameter
 ######################################################################################################################################################
 # License Agreement:
 # This file is part of The Agile Deployment Toolkit.
