da012760847e0293587d4d52e4da29af1349b076
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-03-21 08:03:35 +0000

    Update GetBuildClientIP.sh

--- helperscripts/GetBuildClientIP.sh
+++ helperscripts/GetBuildClientIP.sh
@@ -24,18 +24,18 @@ export BUILD_CLIENT_IP="`/usr/bin/wget http://ipinfo.io/ip -qO -`"
 
 if ( [ "${BUILD_CLIENT_IP}" = "" ] )
 then
-    export BUILD_CLIENT_IP="`/usr/bin/curl -4 icanhazip.com`"
+	export BUILD_CLIENT_IP="`/usr/bin/curl -4 icanhazip.com`"
 fi
 
 if ( [ "${BUILD_CLIENT_IP}" = "" ] )
 then
-    export BUILD_CLIENT_IP="`/bin/hostname -I | /usr/bin/awk '{print $1}'`"
+	export BUILD_CLIENT_IP="`/bin/hostname -I | /usr/bin/awk '{print $1}'`"
 fi
 
 if ( [ "${BUILD_CLIENT_IP}" = "" ] )
 then
-    /bin/echo "Couldn't get build client IP address after 3 separate attempts, having to exit"
-    exit
+	/bin/echo "Couldn't get build client IP address after 3 separate attempts, having to exit"
+	exit
 else 
-    /bin/echo ${BUILD_CLIENT_IP}
+	/bin/echo ${BUILD_CLIENT_IP}
 fi
