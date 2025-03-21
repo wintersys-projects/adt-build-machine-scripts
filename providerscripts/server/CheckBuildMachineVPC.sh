10965f68765b9bc4e0ed5e3b808c0aaf59d5a762
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-03-21 23:44:09 +0000

    Update CheckBuildMachineVPC.sh

--- providerscripts/server/CheckBuildMachineVPC.sh
+++ providerscripts/server/CheckBuildMachineVPC.sh
@@ -1,4 +1,25 @@
-
+#!/bin/sh
+###################################################################################
+# Author : Peter Winter
+# Date   : 15/02/2024
+# Description : This script does a rudimentary check to check that your build-machine
+# is connected to a VPC when its supposed to be
+###################################################################################
+# License Agreement:
+# This file is part of The Agile Deployment Toolkit.
+# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+####################################################################################
+####################################################################################
+#set -x
 
 status () {
 	/bin/echo "${1}" | /usr/bin/tee /dev/fd/3 2>/dev/null
