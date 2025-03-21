2f697aac73acaf0a1f9e66b3e7e0d640bba6702d
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-03-21 18:44:34 +0000

    Update ListFromDatastore.sh

--- providerscripts/datastore/ListFromDatastore.sh
+++ providerscripts/datastore/ListFromDatastore.sh
@@ -21,26 +21,26 @@
 #set -x
 
 status () {
-        /bin/echo "${1}" | /usr/bin/tee /dev/fd/3 2>/dev/null
-        script_name="`/bin/echo ${0} | /usr/bin/awk -F'/' '{print $NF}'`"
-        /bin/echo "${script_name}: ${1}" >> /dev/fd/4  2>/dev/null
+    /bin/echo "${1}" | /usr/bin/tee /dev/fd/3 2>/dev/null
+    script_name="`/bin/echo ${0} | /usr/bin/awk -F'/' '{print $NF}'`"
+    /bin/echo "${script_name}: ${1}" >> /dev/fd/4  2>/dev/null
 }
 
 file_to_list="$1"
 
 if ( [ "${BUILD_HOME}" = "" ] )
 then 
-        BUILD_HOME="`/bin/cat /home/buildhome.dat`"
+    BUILD_HOME="`/bin/cat /home/buildhome.dat`"
 fi
 
 
 if ( [ "`/bin/grep "^DATASTORETOOL:*" ${BUILD_HOME}/builddescriptors/buildstyles.dat | /bin/grep s3cmd`" != "" ] )
 then
-        datastore_tool="/usr/bin/s3cmd --force "
+    datastore_tool="/usr/bin/s3cmd --force "
 elif ( [ "`/bin/grep "^DATASTORETOOL:*" ${BUILD_HOME}/builddescriptors/buildstyles.dat | /bin/grep s5cmd`" != "" ] )
 then
-        host_base="`/bin/grep host_base /root/.s5cfg | /bin/grep host_base | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`" 
-        datastore_tool="/usr/bin/s5cmd --credentials-file /root/.s5cfg --endpoint-url https://${host_base} "
+    host_base="`/bin/grep host_base /root/.s5cfg | /bin/grep host_base | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`" 
+    datastore_tool="/usr/bin/s5cmd --credentials-file /root/.s5cfg --endpoint-url https://${host_base} "
 fi
 
 ${datastore_tool} ls s3://${file_to_list} 2>/dev/null
