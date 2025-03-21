28ca366ddb03ac4aed25c67fc74eace43caa4745
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-03-21 19:09:55 +0000

    Update GetProxyDNSIPs.sh

--- providerscripts/security/firewall/GetProxyDNSIPs.sh
+++ providerscripts/security/firewall/GetProxyDNSIPs.sh
@@ -21,9 +21,9 @@
 #set -x
 
 status () {
-        /bin/echo "${1}" | /usr/bin/tee /dev/fd/3 2>/dev/null
-        script_name="`/bin/echo ${0} | /usr/bin/awk -F'/' '{print $NF}'`"
-        /bin/echo "${script_name}: ${1}" >> /dev/fd/4  2>/dev/null
+	/bin/echo "${1}" | /usr/bin/tee /dev/fd/3 2>/dev/null
+	script_name="`/bin/echo ${0} | /usr/bin/awk -F'/' '{print $NF}'`"
+	/bin/echo "${script_name}: ${1}" >> /dev/fd/4  2>/dev/null
 }
 
 BUILD_HOME="`/bin/cat /home/buildhome.dat`"
@@ -55,22 +55,21 @@ then
 	fi
 	if ( [ "${CLOUDHOST}" = "linode" ] )
 	then
-		 #dynamic
-                 alldnsproxyips="`/usr/bin/curl -X GET "https://api.cloudflare.com/client/v4/ips" | /usr/bin/jq  '.result.ipv4_cidrs[]'  | /usr/bin/tr '\n' ',' | /bin/sed 's/.$//'`"		 #hardcoded
-		 if ( [ "${alldnsproxyips}" = "" ] || [ "${alldnsproxyips}" = "null" ] )
-		 then
+		#dynamic
+		alldnsproxyips="`/usr/bin/curl -X GET "https://api.cloudflare.com/client/v4/ips" | /usr/bin/jq  '.result.ipv4_cidrs[]'  | /usr/bin/tr '\n' ',' | /bin/sed 's/.$//'`"		 #hardcoded
+		if ( [ "${alldnsproxyips}" = "" ] || [ "${alldnsproxyips}" = "null" ] )
+		then
 			alldnsproxyips='"103.21.244.0/22","103.22.200.0/22","103.31.4.0/22","104.16.0.0/13","104.24.0.0/14","108.162.192.0/18","141.101.64.0/18","162.158.0.0/15","172.64.0.0/13","173.245.48.0/20","188.114.96.0/20","190.93.240.0/20","197.234.240.0/22","198.41.128.0/17","131.0.72.0/22","199.27.128.0/21"'		
-   		 fi
+		fi
 	fi
 	if ( [ "${CLOUDHOST}" = "vultr" ] )
 	then
-		 #dynamic
-		 alldnsproxyips="`/usr/bin/curl -X GET "https://api.cloudflare.com/client/v4/ips" | /usr/bin/jq  -r '.result.ipv4_cidrs[]'  | /usr/bin/tr '\n' ' '`"
-		 
-		 #hardcoded
-		 if ( [ "${alldnsproxyips}" = "\"\"" ] || [ "${alldnsproxyips}" = "null" ] )
-		 then
-			  alldnsproxyips="103.21.244.0/22 103.22.200.0/22 103.31.4.0/22 104.16.0.0/13 104.24.0.0/14 108.162.192.0/18 141.101.64.0/18 162.158.0.0/15 172.64.0.0/13 173.245.48.0/20 188.114.96.0/20 190.93.240.0/20 197.234.240.0/22 198.41.128.0/17 131.0.72.0/22 199.27.128.0/21"
-		 fi
+		#dynamic
+		alldnsproxyips="`/usr/bin/curl -X GET "https://api.cloudflare.com/client/v4/ips" | /usr/bin/jq  -r '.result.ipv4_cidrs[]'  | /usr/bin/tr '\n' ' '`"
+		#hardcoded
+		if ( [ "${alldnsproxyips}" = "\"\"" ] || [ "${alldnsproxyips}" = "null" ] )
+		then
+			alldnsproxyips="103.21.244.0/22 103.22.200.0/22 103.31.4.0/22 104.16.0.0/13 104.24.0.0/14 108.162.192.0/18 141.101.64.0/18 162.158.0.0/15 172.64.0.0/13 173.245.48.0/20 188.114.96.0/20 190.93.240.0/20 197.234.240.0/22 198.41.128.0/17 131.0.72.0/22 199.27.128.0/21"
+		fi
 	fi
 fi
