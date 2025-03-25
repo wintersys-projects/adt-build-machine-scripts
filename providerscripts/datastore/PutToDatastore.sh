6fa21bb7f17985e1bec50701db9a57bfe571a191
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-03-25 10:08:15 +0000

    Update PutToDatastore.sh

--- providerscripts/datastore/PutToDatastore.sh
+++ providerscripts/datastore/PutToDatastore.sh
@@ -2,7 +2,7 @@
 #####################################################################################
 # Author: Peter Winter
 # Date :  9/4/2016
-# Description: Put files into a bucket in the datastore
+# Description: This will put a file into a specified datastore bucket
 #####################################################################################
 # License Agreement:
 # This file is part of The Agile Deployment Toolkit.
