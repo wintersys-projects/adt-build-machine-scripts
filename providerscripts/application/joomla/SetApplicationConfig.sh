5a3611167b6ea6d3296f9ccef5a1529faff6c8ef
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-03-25 09:50:23 +0000

    Update SetApplicationConfig.sh

--- providerscripts/application/joomla/SetApplicationConfig.sh
+++ providerscripts/application/joomla/SetApplicationConfig.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ####################################################################################
-# Description: This sets the bare minimum configuration.php values to get the joomla
-# application online
+# Description: This sets up the database credentials and some other configuration
+# settings necessary for the drupal application to be brought online
 # Date: 07/11/2024
 # Author: Peter Winter
 ####################################################################################
