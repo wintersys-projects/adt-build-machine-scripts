8dfd26802c619e0be5aa5950a75c7c959cd55459
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-03-25 10:03:28 +0000

    Update DeleteDatastore.sh

--- providerscripts/datastore/DeleteDatastore.sh
+++ providerscripts/datastore/DeleteDatastore.sh
@@ -2,7 +2,7 @@
 #########################################################################################
 # Author: Peter Winter
 # Date :  9/4/2016
-# Description: Put files into a bucket in the datastore
+# Description: Delete files from the datastore bucket provided
 ##########################################################################################
 # License Agreement:
 # This file is part of The Agile Deployment Toolkit.
