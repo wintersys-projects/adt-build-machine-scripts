c8498f455a7e2d9f8c3cc98e6b6c9256f6ed8154
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-04-30 15:12:06 +0100

    Update InitialiseDatabaseService.sh

--- initscripts/InitialiseDatabaseService.sh
+++ initscripts/InitialiseDatabaseService.sh
@@ -137,14 +137,14 @@ then
                         done
 
                         #Get the user's password by creating or new user or interrogating an existing one
-                        database_password=""
+                     #   database_password=""
 
-                        if ( [ "`/usr/local/bin/doctl database user list ${cluster_id} -o json | /usr/bin/jq -r '.[] | select (.name == "'${database_user}'").name'`" = "" ] )
-                        then
-                                database_password="`/usr/local/bin/doctl databases user create  ${cluster_id} ${database_user} -o json | /usr/bin/jq -r '.[].password'`"
-                        else 
-                                database_password="`/usr/local/bin/doctl database user list ${cluster_id} -o json | /usr/bin/jq -r '.[] | select (.name == "'${database_user}'").password'`"
-                        fi
+                     #   if ( [ "`/usr/local/bin/doctl database user list ${cluster_id} -o json | /usr/bin/jq -r '.[] | select (.name == "'${database_user}'").name'`" = "" ] )
+                     #   then
+                     #           database_password="`/usr/local/bin/doctl databases user create  ${cluster_id} ${database_user} -o json | /usr/bin/jq -r '.[].password'`"
+                     #   else 
+                     #           database_password="`/usr/local/bin/doctl database user list ${cluster_id} -o json | /usr/bin/jq -r '.[] | select (.name == "'${database_user}'").password'`"
+                     #   fi
 
                         status "######################################################################################################################################################"
                         status "You might want to check that a database cluster called ${cluster_name} with a database ${db_name} is present using your Digital Ocean gui system"
@@ -175,8 +175,9 @@ then
                         export DATABASE_INSTALLATION_TYPE="DBaaS"
                         export DATABASE_DBaaS_INSTALLATION_TYPE="${DATABASE_DBaaS_INSTALLATION_TYPE}:${cluster_id}"
                         export DB_IDENTIFIER="private-`/usr/local/bin/doctl databases connection ${cluster_id} -o json | /usr/bin/jq -r '.host'`"
-                        export DB_USERNAME="${database_user}"
-                        export DB_PASSWORD="${database_password}"
+                        export DB_USERNAME="`/usr/local/bin/doctl databases connection ${cluster_id} -o json | /usr/bin/jq -r '.user'`"
+                        export DB_PASSWORD="`/usr/local/bin/doctl databases connection ${cluster_id} -o json | /usr/bin/jq -r '.password'`"
+                      #  export DB_NAME="`/usr/local/bin/doctl databases connection ${cluster_id} -o json | /usr/bin/jq -r '.database'`"
                         export DB_NAME="${db_name}"
                         export DB_PORT="`/usr/local/bin/doctl -o json databases connection ${cluster_id} | /usr/bin/jq -r '.port'`"
 
