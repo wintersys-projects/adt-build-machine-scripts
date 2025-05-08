9ff048d2140505e3236ba4cf970b7f56d416bfaa
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-05-08 23:27:41 +0100

    Update InitialiseSecurityKeys.sh

--- initscripts/InitialiseSecurityKeys.sh
+++ initscripts/InitialiseSecurityKeys.sh
@@ -80,10 +80,10 @@ fi
 /bin/chmod 700 ${BUILD_KEY}.pub
 
 key_name="${PUBLIC_KEY_NAME}-${BUILD_IDENTIFIER}"
-${BUILD_HOME}/providerscripts/security/ssh-keys/DeleteSSHKeyPair.sh "${key_name}" "${TOKEN}" ${CLOUDHOST}
-${BUILD_HOME}/providerscripts/security/ssh-keys/RegisterSSHKeyPair.sh "${key_name}" "${TOKEN}" "`/bin/cat ${BUILD_KEY}.pub`" ${CLOUDHOST}
+${BUILD_HOME}/providerscripts/security/ssh/DeleteSSHKeyPair.sh "${key_name}" "${TOKEN}" ${CLOUDHOST}
+${BUILD_HOME}/providerscripts/security/ssh/RegisterSSHKeyPair.sh "${key_name}" "${TOKEN}" "`/bin/cat ${BUILD_KEY}.pub`" ${CLOUDHOST}
 
-PUBLIC_KEY_ID="`${BUILD_HOME}/providerscripts/security/GetSSHKeyID.sh \"${key_name}\" ${CLOUDHOST}`"
+PUBLIC_KEY_ID="`${BUILD_HOME}/providerscripts/security/ssh/GetSSHKeyID.sh \"${key_name}\" ${CLOUDHOST}`"
 
 if ( [ ! -d ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/credentials ] )
 then
