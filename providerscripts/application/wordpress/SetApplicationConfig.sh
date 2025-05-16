28c9f023e815a40f4d37ac859cc43e6a94849234
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-05-16 22:09:09 +0100

    Update SetApplicationConfig.sh

--- providerscripts/application/wordpress/SetApplicationConfig.sh
+++ providerscripts/application/wordpress/SetApplicationConfig.sh
@@ -23,40 +23,40 @@
 
 if ( [ ! -f ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/dbp.dat ] )
 then
-	status "Error, cannot find database prefix file"
+        status "Error, cannot find database prefix file"
 fi
 
 dbprefix="`/bin/cat ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/dbp.dat`"
-/bin/sed -i '/DB_HOST/c\ define("DB_HOST", "'${database_identifier}:${db_port}'");' ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config.php.default
-/bin/sed -i '/DB_USER/c\ define("DB_USER", "'${database_username}'");' ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config.php.default
-/bin/sed -i '/DB_PASSWORD/c\ define("DB_PASSWORD", "'${database_password}'");' ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config.php.default
-/bin/sed -i '/DB_NAME/c\ define("DB_NAME", "'${database_name}'");' ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config.php.default
+/bin/sed -i '/DB_HOST/c\ define("DB_HOST", "'${DB_IDENTIFIER}:${DB_PORT}'");' ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config.php.default
+/bin/sed -i '/DB_USER/c\ define("DB_USER", "'${DB_USERNAME}'");' ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config.php.default
+/bin/sed -i '/DB_PASSWORD/c\ define("DB_PASSWORD", "'${DB_PASSWORD}'");' ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config.php.default
+/bin/sed -i '/DB_NAME/c\ define("DB_NAME", "'${DB_NAME}'");' ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config.php.default
 /bin/sed -i '/\$table_prefix/c\ \$table_prefix="'${dbprefix}'";' ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config.php.default
 
 if ( [ "`/bin/grep SALTEDALREADY ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config.php.default`" = "" ] )
 then
-	/bin/sed -i "/'AUTH_KEY'/i XXYYZZ" ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config.php.default
-	/bin/sed -i '/AUTH_KEY/,+7d' ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config.php.default
-	salts="`/usr/bin/curl https://api.wordpress.org/secret-key/1.1/salt`"
-	/bin/sed -n '/XXYYZZ/q;p' ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config.php.default > /tmp/firsthalf
-	/bin/sed '0,/^XXYYZZ$/d' ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config.php.default > /tmp/secondhalf
-	/bin/cat /tmp/firsthalf > /tmp/fullfile
-	/bin/echo ${salts} >> /tmp/fullfile
-	/bin/echo "/* SALTEDALREADY */" >> /tmp/fullfile
-	/bin/echo "define( 'DISALLOW_FILE_EDIT', true );" >> /tmp/fullfile
-	/bin/echo "define('WP_CACHE', false);" >> /tmp/fullfile
-	/bin/echo "define('CONCATENATE_SCRIPTS', true);" >> /tmp/fullfile
-	/bin/echo "define('COMPRESS_SCRIPTS', true);" >> /tmp/fullfile
-	/bin/echo "define('COMPRESS_CSS', true);" >> /tmp/fullfile
-	/bin/echo "define('DISABLE_WP_CRON', true);" >> /tmp/fullfile
-	/bin/cat /tmp/secondhalf >> /tmp/fullfile
-	/bin/rm /tmp/firsthalf /tmp/secondhalf
-	/bin/mv /tmp/fullfile ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config.php.default
+        /bin/sed -i "/'AUTH_KEY'/i XXYYZZ" ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config.php.default
+        /bin/sed -i '/AUTH_KEY/,+7d' ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config.php.default
+        salts="`/usr/bin/curl https://api.wordpress.org/secret-key/1.1/salt`"
+        /bin/sed -n '/XXYYZZ/q;p' ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config.php.default > /tmp/firsthalf
+        /bin/sed '0,/^XXYYZZ$/d' ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config.php.default > /tmp/secondhalf
+        /bin/cat /tmp/firsthalf > /tmp/fullfile
+        /bin/echo ${salts} >> /tmp/fullfile
+        /bin/echo "/* SALTEDALREADY */" >> /tmp/fullfile
+        /bin/echo "define( 'DISALLOW_FILE_EDIT', true );" >> /tmp/fullfile
+        /bin/echo "define('WP_CACHE', false);" >> /tmp/fullfile
+        /bin/echo "define('CONCATENATE_SCRIPTS', true);" >> /tmp/fullfile
+        /bin/echo "define('COMPRESS_SCRIPTS', true);" >> /tmp/fullfile
+        /bin/echo "define('COMPRESS_CSS', true);" >> /tmp/fullfile
+        /bin/echo "define('DISABLE_WP_CRON', true);" >> /tmp/fullfile
+        /bin/cat /tmp/secondhalf >> /tmp/fullfile
+        /bin/rm /tmp/firsthalf /tmp/secondhalf
+        /bin/mv /tmp/fullfile ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config.php.default
 fi
 
 ${BUILD_HOME}/providerscripts/datastore/configwrapper/PutToConfigDatastore.sh ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config.php.default wordpress_config.php
 
 if ( [ "`${BUILD_HOME}/providerscripts/datastore/configwrapper/ListFromConfigDatastore.sh wordpress_config.php`" = "" ] )
 then
-	status "Didn't generate the wordpress configuration file in the config datastore, this will cause trouble later"
+        status "Didn't generate the wordpress configuration file in the config datastore, this will cause trouble later"
 fi
