0b237f73537a6eed35a2a107117e6f78e5179642
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-05-28 14:46:36 +0100

    Update PerformDatabaseBaseline.sh

--- helperscripts/PerformDatabaseBaseline.sh
+++ helperscripts/PerformDatabaseBaseline.sh
@@ -146,7 +146,7 @@ read x
 
 generated="not ok"
 
-/usr/bin/ssh -o ConnectTimeout=10 -o ConnectionAttempts=30 -o UserKnownHostsFile=${DATABASE_PUBLIC_KEYS}  -o StrictHostKeyChecking=yes -p ${SSH_PORT} -i ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/keys/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY_${BUILD_IDENTIFIER} ${SERVER_USERNAME}@${DB_IP} "${SUDO} /home/${SERVER_USERNAME}/providerscripts/backupscripts/CreateDBBaseline.sh ${identifier}" 2>/dev/null
+/usr/bin/ssh -o ConnectTimeout=10 -o ConnectionAttempts=30 -o UserKnownHostsFile=${DATABASE_PUBLIC_KEYS}  -o StrictHostKeyChecking=yes -p ${SSH_PORT} -i ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/keys/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY_${BUILD_IDENTIFIER} ${SERVER_USERNAME}@${DB_IP} "${SUDO} /home/${SERVER_USERNAME}/application/backupscripts/CreateDBBaseline.sh ${identifier}" 2>/dev/null
 
 if ( [ "$?" = "0" ] )
 then
