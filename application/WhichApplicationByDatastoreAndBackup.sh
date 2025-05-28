66b196518c648ae05a2bb26f8462f90873ad7db7
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-05-28 18:05:33 +0100

    Update WhichApplicationByDatastoreAndBackup.sh

--- application/WhichApplicationByDatastoreAndBackup.sh
+++ application/WhichApplicationByDatastoreAndBackup.sh
@@ -22,9 +22,9 @@
 #set -x
 
 status () {
-	/bin/echo "${1}" | /usr/bin/tee /dev/fd/3 2>/dev/null
-	script_name="`/bin/echo ${0} | /usr/bin/awk -F'/' '{print $NF}'`"
-	/bin/echo "${script_name}: ${1}" | /usr/bin/tee -a /dev/fd/4 2>/dev/null
+        /bin/echo "${1}" | /usr/bin/tee /dev/fd/3 2>/dev/null
+        script_name="`/bin/echo ${0} | /usr/bin/awk -F'/' '{print $NF}'`"
+        /bin/echo "${script_name}: ${1}" | /usr/bin/tee -a /dev/fd/4 2>/dev/null
 }
 
 BUILD_HOME="`/bin/cat /home/buildhome.dat`" 
@@ -35,12 +35,43 @@ interrogation_home="${BUILD_HOME}/interrogation"
 
 APPLICATION=""
 
-if ( [ -f ${interrogation_home}/applicationDB.sql ] )
+if ( [ ! -f ${interrogation_home}/dbe.dat ] )
 then
-        db_type="sql"
-elif ( [ -f ${interrogation_home}/applicationDB.sql ] )
+        status "NOTICE: couldn't detect the database type for your application"
+else
+        if ( [ "`/bin/grep Maria ${interrogation_home}/dbe.dat`" != "" ] )
+        then
+                db_type="sql"
+        fi
+        if ( [ "`/bin/grep MySQL ${interrogation_home}/dbe.dat`" != "" ] )
+        then
+                db_type="sql"
+        fi
+        if ( [ "`/bin/grep Postgres ${interrogation_home}/dbe.dat`" != "" ] )
+        then
+                db_type="postgres"
+        fi
+fi
+
+if ( [ ! -f ${interrogation_home}/applicationDB.sql ] && [ ! -f ${interrogation_home}/applicationDB.psql ] )
 then
-        db_type="postgres"
+        status "NOTICE: Can't find a suitable database dump file for your application"
+        status "Press <enter> to acknowledge and accept <ctrl-c> to exit and investigate"
+        read x
+fi
+
+if ( [ -f ${interrogation_home}/applicationDB.sql ] && [ "${db_type}" != "sql" ] )
+then
+        status "It seems like there is a mismatch between the type of database and thw webroot type"
+        /usr/bin/kill -9 $PPID
+        exit
+fi
+
+if ( [ -f ${interrogation_home}/applicationDB.psql ] && [ "${db_type}" != "postgres" ] )
+then
+        status "It seems like there is a mismatch between the type of database and thw webroot type"
+        /usr/bin/kill -9 $PPID
+        exit
 fi
 
 #################JOOMLA################
@@ -55,125 +86,125 @@ then
 
         status "Discovered you are deploying joomla from a datastore backup with ${db_type} database type"
         status "Press the <enter> key to accept as true"
-	
-	if ( [ "`${BUILD_HOME}/helperscripts/IsHardcoreBuild.sh`" != "1" ] )
-	then
-		read x
-	fi
- 	if ( [ -f ${interrogation_home}/configuration.php.default ] )
-	then
- 		/bin/cp ${interrogation_home}/configuration.php.default ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/configuration.php.default
-	fi
- 	if ( [ ! -f ${interrogation_home}/dbp.dat ] )
- 	then
-		status "Error, cannot find db prefix file"
-		/usr/bin/kill -9 $PPID
-		exit
-	fi
-	/bin/cp ${interrogation_home}/dbp.dat ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}
-	${BUILD_HOME}/providerscripts/datastore/configwrapper/PutToConfigDatastore.sh ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/dbp.dat
+
+        if ( [ "`${BUILD_HOME}/helperscripts/IsHardcoreBuild.sh`" != "1" ] )
+        then
+                read x
+        fi
+        if ( [ -f ${interrogation_home}/configuration.php.default ] )
+        then
+                /bin/cp ${interrogation_home}/configuration.php.default ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/configuration.php.default
+        fi
+        if ( [ ! -f ${interrogation_home}/dbp.dat ] )
+        then
+                status "Error, cannot find db prefix file"
+                /usr/bin/kill -9 $PPID
+                exit
+        fi
+        /bin/cp ${interrogation_home}/dbp.dat ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}
+        ${BUILD_HOME}/providerscripts/datastore/configwrapper/PutToConfigDatastore.sh ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/dbp.dat
 #################WORDPRESS################
 elif ( [ "`/bin/cat ${interrogation_home}/dba.dat`" = "WORDPRESS" ] )
 then
-	/bin/touch ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/APPLICATION:wordpress
-	APPLICATION="wordpress"
-	if ( [ "${DIRECTORIES_TO_MOUNT}" = "" ] )
-	then
-		DIRECTORIES_TO_MOUNT="wp-content.uploads"
-	fi
-	status "Discovered you are deploying wordpress from a datastore backup with ${db_type} database type"
-	status "Press the <enter> key to accept as true"
-	if ( [ "`${BUILD_HOME}/helperscripts/IsHardcoreBuild.sh`" != "1" ] )
-	then
-		read x
-	fi
-	if ( [ -f ${interrogation_home}/wp-config.php.default ] )
-	then
-		/bin/cp ${interrogation_home}/wp-config.php.default ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config.php.default
-	else
-		status "Couldn't find joomla default configuration file in baseline webroot"
-		/usr/bin/kill -9 $PPID    
-		exit
-	fi
+        /bin/touch ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/APPLICATION:wordpress
+        APPLICATION="wordpress"
+        if ( [ "${DIRECTORIES_TO_MOUNT}" = "" ] )
+        then
+                DIRECTORIES_TO_MOUNT="wp-content.uploads"
+        fi
+        status "Discovered you are deploying wordpress from a datastore backup with ${db_type} database type"
+        status "Press the <enter> key to accept as true"
+        if ( [ "`${BUILD_HOME}/helperscripts/IsHardcoreBuild.sh`" != "1" ] )
+        then
+                read x
+        fi
+        if ( [ -f ${interrogation_home}/wp-config.php.default ] )
+        then
+                /bin/cp ${interrogation_home}/wp-config.php.default ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/wp-config.php.default
+        else
+                status "Couldn't find joomla default configuration file in baseline webroot"
+                /usr/bin/kill -9 $PPID    
+                exit
+        fi
 
-	if ( [ ! -f ${interrogation_home}/dbp.dat ] )
-	then
-		status "Error, cannot find db prefix file"
-	fi
-	/bin/cp ${interrogation_home}/dbp.dat ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}
-	${BUILD_HOME}/providerscripts/datastore/configwrapper/PutToConfigDatastore.sh ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/dbp.dat
+        if ( [ ! -f ${interrogation_home}/dbp.dat ] )
+        then
+                status "Error, cannot find db prefix file"
+        fi
+        /bin/cp ${interrogation_home}/dbp.dat ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}
+        ${BUILD_HOME}/providerscripts/datastore/configwrapper/PutToConfigDatastore.sh ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/dbp.dat
 #################DRUPAL################
 elif ( [ "`/bin/cat ${interrogation_home}/dba.dat`" = "DRUPAL" ] )
 then
-	/bin/touch ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/APPLICATION:drupal
-	APPLICATION="drupal"
-	if ( [ "${DIRECTORIES_TO_MOUNT}" = "" ] )
-	then
-		DIRECTORIES_TO_MOUNT="sites.default.files.pictures:sites.default.files.styles:sites.default.files.inline-images"
-	fi
-	status "Discovered you are deploying drupal from a datastore backup with ${db_type} database type"
-	status "Press the <enter> key to accept as true"
-	if ( [ "`${BUILD_HOME}/helperscripts/IsHardcoreBuild.sh`" != "1" ] )
-	then
-		read x
-	fi
-	if ( [ -f ${interrogation_home}/sites/default/settings.php.default ] )
-	then
-		/bin/cp ${interrogation_home}/sites/default/settings.php.default ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/settings.php.default
-	else
-		status "Couldn't find drupal default configuration file in backup webroot"
-		/usr/bin/kill -9 $PPID   
-		exit
-	fi
+        /bin/touch ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/APPLICATION:drupal
+        APPLICATION="drupal"
+        if ( [ "${DIRECTORIES_TO_MOUNT}" = "" ] )
+        then
+                DIRECTORIES_TO_MOUNT="sites.default.files.pictures:sites.default.files.styles:sites.default.files.inline-images"
+        fi
+        status "Discovered you are deploying drupal from a datastore backup with ${db_type} database type"
+        status "Press the <enter> key to accept as true"
+        if ( [ "`${BUILD_HOME}/helperscripts/IsHardcoreBuild.sh`" != "1" ] )
+        then
+                read x
+        fi
+        if ( [ -f ${interrogation_home}/sites/default/settings.php.default ] )
+        then
+                /bin/cp ${interrogation_home}/sites/default/settings.php.default ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/settings.php.default
+        else
+                status "Couldn't find drupal default configuration file in backup webroot"
+                /usr/bin/kill -9 $PPID   
+                exit
+        fi
 
-	if ( [ ! -f ${interrogation_home}/dbp.dat ] )
-	then
-		status "Error, cannot find db prefix file"
-	fi
+        if ( [ ! -f ${interrogation_home}/dbp.dat ] )
+        then
+                status "Error, cannot find db prefix file"
+        fi
 
-	/bin/cp ${interrogation_home}/dbp.dat ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}
-	${BUILD_HOME}/providerscripts/datastore/configwrapper/PutToConfigDatastore.sh ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/dbp.dat
-	#################MOODLE################
+        /bin/cp ${interrogation_home}/dbp.dat ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}
+        ${BUILD_HOME}/providerscripts/datastore/configwrapper/PutToConfigDatastore.sh ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/dbp.dat
+        #################MOODLE################
 elif ( [ "`/bin/cat ${interrogation_home}/dba.dat`" = "MOODLE" ] )
 then
-	/bin/touch ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/APPLICATION:moodle
-	APPLICATION="moodle"
-	if ( [ "${DIRECTORIES_TO_MOUNT}" = "" ] )
-	then
-		DIRECTORIES_TO_MOUNT="moodledata.filedir"
-	fi
-	status "Discovered you are deploying moodle from a datastore backup with ${db_type} database type"
-	status "Press the <enter> key to accept as true"
-	if ( [ "`${BUILD_HOME}/helperscripts/IsHardcoreBuild.sh`" != "1" ] )
-	then
-		read x
-	fi
+        /bin/touch ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/APPLICATION:moodle
+        APPLICATION="moodle"
+        if ( [ "${DIRECTORIES_TO_MOUNT}" = "" ] )
+        then
+                DIRECTORIES_TO_MOUNT="moodledata.filedir"
+        fi
+        status "Discovered you are deploying moodle from a datastore backup with ${db_type} database type"
+        status "Press the <enter> key to accept as true"
+        if ( [ "`${BUILD_HOME}/helperscripts/IsHardcoreBuild.sh`" != "1" ] )
+        then
+                read x
+        fi
 
-	if ( [ -f ${interrogation_home}/config.php.default ] )
-	then
-		/bin/cp ${interrogation_home}/config.php.default ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/config.php.default
-	else
-		status "Couldn't find moodle default configuration file in backup archive webroot"
-		/usr/bin/kill -9 $PPID   
-		exit
-	fi
+        if ( [ -f ${interrogation_home}/config.php.default ] )
+        then
+                /bin/cp ${interrogation_home}/config.php.default ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/config.php.default
+        else
+                status "Couldn't find moodle default configuration file in backup archive webroot"
+                /usr/bin/kill -9 $PPID   
+                exit
+        fi
 
-	if ( [ ! -f ${interrogation_home}/dbp.dat ] )
-	then
-		status "Error, cannot find db prefix file"
-	fi
+        if ( [ ! -f ${interrogation_home}/dbp.dat ] )
+        then
+                status "Error, cannot find db prefix file"
+        fi
      
-	/bin/cp ${interrogation_home}/dbp.dat ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}
-	${BUILD_HOME}/providerscripts/datastore/configwrapper/PutToConfigDatastore.sh ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/dbp.dat
+        /bin/cp ${interrogation_home}/dbp.dat ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}
+        ${BUILD_HOME}/providerscripts/datastore/configwrapper/PutToConfigDatastore.sh ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/dbp.dat
 fi
 
 /bin/rm -r ${interrogation_home}
 
 if ( [ "${APPLICATION}" = "" ] )
 then
-	status "Couldn't find a recognised application type. If you are sure you are OK with this, hit <enter> otherwise <ctrl-c> and have a look into what is going on"
-	if ( [ "`${BUILD_HOME}/helperscripts/IsHardcoreBuild.sh`" != "1" ] )
-	then
-		read x
-	fi
+        status "Couldn't find a recognised application type. If you are sure you are OK with this, hit <enter> otherwise <ctrl-c> and have a look into what is going on"
+        if ( [ "`${BUILD_HOME}/helperscripts/IsHardcoreBuild.sh`" != "1" ] )
+        then
+                read x
+        fi
 fi
