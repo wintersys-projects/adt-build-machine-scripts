e733f21bbfc5ff933a30ecf99d021cc85ffef4a8
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-06-03 11:06:35 +0100

    Update ConfigureNativeFirewall.sh

--- providerscripts/security/firewall/ConfigureNativeFirewall.sh
+++ providerscripts/security/firewall/ConfigureNativeFirewall.sh
@@ -12,23 +12,24 @@ AUTHENTICATION_SERVER="`${BUILD_HOME}/helperscripts/GetVariableValue.sh AUTHENTI
 BUILD_MACHINE_VPC="`${BUILD_HOME}/helperscripts/GetVariableValue.sh BUILD_MACHINE_VPC`"
 build_machine_ip="`${BUILD_HOME}/helperscripts/GetBuildMachineIP.sh`"
 
+set -x
+
 if ( [ "${ACTIVE_FIREWALLS}" = "2" ] || [ "${ACTIVE_FIREWALLS}" = "3" ] )
 then
         if ( [ "${CLOUDHOST}" = "linode" ] )
         then
-                ${BUILD_HOME}/providerscripts/security/firewall/GetProxyDNSIPs.sh
+                all_dns_proxy_ips="`${BUILD_HOME}/providerscripts/security/firewall/GetProxyDNSIPs.sh`"
 
                 ruleset=""
                 rule_vpc='{"addresses":{"ipv4":["'${VPC_IP_RANGE}'"]},"action":"ACCEPT","protocol":"TCP","ports":"1-65535"}'
                 rule_build_machine='{"addresses":{"ipv4":["'${build_machine_ip}/32'"]},"action":"ACCEPT","protocol":"TCP","ports":"'${SSH_PORT}'"}'
                 rule_icmp='{"addresses":{"ipv4":["0.0.0.0/0"]},"action":"ACCEPT","protocol":"ICMP"}'
-                rule_ssl='{"addresses":{"ipv4":["0.0.0.0/0"]},"action":"ACCEPT","protocol":"TCP","ports":"443"}'
 
-                if ( [ "${alldnsproxyips}" = "" ] )
+                if ( [ "${all_dns_proxy_ips}" = "" ] )
                 then
                         rule_ssl='{"addresses":{"ipv4":["0.0.0.0/0"]},"action":"ACCEPT","protocol":"TCP","ports":"443"}'
                 else
-                        rule_ssl='{"addresses":{"ipv4":['${alldnsproxyips}']},"action":"ACCEPT","protocol":"TCP","ports":"443"}'
+                        rule_ssl='{"addresses":{"ipv4":['${all_dns_proxy_ips}']},"action":"ACCEPT","protocol":"TCP","ports":"443"}'
                 fi
 
                 firewall_id="`/usr/local/bin/linode-cli --json firewalls list | /usr/bin/jq -r '.[] | select (.label | contains ("'${firewall_name}'")) |  select (.label | endswith ("'-${BUILD_IDENTIFIER}'")).id'`"
@@ -37,7 +38,7 @@ then
                 then
                         firewall_id="`/usr/local/bin/linode-cli firewalls create --json --label "${firewall_name}-${BUILD_IDENTIFIER}" --rules.inbound_policy DROP   --rules.outbound_policy ACCEPT | /usr/bin/jq -r '.[].id'`"
                 else
-                        /usr/local/bin/linode-cli firewalls rules-update --inbound '[]' --outbound '[]' --inbound_policy DROP --outbound_policy ACCEPT ${firewall_id}
+                         /usr/local/bin/linode-cli firewalls rules-update --inbound '[]' --outbound '[]' --inbound_policy DROP --outbound_policy ACCEPT ${firewall_id}
                 fi
 
                 ruleset=""
@@ -53,23 +54,20 @@ then
                         fi
                 fi
 
-                if ( [ "${alldnsproxyips}" = "" ] )
+                if ( ( [ "${REVERSE_PROXY}" != "1" ] && [ "${firewall_name}" = "adt-webserver" ] ) ||  [ "${firewall_name}" = "adt-authenticator" ]  ||  [ "${firewall_name}" = "adt-proxyserver" ] )
                 then
-                        if ( ( [ "${REVERSE_PROXY}" != "1" ] && [ "${firewall_name}" = "adt-webserver" ] ) ||  [ "${firewall_name}" = "adt-authenticator" ]  ||  [ "${firewall_name}" = "adt-proxyserver" ] )
+                        if ( [ "${BUILD_MACHINE_VPC}" = "0" ] )
                         then
-                                if ( [ "${BUILD_MACHINE_VPC}" = "0" ] )
-                                then
-                                        ruleset='['${rule_vpc}','${rule_build_machine}','${rule_icmp}','${rule_ssl}']'
-                                else
-                                        ruleset='['${rule_vpc}','${rule_icmp}','${rule_ssl}']'
-                                fi
+                                ruleset='['${rule_vpc}','${rule_build_machine}','${rule_icmp}','${rule_ssl}']'
                         else
-                                if ( [ "${BUILD_MACHINE_VPC}" = "0" ] )
-                                then
-                                        ruleset='['${rule_vpc}','${rule_build_machine}','${rule_icmp}']'
-                                else
-                                        ruleset='['${rule_vpc}','${rule_icmp}']'
-                                fi
+                                ruleset='['${rule_vpc}','${rule_icmp}','${rule_ssl}']'
+                        fi
+                else
+                        if ( [ "${BUILD_MACHINE_VPC}" = "0" ] )
+                        then
+                                ruleset='['${rule_vpc}','${rule_build_machine}','${rule_icmp}']'
+                        else
+                                ruleset='['${rule_vpc}','${rule_icmp}']'
                         fi
                 fi
 
