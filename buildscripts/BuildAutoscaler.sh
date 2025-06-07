5f2d4e128a0e38bda6873a49c9d181ee50839212
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-06-07 19:37:59 +0100

    Update BuildAutoscaler.sh

--- buildscripts/BuildAutoscaler.sh
+++ buildscripts/BuildAutoscaler.sh
@@ -190,6 +190,17 @@ do
                         done="1"
                 fi
 
+                if ( [ "${BUILD_FROM_BACKUP}" = "1" ] && [ "${done}" = "1" ] )
+                then
+                        if ( [ -f ${BUILD_HOME}/runtimedata/wholemachinebackups/autoscalers/${WEBSITE_URL}/autoscaler_backup.tar.gz ] )
+                        then
+                                status "Copying to the autoscaler type machine the appropriate whole machine backup"
+                                /usr/bin/scp -q -P ${SSH_PORT} -i ${BUILD_KEY} ${OPTIONS} ${BUILD_HOME}/runtimedata/wholemachinebackups/autoscalers/${WEBSITE_URL}/autoscaler_backup.tar.gz ${SERVER_USER}@${as_active_ip}:/tmp
+                        else
+                                status "Failed to locate whole machine backup to build autoscaler from when BUILD_FROM_BACKUP is set to 1"
+                        fi
+                fi
+
                 #If $done != 1 then it means the DB server didn't build correctly and fully, so destroy the machine it was being built on
                 if ( [ "${done}" != "1" ] )
                 then
