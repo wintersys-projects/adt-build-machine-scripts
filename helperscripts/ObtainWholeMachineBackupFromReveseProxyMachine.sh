b0dd0aba1e96fb67dd447fc83ce88d760cf70abb
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-06-08 15:14:15 +0100

    Update ObtainWholeMachineBackupFromReveseProxyMachine.sh

--- helperscripts/ObtainWholeMachineBackupFromReveseProxyMachine.sh
+++ helperscripts/ObtainWholeMachineBackupFromReveseProxyMachine.sh
@@ -118,6 +118,11 @@ else
         ALGORITHM="`${BUILD_HOME}/helperscripts/GetVariableValue.sh ALGORITHM`"
 fi
 
+/bin/echo "About to attempt a whole machine backup of an reverse proxy class machine. Press <enter> to have the backup process begin"
+read x
+
+/usr/bin/ssh -o ConnectTimeout=5 -o ConnectionAttempts=2 -o UserKnownHostsFile=${REVERSE_PROXY_PUBLIC_KEYS} -o StrictHostKeyChecking=yes -p ${SSH_PORT} -i ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/keys/id_${ALGORITHM}_AGILE_DEPLOYMENT_BUILD_KEY_${BUILD_IDENTIFIER} ${SERVER_USERNAME}@${RP_IP} "${SUDO} /home/${SERVER_USERNAME}/utilities/housekeeping/GenerateWholeMachineBackup.sh"
+
 if ( [ ! -d ${BUILD_HOME}/runtimedata/wholemachinebackups/${WEBSITE_URL}/reverseproxy ] )
 then
         /bin/mkdir -p ${BUILD_HOME}/runtimedata/wholemachinebackups/${WEBSITE_URL}/reverseproxy
