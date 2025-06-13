3f92f4a1654a93710da901c197880b1be762f596
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-06-13 19:43:50 +0100

    Update InitialiseDatastoreConfig.sh

--- initscripts/InitialiseDatastoreConfig.sh
+++ initscripts/InitialiseDatastoreConfig.sh
@@ -151,7 +151,7 @@ fi
 website_bucket="`/bin/echo ${WEBSITE_URL} | /bin/sed 's/\./-/g'`"
 identifier="`/bin/echo ${SERVER_USER} | /usr/bin/fold -w 4 | /usr/bin/head -n 1 | /usr/bin/tr '[:upper:]' '[:lower:]'`"
 
-for bucket in `${BUILD_HOME}/providerscripts/datastore/ListFromDatastore.sh | /bin/grep "${website_bucket}-config"  | /bin/grep -v "${identifier}" | /usr/bin/awk '{print  $NF}' | /bin/sed 's,s3://,,'`
+for bucket in `${BUILD_HOME}/providerscripts/datastore/ListFromDatastore.sh | /bin/grep "${website_bucket}-config" | /usr/bin/awk '{print  $NF}' | /bin/sed 's,s3://,,'`
 do
         ${BUILD_HOME}/providerscripts/datastore/DeleteFromDatastore.sh ${bucket}/*
         ${BUILD_HOME}/providerscripts/datastore/DeleteDatastore.sh ${bucket}
