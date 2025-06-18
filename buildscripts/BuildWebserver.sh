302a16f964aaf93cddef3a16dd220d2cc70fa80b
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-06-18 12:49:59 +0100

    Update BuildWebserver.sh

--- buildscripts/BuildWebserver.sh
+++ buildscripts/BuildWebserver.sh
@@ -205,7 +205,7 @@ do
 
   		probe_attempts="600"
 
-		if ( [ "`/bin/grep "^${WEBSERVER_CHOICE}:source" ${BUILD_HOME}/builddescriptors/buildstyles.dat`" != "" ] )
+		if ( [ "`/bin/grep "^${WEBSERVER_CHOICE}:source" ${BUILD_HOME}/builddescriptors/buildstyles.dat`" != "" ] && [ "${BUILD_FROM_BACKUP}" != "1" ] )
   		then
         		status "${webserver_name} is compiling from source, it may take a bit longer"
     			probe_attempts="`/usr/bin/expr ${probe_attempts} + 150`"
