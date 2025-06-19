d6fb3d8d39fb9c8733a0b2579a6c3765def2d13e
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-06-19 11:04:31 +0100

    Update ConfigureNativeFirewall.sh

--- providerscripts/security/firewall/ConfigureNativeFirewall.sh
+++ providerscripts/security/firewall/ConfigureNativeFirewall.sh
@@ -16,6 +16,78 @@ build_machine_ip="`${BUILD_HOME}/helperscripts/GetBuildMachineIP.sh`"
 
 if ( [ "${ACTIVE_FIREWALLS}" = "2" ] || [ "${ACTIVE_FIREWALLS}" = "3" ] )
 then
+        if ( [ "${CLOUDHOST}" = "exoscale" ] )
+        then
+                all_dns_proxy_ips="`${BUILD_HOME}/providerscripts/security/firewall/GetProxyDNSIPs.sh`"
+
+                firewall_id="`/usr/bin/exo -O json compute security-group list | /usr/bin/jq -r '.[] | select (.name | contains ("'${firewall_name}'")) |  select (.name | endswith ("'-${BUILD_IDENTIFIER}'")).id'`"
+                if ( [ "${firewall_id}" = "" ] )
+                then
+                        /usr/bin/exo compute security-group create "${firewall_name}-${BUILD_IDENTIFIER}"
+                else
+                        rules=""
+                        rules="`/usr/bin/exo compute security-group show ${firewall_id} -O json | /usr/bin/jq -r '.ingress_rules[].id'`"
+                        rules="${rules} `/usr/bin/exo compute security-group show ${firewall_id} -O json | /usr/bin/jq -r '.egress_rules[].id'`"
+                        for ruleid in ${rules}
+                        do
+                                /usr/bin/yes | /usr/bin/exo compute security-group rule delete ${firewall_id} ${ruleid}
+                        done
+                fi
+
+                if ( [ "${firewall_name}" = "adt-autoscaler" ] )
+                then
+                        if ( [ "${BUILD_MACHINE_VPC}" = "0" ] )
+                        then
+                                /usr/bin/exo compute security-group rule add adt-autoscaler-${BUILD_IDENTIFIER} --network ${build_machine_ip}/32 --port ${SSH_PORT}
+                        fi
+                        /usr/bin/exo compute security-group rule add adt-autoscaler-${BUILD_IDENTIFIER} --network ${VPC_IP_RANGE} --port ${SSH_PORT}
+                        /usr/bin/exo compute security-group rule add adt-autoscaler-${BUILD_IDENTIFIER} --protocol icmp --network 0.0.0.0/0 --icmp-code 0 --icmp-type 8
+                fi
+
+                if ( [ "${firewall_name}" = "adt-webserver" ] ||  [ "${firewall_name}" = "adt-authenticator" ]  ||  [ "${firewall_name}" = "adt-proxyserver" ] )
+                then
+                        if ( [ "${BUILD_MACHINE_VPC}" = "0" ] )
+                        then
+                                /usr/bin/exo compute security-group rule add ${firewall_name}-${BUILD_IDENTIFIER} --network ${build_machine_ip}/32 --port ${SSH_PORT}
+                        fi
+
+                        /usr/bin/exo compute security-group rule add ${firewall_name}-${BUILD_IDENTIFIER} --protocol icmp --network 0.0.0.0/0 --icmp-code 0 --icmp-type 8
+                        /usr/bin/exo compute security-group rule add ${firewall_name}-${BUILD_IDENTIFIER} --network ${VPC_IP_RANGE} --port ${SSH_PORT}
+
+
+                        if ( [ "${all_dns_proxy_ips}" != "" ] || [ "${firewall_name}" = "adt-authenticator" ] )
+                        then
+                                if ( ( [ "${NO_REVERSE_PROXY}" = "0" ] && [ "${firewall_name}" = "adt-webserver" ] ) || [ "${firewall_name}" = "adt-proxyserver" ] )
+                                then
+                                        all_dns_proxy_ips="`/bin/echo ${all_dns_proxy_ips} | /bin/sed 's/,/ /g' | /bin/sed 's/^"//g' | /bin/sed 's/"$//g'`"
+                                        for ip in ${all_dns_proxy_ips}
+                                        do
+                                                /usr/bin/exo compute security-group rule add ${firewall_name}-${BUILD_IDENTIFIER} --network ${ip} --port 443
+                                        done
+                                fi
+                                /usr/bin/exo compute security-group rule add ${firewall_name}-${BUILD_IDENTIFIER} --network ${VPC_IP_RANGE} --port 443
+                        else
+                                /usr/bin/exo compute security-group rule add ${firewall_name}-${BUILD_IDENTIFIER} --network 0.0.0.0/0 --port 443
+                        fi
+
+                fi
+
+                if ( [ "${firewall_name}" = "adt-database" ] )
+                then
+                        if ( [ "${BUILD_MACHINE_VPC}" = "0" ] )
+                        then
+                                /usr/bin/exo compute security-group rule add adt-database-${BUILD_IDENTIFIER} --network ${build_machine_ip}/32 --port ${SSH_PORT}
+                                /usr/bin/exo compute security-group rule add adt-database-${BUILD_IDENTIFIER} --network ${build_machine_ip}/32 --port ${DB_PORT}
+                        fi
+
+                        /usr/bin/exo compute security-group rule add adt-database-${BUILD_IDENTIFIER} --protocol icmp --network 0.0.0.0/0 --icmp-code 0 --icmp-type 8
+                        /usr/bin/exo compute security-group rule add adt-database-${BUILD_IDENTIFIER} --network ${VPC_IP_RANGE} --port ${SSH_PORT}
+                        /usr/bin/exo compute security-group rule add adt-database-${BUILD_IDENTIFIER} --network ${VPC_IP_RANGE} --port ${DB_PORT}
+                fi
+
+                /bin/echo "ADT_FIREWALL_ID:${firewall_name}-${BUILD_IDENTIFIER}"
+        fi
+        
         if ( [ "${CLOUDHOST}" = "linode" ] )
         then
                 all_dns_proxy_ips="`${BUILD_HOME}/providerscripts/security/firewall/GetProxyDNSIPs.sh`"
