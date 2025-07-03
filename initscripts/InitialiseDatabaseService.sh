ae55a2042eb9b4a033eefd45cea57d923c38a543
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-07-03 11:53:53 +0100

    Update InitialiseDatabaseService.sh

--- initscripts/InitialiseDatabaseService.sh
+++ initscripts/InitialiseDatabaseService.sh
@@ -548,21 +548,21 @@ else
         DB_IDENTIFIER="self-managed"
 fi
 
-if ( [ "${BUILD_FROM_SNAPSHOT}" = "1" ] && [ -f ${BUILD_HOME}/runtimedata/wholemachinebackups/${WEBSITE_URL}/snapshots/db_credentials.dat ] )
+if ( [ "${BUILD_FROM_SNAPSHOT}" = "1" ] && [ -f ${BUILD_HOME}/runtimedata/wholemachinesnapshots/${WEBSITE_URL}/snapshots/db_credentials.dat ] )
 then
-        DB_USERNAME="`/bin/grep USERNAME ${BUILD_HOME}/runtimedata/wholemachinebackups/${WEBSITE_URL}/snapshots/db_credentials.dat | /usr/bin/awk -F':' '{print $NF}'`"
-        DB_PASSWORD="`/bin/grep PASSWORD ${BUILD_HOME}/runtimedata/wholemachinebackups/${WEBSITE_URL}/snapshots/db_credentials.dat | /usr/bin/awk -F':' '{print $NF}'`"
-        DB_NAME="`/bin/grep DBNAME ${BUILD_HOME}/runtimedata/wholemachinebackups/${WEBSITE_URL}/snapshots/db_credentials.dat | /usr/bin/awk -F':' '{print $NF}'`"
+        DB_USERNAME="`/bin/grep USERNAME ${BUILD_HOME}/runtimedata/wholemachinesnapshots/${WEBSITE_URL}/snapshots/db_credentials.dat | /usr/bin/awk -F':' '{print $NF}'`"
+        DB_PASSWORD="`/bin/grep PASSWORD ${BUILD_HOME}/runtimedata/wholemachinesnapshots/${WEBSITE_URL}/snapshots/db_credentials.dat | /usr/bin/awk -F':' '{print $NF}'`"
+        DB_NAME="`/bin/grep DBNAME ${BUILD_HOME}/runtimedata/wholemachinesnapshots/${WEBSITE_URL}/snapshots/db_credentials.dat | /usr/bin/awk -F':' '{print $NF}'`"
 fi
 
-if ( [ ! -d ${BUILD_HOME}/runtimedata/wholemachinebackups/${WEBSITE_URL}/snapshots ] )
+if ( [ ! -d ${BUILD_HOME}/runtimedata/wholemachinesnapshots/${WEBSITE_URL}/snapshots ] )
 then
-        /bin/mkdir -p ${BUILD_HOME}/runtimedata/wholemachinebackups/${WEBSITE_URL}/snapshots
+        /bin/mkdir -p ${BUILD_HOME}/runtimedata/wholemachinesnapshots/${WEBSITE_URL}/snapshots
 fi
 
-/bin/echo "USERNAME:${DB_USERNAME}" > ${BUILD_HOME}/runtimedata/wholemachinebackups/${WEBSITE_URL}/snapshots/db_credentials.dat.candidate
-/bin/echo "PASSWORD:${DB_PASSWORD}" >> ${BUILD_HOME}/runtimedata/wholemachinebackups/${WEBSITE_URL}/snapshots/db_credentials.dat.candidate
-/bin/echo "DBNAME:${DB_NAME}" >> ${BUILD_HOME}/runtimedata/wholemachinebackups/${WEBSITE_URL}/snapshots/db_credentials.dat.candidate
+/bin/echo "USERNAME:${DB_USERNAME}" > ${BUILD_HOME}/runtimedata/wholemachinesnapshots/${WEBSITE_URL}/snapshots/db_credentials.dat.candidate
+/bin/echo "PASSWORD:${DB_PASSWORD}" >> ${BUILD_HOME}/runtimedata/wholemachinesnapshots/${WEBSITE_URL}/snapshots/db_credentials.dat.candidate
+/bin/echo "DBNAME:${DB_NAME}" >> ${BUILD_HOME}/runtimedata/wholemachinesnapshots/${WEBSITE_URL}/snapshots/db_credentials.dat.candidate
 
 
 #Persist our credentials to the file system to be used at will      
