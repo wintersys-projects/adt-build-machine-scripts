f4929189caa394961de8e49e7ef682cd9394e390
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-07-03 15:29:48 +0100

    Update ExpeditedAgileDeploymentToolkit.sh

--- ExpeditedAgileDeploymentToolkit.sh
+++ ExpeditedAgileDeploymentToolkit.sh
@@ -90,8 +90,8 @@ end_it_all() {
 
 end_it_all &
 
-trap '/bin/sleep 2;/usr/bin/pwd' EXIT
-trap '/bin/touch /tmp/END_IT_ALL_USER;exit' INT
+trap '/bin/sleep 4; /bin/echo "Cleaned up the place"; /usr/bin/pwd' EXIT
+trap '/bin/touch /tmp/END_IT_ALL_USER; exit' INT
 
 #Set up the intial logging  output. This is where the logging messages will be stored when they occur before
 #the main logging configuration has been set up. There is an output log for stdout and and error log for stderr
