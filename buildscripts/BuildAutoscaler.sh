116b7769ade081557d228957ee4a1ec7d33a9da9
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-07-03 11:36:40 +0100

    Update BuildAutoscaler.sh

--- buildscripts/BuildAutoscaler.sh
+++ buildscripts/BuildAutoscaler.sh
@@ -209,23 +209,6 @@ do
                         done="1"
                 fi
 
-                if ( [ "${BUILD_FROM_BACKUP}" = "1" ] && [ "${done}" = "1" ] )
-                then
-                        if ( [ -f ${BUILD_HOME}/runtimedata/wholemachinebackups/${WEBSITE_URL}/autoscaler/autoscaler_backup.tar ] && [ -f ${BUILD_HOME}/runtimedata/wholemachinebackups/${WEBSITE_URL}/autoscaler/autoscaler_hidden.tar ] )
-                        then
-                                status "Copying the appropriate whole machine backup to the autoscaler machine"
-                                /usr/bin/scp -q -P ${SSH_PORT} -i ${BUILD_KEY} ${OPTIONS} ${BUILD_HOME}/runtimedata/wholemachinebackups/${WEBSITE_URL}/autoscaler/autoscaler_backup.tar ${SERVER_USER}@${as_active_ip}:/tmp
-                                status "Extracting the whole machine backup onto the autoscaler machine"
-                                /usr/bin/ssh -q -p ${SSH_PORT} -i ${BUILD_KEY} ${OPTIONS} ${SERVER_USER}@${as_active_ip} "${SUDO} /usr/bin/tar xvf /tmp/autoscaler_backup.tar --keep-newer-files -C /"
-                                /usr/bin/scp -q -P ${SSH_PORT} -i ${BUILD_KEY} ${OPTIONS} ${BUILD_HOME}/runtimedata/wholemachinebackups/${WEBSITE_URL}/autoscaler/autoscaler_hidden.tar ${SERVER_USER}@${as_active_ip}:/tmp
-                                /usr/bin/ssh -q -p ${SSH_PORT} -i ${BUILD_KEY} ${OPTIONS} ${SERVER_USER}@${as_active_ip} "${SUDO} /usr/bin/tar xvf /tmp/autoscaler_hidden.tar --keep-newer-files -C /"
-                                /usr/bin/ssh -q -p ${SSH_PORT} -i ${BUILD_KEY} ${OPTIONS} ${SERVER_USER}@${as_active_ip} "${SUDO} /home/${SERVER_USER}/utilities/housekeeping/ResetClonedAutoscaler.sh"                        
-                        else
-                                status "Failed to locate whole machine backup to build autoscaler from when BUILD_FROM_BACKUP is set to 1"
-                                /bin/touch /tmp/END_IT_ALL
-                        fi
-                fi
-
                 #If $done != 1 then it means the DB server didn't build correctly and fully, so destroy the machine it was being built on
                 if ( [ "${done}" != "1" ] )
                 then
