6bbbfa2484f448b578b8b571ef02a45049233bea
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-07-06 16:50:53 +0100

    Update ExpeditedAgileDeploymentToolkit.sh

--- ExpeditedAgileDeploymentToolkit.sh
+++ ExpeditedAgileDeploymentToolkit.sh
@@ -418,13 +418,6 @@ then
         ${BUILD_HOME}/initscripts/InitialiseAssetDatastore.sh
 fi
 
-# The native firewal is the firewalling system that is provided by the VPS provider, so we set up the native firewalling here
-#${BUILD_HOME}/providerscripts/security/firewall/SetupNativeFirewall.sh "1"
-
-# Initialise all of our security keys and store the PUBLIC_KEY_ID on the filesystem for reference from anywhere
-#${BUILD_HOME}/initscripts/InitialiseSecurityKeys.sh
-#PUBLIC_KEY_ID="`/bin/cat ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/credentials/PUBLICKEYID`"
-
 # Output any pre-processing messages
 ${BUILD_HOME}/processingscripts/PreProcessingMessages.sh
 # Store our scaling requirements in the datastore (how many webservers to provision)
