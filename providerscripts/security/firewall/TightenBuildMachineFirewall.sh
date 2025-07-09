b29aa6bb88d9b70aa1b08d4e7cbb712050c94f31
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-07-09 23:40:15 +0100

    Update TightenBuildMachineFirewall.sh

--- providerscripts/security/firewall/TightenBuildMachineFirewall.sh
+++ providerscripts/security/firewall/TightenBuildMachineFirewall.sh
@@ -62,7 +62,7 @@ fi
 
 if ( [ "`/usr/bin/crontab -l | /bin/grep TightenBuildMachineFirewall.sh`" = "" ] )
 then
-	/bin/echo "*/1 * * * * ${BUILD_HOME}/providerscripts/security/firewall/TightenBuildMachineFirewall.sh" >> /var/spool/cron/crontabs/root
+	/bin/echo "*/1 * * * * ${BUILD_HOME}/security/firewall/TightenBuildMachineFirewall.sh" >> /var/spool/cron/crontabs/root
 	/usr/bin/crontab -u root /var/spool/cron/crontabs/root 2>/dev/null
 fi
 
