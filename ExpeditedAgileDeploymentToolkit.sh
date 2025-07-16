4b9c3bf8752c2e427afa745b1e86149e085a39d7
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-07-16 10:28:25 +0100

    Update ExpeditedAgileDeploymentToolkit.sh

--- ExpeditedAgileDeploymentToolkit.sh
+++ ExpeditedAgileDeploymentToolkit.sh
@@ -432,9 +432,7 @@ ${BUILD_HOME}/initscripts/InitialiseDatabaseService.sh
 # If we are building an authentication server then that server will require its own SSL certificate, so, generate one here
 if ( [ "${AUTHENTICATION_SERVER}" = "1" ] )
 then
-        WEBSITE_URL="`${BUILD_HOME}/helperscripts/GetVariableValue.sh WEBSITE_URL`"
-        auth_website_url="`/bin/echo ${WEBSITE_URL} | /bin/sed 's/[^.]*./auth./'`"
-        ${BUILD_HOME}/initscripts/InitialiseNewSSLCertificate.sh ${auth_website_url} "yes"
+        ${BUILD_HOME}/initscripts/InitialiseNewSSLCertificate.sh "FILLER" "yes"
 fi
 
 # Generate the SSL certificate that will be used by our webservers
