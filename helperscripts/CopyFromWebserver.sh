b0b217656577f341c8de64d72809208b58d669a8
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-07-16 08:02:10 +0100

    Update CopyFromWebserver.sh

--- helperscripts/CopyFromWebserver.sh
+++ helperscripts/CopyFromWebserver.sh
@@ -86,6 +86,7 @@ fi
 
 /bin/echo "Please enter the name of the build of the server you wish to connect with"
 read BUILD_IDENTIFIER
+/bin/echo "${BUILD_IDENTIFIER}" > ${BUILD_HOME}/runtimedata/ACTIVE_BUILD_IDENTIFIER
 
 if ( [ "${CLOUDHOST}" = "vultr" ] )
 then
@@ -93,7 +94,6 @@ then
 fi
 
 token_to_match="ws-`${BUILD_HOME}/helperscripts/GetVariableValue.sh REGION`-${BUILD_IDENTIFIER}"
-/bin/echo "${BUILD_IDENTIFIER}" > ${BUILD_HOME}/runtimedata/ACTIVE_BUILD_IDENTIFIER
 
 if ( [ -f ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/VPC-ACTIVE ] )
 then
