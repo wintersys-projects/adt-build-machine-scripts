b9bd7077b5f3fb5510bda17c9e0936d6a60e5847
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-07-27 19:41:24 +0100

    Update AuthenticatorCloudInit.sh

--- initscripts/cloud-init/AuthenticatorCloudInit.sh
+++ initscripts/cloud-init/AuthenticatorCloudInit.sh
@@ -47,8 +47,7 @@ BUILD_FROM_SNAPSHOT="`${BUILD_HOME}/helperscripts/GetVariableValue.sh BUILD_FROM
 TIMEZONE="${TIMEZONE_CONTINENT}/${TIMEZONE_CITY}"
 SSH_PORT="`${BUILD_HOME}/helperscripts/GetVariableValue.sh SSH_PORT`"
 AUTHENTICATION_SERVER="`${BUILD_HOME}/helperscripts/GetVariableValue.sh AUTHENTICATION_SERVER`"
-NO_REVERSE_PROXY="`${BUILD_HOME}/helperscripts/GetVariableValue.sh NO_REVERSE_PROXY`"
-WEBSITE_URL="`${BUILD_HOME}/helperscripts/GetVariableValue.sh WEBSITE_URL`"
+
 
 #if ( [ -f ${BUILD_HOME}/runtimedata/wholemachinesnapshots/${WEBSITE_URL}/snapshots/snapshot_ids.dat ] )
 #then
