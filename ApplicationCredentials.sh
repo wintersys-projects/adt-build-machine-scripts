32962792f6e5e7285904b5294f42b48bea6a4339
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-07-27 17:48:07 +0100

    Update ApplicationCredentials.sh

--- ApplicationCredentials.sh
+++ ApplicationCredentials.sh
@@ -1,3 +1,26 @@
+#!/bin/sh
+###############################################################################################
+# Description: You can use this script to get the credentials for your application when you are
+# making a virgin deployment of an application
+# Author Peter Winter
+# Date 22/9/2024
+##############################################################################################
+# License Agreement:
+# This file is part of The Agile Deployment Toolkit.
+# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################################
+###############################################################################################
+#set -x
+
 BUILD_HOME="`/bin/cat /home/buildhome.dat`"
 
 cloudhost="${1}"
