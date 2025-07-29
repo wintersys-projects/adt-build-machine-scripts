5d9a6242387002693b13cb4769dc0324a420c6ec
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-07-29 10:29:32 +0100

    Update SetApplicationConfig.sh

--- application/joomla/SetApplicationConfig.sh
+++ application/joomla/SetApplicationConfig.sh
@@ -19,7 +19,7 @@
 # along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
 #####################################################################################
 #####################################################################################
-set -x
+#set -x
  
 if ( [ ! -f ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/dbp.dat ] )
 then
