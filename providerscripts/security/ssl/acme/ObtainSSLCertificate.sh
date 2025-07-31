fc18cdc47178b8becd3f5adbc42ba637473e7b2b
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-07-31 22:52:27 +0100

    Update ObtainSSLCertificate.sh

--- providerscripts/security/ssl/acme/ObtainSSLCertificate.sh
+++ providerscripts/security/ssl/acme/ObtainSSLCertificate.sh
@@ -6,8 +6,7 @@ status () {
 	/bin/echo "${script_name}: ${1}" | /usr/bin/tee -a /dev/fd/4 2>/dev/null
 }
 
-website_url="${1}"
-
 BUILD_HOME="`/bin/cat /home/buildhome.dat`"
+SYSTEM_FROMEMAIL_ADDRESS="`${BUILD_HOME}/helperscripts/GetVariableValue.sh 'SYSTEM_FROMEMAIL_ADDRESS'`"
 
-${BUILD_HOME}/installscripts/InstallAcme.sh 
+${BUILD_HOME}/installscripts/InstallAcme.sh ${SYSTEM_FROMEMAIL_ADDRESS}
