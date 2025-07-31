f642aace8bccc71a1d9cf8f3c005a6773afacaaf
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-07-31 10:09:13 +0100

    Update InstallAcme.sh

--- installscripts/InstallAcme.sh
+++ installscripts/InstallAcme.sh
@@ -1,5 +1,41 @@
+#!/bin/sh
+######################################################################################################
+# Description: This script will install the acme ssl certificate generation utility
+# Author: Peter Winter
+# Date: 17/01/2017
+#######################################################################################################
+# License Agreement:
+# This file is part of The Agile Deployment Toolkit.
+# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+#######################################################################################################
+#######################################################################################################
+#set -x
+
+if ( [ "${1}" != "" ] )
+then
+	buildos="${1}"
+fi
+
+BUILD_HOME="`/bin/cat /home/buildhome.dat`"
+
+if ( [ "${buildos}" = "ubuntu" ] )
+then
+	/usr/bin/wget -O -  https://get.acme.sh | sh -s email=my@example.com
+fi
+
+if ( [ "${buildos}" = "debian" ] )
+then
+	/usr/bin/wget -O -  https://get.acme.sh | sh -s email=my@example.com
+fi
 
 
 
-
-/usr/bin/wget -O -  https://get.acme.sh | sh -s email=my@example.com
