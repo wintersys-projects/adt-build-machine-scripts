2704afe702d96f62db3459c5adb6458a48f3d693
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-08-12 09:32:28 +0100

    Update ObtainSSLCertificate.sh

--- providerscripts/security/ssl/acme/ObtainSSLCertificate.sh
+++ providerscripts/security/ssl/acme/ObtainSSLCertificate.sh
@@ -100,30 +100,30 @@ then
 
         export CF_Email="${DNS_USERNAME}"
         export CF_Key="${DNS_SECURITY_KEY}"
-        ~/.acme.sh/acme.sh --issue --dns dns_cf -d ${ROOT_DOMAIN} -d "${WEBSITE_URL}" 
+        ~/.acme.sh/acme.sh --issue --dns dns_cf -d "${WEBSITE_URL}" ${server}
 fi
 
 if ( [ "${DNS_CHOICE}" = "digitalocean" ] )
 then
         export DO_API_KEY="${DNS_SECURITY_KEY}" 
-        ~/.acme.sh/acme.sh --issue --dns dns_dgon -d ${ROOT_DOMAIN} -d "${WEBSITE_URL}" ${server}
+        ~/.acme.sh/acme.sh --issue --dns dns_dgon -d "${WEBSITE_URL}" ${server}
 fi
 
 if ( [ "${DNS_CHOICE}" = "exoscale" ] )
 then
         export EXOSCALE_API_KEY="`/bin/echo ${DNS_SECURITY_KEY} | /usr/bin/awk -F':' '{print $1}'`"
         export EXOSCALE_API_SECRET="`/bin/echo ${DNS_SECURITY_KEY} | /usr/bin/awk -F':' '{print $2}'`"
-        ~/.acme.sh/acme.sh --issue --dns dns_exoscale -d ${ROOT_DOMAIN} -d "${WEBSITE_URL}" ${server}
+        ~/.acme.sh/acme.sh --issue --dns dns_exoscale -d "${WEBSITE_URL}" ${server}
 fi
 
 if ( [ "${DNS_CHOICE}" = "linode" ] )
 then
         export LINODE_V4_API_KEY="${DNS_SECURITY_KEY}" 
-        ~/.acme.sh/acme.sh --issue --dns dns_linode_v4 -d ${ROOT_DOMAIN} -d "${WEBSITE_URL}"  ${server} 
+        ~/.acme.sh/acme.sh --issue --dns dns_linode_v4 -d "${WEBSITE_URL}"  ${server} --dnssleep 90
 fi
 
 if ( [ "${DNS_CHOICE}" = "vultr" ] )
 then
         export VULTR_API_KEY="`/bin/cat ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/TOKEN`"
-        ~/.acme.sh/acme.sh --issue --dns dns_vultr -d ${ROOT_DOMAIN} -d "${WEBSITE_URL}" ${server}
+        ~/.acme.sh/acme.sh --issue --dns dns_vultr -d "${WEBSITE_URL}" ${server}
 fi
