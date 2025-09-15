7e4815bf8dc8947dae5fefa6e199678250e2ae92
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-09-15 21:08:15 +0100

    Create exoscale.sh

---
+++ providerscripts/security/ssl/acme/acme-overrides/exoscale.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/bash
+
+# shellcheck disable=SC2034
+dns_exoscale_info='Exoscale.com
+Site: Exoscale.com
+Docs: github.com/acmesh-official/acme.sh/wiki/dnsapi#dns_exoscale
+'
+
+########  Public functions #####################
+
+# Usage: add  _acme-challenge.www.domain.com   "XKrxpRBosdIKFzxW_CT3KLZNf6q0HG9i01zxXp5CPBs"
+# Used to add txt record
+dns_exoscale_add() {
+        fulldomain=$1
+        txtvalue=$2
+
+        _sub_domain="`/bin/echo ${fulldomain} | /usr/bin/cut -d '.' -f -2`"
+        _domain="`/bin/echo ${fulldomain} | /usr/bin/cut -d '.' -f 3-`"
+
+
+        _debug _sub_domain "$_sub_domain"
+        _debug _domain "$_domain"
+
+        _info "Adding record"
+
+        /usr/bin/exo dns add TXT $_domain -c $txtvalue -n $_sub_domain -t 120
+        if ( [ "`/usr/bin/exo dns show $_domain -O json | /usr/bin/jq -r '.[] | select ( .content | contains ( "'$txtvalue'")).id'`" != "" ] )
+        then
+                _info "Added, OK"
+                return 0
+        fi
+
+        _err "Add txt record error."
+        return 1
+
+}
+
+# Usage: fulldomain txtvalue
+# Used to remove the txt record after validation
+dns_exoscale_rm() {
+        fulldomain=$1
+        txtvalue=$2
+
+        _domain="`/bin/echo ${fulldomain} | /usr/bin/cut -d '.' -f 3-`"
+        _domain_id="`/usr/bin/exo dns show $_domain -O json | /usr/bin/jq -r '.[] | select ( .content | contains ( "'$txtvalue'")).id'`" 
+        /usr/bin/exo dns remove $_domain $_domain_id --force
+
+        if ( [ "$?" = "1" ] )
+        then
+                _err "Delete record error."
+                return 1
+        fi
+
+        return 0
+}
