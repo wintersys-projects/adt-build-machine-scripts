e638ed2871b4e5760d54b39d812eaab016557c7f
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-09-20 10:58:22 +0100

    Update digitalocean.sh

--- providerscripts/security/ssl/acme/acme-overrides/digitalocean.sh
+++ providerscripts/security/ssl/acme/acme-overrides/digitalocean.sh
@@ -48,3 +48,37 @@ dns_dgon_add() {
         return 1
 
 }
+
+## Remove the txt record after validation.
+## Usage: fulldomain txtvalue
+## EG: "_acme-challenge.www.other.domain.com" "XKrxpRBosdq0HG9i01zxXp5CPBs"
+dns_dgon_rm() {
+        fulldomain="$(echo "$1" | _lower_case)"
+        txtvalue=$2
+
+        _domain="`/bin/echo ${fulldomain} | /usr/bin/cut -d '.' -f 3-`"
+        _domain_id="`/usr/local/bin/doctl compute domain records list $_domain -o json | /usr/bin/jq -r '.[] | select ( .data | contains ( "'$txtvalue'")).id'`"
+
+    #    DO_API_KEY="${DO_API_KEY:-$(_readaccountconf_mutable DO_API_KEY)}"
+    #    # Check if API Key Exists
+    #    if [ -z "$DO_API_KEY" ]; then
+    #            DO_API_KEY=""
+    #            _err "You did not specify DigitalOcean API key."
+    #            _err "Please export DO_API_KEY and try again."
+    #            return 1
+     #   fi
+
+        if ( [ "`/usr/local/bin/doctl compute domain records list $_domain -o json | /usr/bin/jq -r '.[] | select ( .data | contains ( "'$txtvalue'")).id'`" != "" ] )
+        then
+                /usr/local/bin/doctl compute domain records delete --force $_domain ${_domain_id}
+        fi
+
+        if ( [ "`/usr/local/bin/doctl compute domain records list $_domain -o json | /usr/bin/jq -r '.[] | select ( .data | contains ( "'$txtvalue'")).id'`" = "" ] )
+        then
+                _info "Removed, OK"
+                return 0
+        fi
+
+        _err "Remove txt record error."
+        return 1
+}
