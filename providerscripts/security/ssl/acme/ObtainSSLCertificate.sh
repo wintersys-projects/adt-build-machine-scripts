55d97822b472e32668f30f4eec871398ca12d0c5
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-09-22 13:00:13 +0100

    Update ObtainSSLCertificate.sh

--- providerscripts/security/ssl/acme/ObtainSSLCertificate.sh
+++ providerscripts/security/ssl/acme/ObtainSSLCertificate.sh
@@ -119,7 +119,7 @@ then
 
         export CF_Account_ID="${account_id}"
         export CF_Token="${api_token}"
-        ~/.acme.sh/acme.sh --issue --dns dns_cf -d "${WEBSITE_URL}" --server ${server} 
+        ~/.acme.sh/acme.sh --issue --dns dns_cf -d "${WEBSITE_URL}" --server ${server} --standalone
 fi
 
 
@@ -129,7 +129,7 @@ then
         then
                 /bin/cp ${BUILD_HOME}/providerscripts/security/ssl/acme/acme-overrides/digitalocean.sh ~/.acme.sh/dnsapi/dns_dgon.sh
         fi
-        ~/.acme.sh/acme.sh --issue --dns dns_dgon -d "${WEBSITE_URL}" --server ${server}
+        ~/.acme.sh/acme.sh --issue --dns dns_dgon -d "${WEBSITE_URL}" --server ${server} --standalone
 fi
 
 if ( [ "${DNS_CHOICE}" = "exoscale" ] )
@@ -138,7 +138,7 @@ then
         then
                 /bin/cp ${BUILD_HOME}/providerscripts/security/ssl/acme/acme-overrides/exoscale.sh ~/.acme.sh/dnsapi/dns_exoscale.sh
         fi
-        ~/.acme.sh/acme.sh --issue --dns dns_exoscale -d "${WEBSITE_URL}" --server ${server} 
+        ~/.acme.sh/acme.sh --issue --dns dns_exoscale -d "${WEBSITE_URL}" --server ${server} --standalone 
 fi
 
 if ( [ "${DNS_CHOICE}" = "linode" ] )
@@ -147,7 +147,7 @@ then
         then
                 /bin/cp ${BUILD_HOME}/providerscripts/security/ssl/acme/acme-overrides/linode.sh ~/.acme.sh/dnsapi/dns_linode_v4.sh
         fi
-        ~/.acme.sh/acme.sh --issue --dns dns_linode_v4 -d "${WEBSITE_URL}" --server ${server} 
+        ~/.acme.sh/acme.sh --issue --dns dns_linode_v4 -d "${WEBSITE_URL}" --server ${server} --standalone
 fi
 
 if ( [ "${DNS_CHOICE}" = "vultr" ] )
@@ -156,7 +156,7 @@ then
         then
                 /bin/cp ${BUILD_HOME}/providerscripts/security/ssl/acme/acme-overrides/vultr.sh ~/.acme.sh/dnsapi/dns_vultr.sh
         fi
-        ~/.acme.sh/acme.sh --issue --dns dns_vultr -d "${WEBSITE_URL}" --server ${server} 
+        ~/.acme.sh/acme.sh --issue --dns dns_vultr -d "${WEBSITE_URL}" --server ${server} --standalone
 fi
 
 
