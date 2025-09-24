cc76b73e1be7c5921b64e2d9b8b9652822d172bc
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-09-24 09:48:53 +0100

    Update InitialiseRenewSSLCertificateCron.sh

--- cron/InitialiseRenewSSLCertificateCron.sh
+++ cron/InitialiseRenewSSLCertificateCron.sh
@@ -42,6 +42,8 @@ then
 	then
         status "Please note: the Initialise New SSL Certificate cronjob is currently commented out. If you want to enable SSL certificate renewal please uncomment the line shown below in your crontab"
         status "`/usr/bin/crontab -l | /bin/grep "InitialiseNewSSLCertificate.sh" | /bin/grep "${BUILD_IDENTIFIER}" | bin/grep "${CLOUDHOST}" | /bin/grep "^#"`"
+		status "Press <enter> to continue"
+		read x
 	fi
 fi
         
