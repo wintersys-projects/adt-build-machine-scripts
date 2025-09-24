cb740dcf75091c5755d917ccae764c7f6040a090
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-09-24 09:16:01 +0100

    Update InitialiseUpdateAndUpgradeCron.sh

--- cron/InitialiseUpdateAndUpgradeCron.sh
+++ cron/InitialiseUpdateAndUpgradeCron.sh
@@ -1,3 +1,25 @@
+#!/bin/sh
+#################################################################################
+# Author: Peter Winter
+# Date  : 13/07/2016
+# Description : Initialise update and upgrade software cron
+##################################################################################
+# License Agreement:
+# This file is part of The Agile Deployment Toolkit.
+# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+####################################################################################
+####################################################################################
+#set -x
+
 if ( [ "`/usr/bin/crontab -l | /bin/grep 'apt' | /bin/grep 'update' | /bin/grep 'upgrade'`" = "" ] )
 then
         /bin/echo "45 4 * * * /usr/bin/apt -y -qq update && /usr/bin/apt -y -qq upgrade && /usr/sbin/shutdown -r now" >> /var/spool/cron/crontabs/root
