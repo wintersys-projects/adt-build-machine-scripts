f6c1ecd4de9c8ebe4b1f71dc77beb389efcb5989
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-09-24 09:21:37 +0100

    Update InitialiseRenewSSLCertificateCron.sh

--- cron/InitialiseRenewSSLCertificateCron.sh
+++ cron/InitialiseRenewSSLCertificateCron.sh
@@ -26,6 +26,6 @@ BUILD_IDENTIFIER="`${BUILD_HOME}/helperscripts/GetVariableValue.sh BUILD_IDENTIF
 
 if ( [ "`/usr/bin/crontab -l | /bin/grep InitialiseNewSSLCertificate.sh | /bin/grep -"w ${BUILD_IDENTIFIER}" | /bin/grep -w "${CLOUDHOST}"`" = "" ] )
 then
-        /bin/echo '*/1 * * * * '${BUILD_HOME}'/initscripts/InitialiseNewSSLCertificate.sh "none" "none" "'${BUILD_IDENTIFIER}'" '"${CLOUDHOST}'"'' >> /var/spool/cron/crontabs/root
+        /bin/echo '#*/1 * * * * '${BUILD_HOME}'/initscripts/InitialiseNewSSLCertificate.sh "none" "none" "'${BUILD_IDENTIFIER}'" '"${CLOUDHOST}'"'' >> /var/spool/cron/crontabs/root
         /usr/bin/crontab -u root /var/spool/cron/crontabs/root 2>/dev/null
 fi
