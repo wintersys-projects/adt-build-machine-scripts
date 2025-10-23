f0ea884cef0cea9dee8ac5a22b38a327e3f9f011
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-10-23 18:30:12 +0100

    Update InitialiseBuildMachineFirewallCron.sh

--- cron/InitialiseBuildMachineFirewallCron.sh
+++ cron/InitialiseBuildMachineFirewallCron.sh
@@ -22,8 +22,8 @@
 
 BUILD_HOME="`/bin/cat /home/buildhome.dat`"
 
-if ( [ "`/usr/bin/crontab -l | /bin/grep TightenBuildMachineFirewall.sh`" = "" ] )
+if ( [ "`/usr/bin/crontab -l | /bin/grep AdjustBuildMachineFirewall.sh`" = "" ] )
 then
-        /bin/echo "*/1 * * * * ${BUILD_HOME}/security/firewall/TightenBuildMachineFirewall.sh" >> /var/spool/cron/crontabs/root
+        /bin/echo "*/1 * * * * ${BUILD_HOME}/security/firewall/AdjustBuildMachineFirewall.sh" >> /var/spool/cron/crontabs/root
         /usr/bin/crontab -u root /var/spool/cron/crontabs/root 2>/dev/null
 fi
