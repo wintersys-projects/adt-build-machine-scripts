40644528e5eb7a1800197eaed799d3afd618b45f
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-10-31 09:00:07 +0000

    Update InitiateDevelopment.sh

--- helperscripts/InitiateDevelopment.sh
+++ helperscripts/InitiateDevelopment.sh
@@ -4,6 +4,28 @@ read username
 /bin/echo "Please enter the name of the branch that you wish to clone"
 read branch
 
+/bin/echo "Which provider are your adt infrastructure repositories hosted with?"
+/bin/echo "1) Github 2) Bitbucket 3) GitLab"
+read provider
+
+while ( [ "`/bin/echo 1 2 3 | /bin/grep ${provider}`" = "" ] )
+do
+        /bin/echo "That is not a valid provider, please try again"
+        read provider
+done
+
+if ( [ "${[provider}" = "1" ] )
+then
+        provider="github.com"
+elif ( [ "${[provider}" = "2" ] )
+then
+        provider="bitbucket.org"
+elif ( [ "${[provider}" = "3" ] )
+then
+        provider="gitlab.com"
+fi
+
+
 BUILD_HOME="`/bin/cat /home/buildhome.dat`"
 
 if ( [ ! -d ${BUILD_HOME}/development ] )
@@ -15,7 +37,9 @@ fi
 
 /bin/echo "USERNAME:${username}" > ${BUILD_HOME}/development/.config
 /bin/echo "BRANCH:${branch}" >> ${BUILD_HOME}/development/.config
+/bin/echo "PROVIDER:${provider}" >> ${BUILD_HOME}/development/.config
 
-/usr/bin/git clone  -b ${branch} --single-branch https://github.com/${username}/adt-autoscaler-scripts.git ${BUILD_HOME}/development/autoscaler
-/usr/bin/git clone  -b ${branch} --single-branch https://github.com/${username}/adt-webserver-scripts.git ${BUILD_HOME}/development/webserver
-/usr/bin/git clone  -b ${branch} --single-branch https://github.com/${username}/adt-database-scripts.git ${BUILD_HOME}/development/database
+
+/usr/bin/git clone  -b ${branch} --single-branch https://${provider}/${username}/adt-autoscaler-scripts.git ${BUILD_HOME}/development/autoscaler
+/usr/bin/git clone  -b ${branch} --single-branch https://${provider}/${username}/adt-webserver-scripts.git ${BUILD_HOME}/development/webserver
+/usr/bin/git clone  -b ${branch} --single-branch https://${provider}/${username}/adt-database-scripts.git ${BUILD_HOME}/development/database
