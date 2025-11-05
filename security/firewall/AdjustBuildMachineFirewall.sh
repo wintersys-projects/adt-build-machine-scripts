35335b5254755e5d1556eb6fde908c45c9a6e79c
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-11-05 08:16:12 +0000

    Update AdjustBuildMachineFirewall.sh

--- security/firewall/AdjustBuildMachineFirewall.sh
+++ security/firewall/AdjustBuildMachineFirewall.sh
@@ -191,6 +191,8 @@ then
 			/usr/sbin/ip6tables -P INPUT DROP
 			/usr/sbin/ip6tables -P FORWARD DROP
 			/usr/sbin/ip6tables -P OUTPUT DROP
+			/usr/sbin/ip6tables -A INPUT -i lo -j ACCEPT
+			/usr/sbin/ip6tables -A OUTPUT -o lo -j ACCEPT
         	/usr/sbin/iptables-save > /etc/iptables/rules.v4
         	/usr/sbin/ip6tables-save > /etc/iptables/rules.v6
 		fi
