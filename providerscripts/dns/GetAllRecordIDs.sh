7c640506c4d7a0549db8a3fef60eb1019ed9baf3
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-11-06 15:07:05 +0000

    Update GetAllRecordIDs.sh

--- providerscripts/dns/GetAllRecordIDs.sh
+++ providerscripts/dns/GetAllRecordIDs.sh
@@ -52,7 +52,7 @@ dns="${5}"
 
 if ( [ "${dns}" = "digitalocean" ] )
 then
-        /usr/local/bin/doctl compute domain records list ${domainurl} -o json | /usr/bin/jq -r '.[] | select (.type == "A") | select (.name == "'${subdomain}'").id'
+        /usr/local/bin/doctl compute domain records list ${domainurl} --config /root/.config/doctl/dns-do-config.yaml -o json | /usr/bin/jq -r '.[] | select (.type == "A") | select (.name == "'${subdomain}'").id'
 fi
 
 domainurl="`/bin/echo ${2} | /usr/bin/cut -d'.' -f2-`"
