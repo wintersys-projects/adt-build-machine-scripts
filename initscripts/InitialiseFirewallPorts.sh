deff15121dccf78c0bfc9501ea34169c39398f6f
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-11-10 17:16:07 +0000

    Update InitialiseFirewallPorts.sh

--- initscripts/InitialiseFirewallPorts.sh
+++ initscripts/InitialiseFirewallPorts.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 BUILD_HOME="`/bin/cat /home/buildhome.dat`"
-
+SSH_PORT="`${HOME}/utilities/config/ExtractConfigValue.sh 'SSHPORT'`"
+DB_PORT="`${HOME}/utilities/config/ExtractConfigValue.sh 'DBPORT'`"
 
 
 /bin/cp ${BUILD_HOME}/builddescripts/firewallports.dat
