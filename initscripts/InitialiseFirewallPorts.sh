21094def098a652f93b2e0ebc2edd080c192e4dc
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-11-10 17:19:19 +0000

    Update InitialiseFirewallPorts.sh

--- initscripts/InitialiseFirewallPorts.sh
+++ initscripts/InitialiseFirewallPorts.sh
@@ -3,6 +3,15 @@
 BUILD_HOME="`/bin/cat /home/buildhome.dat`"
 SSH_PORT="`${HOME}/utilities/config/ExtractConfigValue.sh 'SSHPORT'`"
 DB_PORT="`${HOME}/utilities/config/ExtractConfigValue.sh 'DBPORT'`"
+CLOUDHOST="`${HOME}/utilities/config/ExtractConfigValue.sh 'CLOUDHOST'`"
+BUILD_IDENTIFIER="`${HOME}/utilities/config/ExtractConfigValue.sh 'BUILD_IDENTIFIER'`"
 
 
-/bin/cp ${BUILD_HOME}/builddescripts/firewallports.dat
+
+if ( [ -f ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER} ] )
+then
+  /bin/cp ${BUILD_HOME}/builddescripts/firewallports.dat ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/firewallports.dat
+  /bin/sed -i "s/SSH_PORT/${SSH_PORT}/g" ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/firewallports.dat
+  /bin/sed -i "s/DB_PORT/${DB_PORT}/g" ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/firewallports.dat
+fi
+
