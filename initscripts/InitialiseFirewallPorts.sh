bd29b9d594a4c65c0ae9da0b94133682ca2e6b90
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-11-10 17:42:03 +0000

    Update InitialiseFirewallPorts.sh

--- initscripts/InitialiseFirewallPorts.sh
+++ initscripts/InitialiseFirewallPorts.sh
@@ -5,7 +5,7 @@ SSH_PORT="`${BUILD_HOME}/helperscripts/GetVariableValue.sh SSH_PORT`"
 DB_PORT="`${BUILD_HOME}/helperscripts/GetVariableValue.sh DB_PORT`"
 CLOUDHOST="`${BUILD_HOME}/helperscripts/GetVariableValue.sh CLOUDHOST`"
 BUILD_IDENTIFIER="`${BUILD_HOME}/helperscripts/GetVariableValue.sh BUILD_IDENTIFIER`"
-
+BUILD_MACHINE_IP="`${BUILD_HOME}/helperscripts/GetBuildMachineIP.sh`"
 
 if ( [ -f ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/firewallports.dat ] )
 then
@@ -17,5 +17,7 @@ then
   /bin/cp ${BUILD_HOME}/builddescripts/firewallports.dat ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/firewallports.dat
   /bin/sed -i "s/SSH_PORT/${SSH_PORT}/g" ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/firewallports.dat
   /bin/sed -i "s/DB_PORT/${DB_PORT}/g" ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/firewallports.dat
+  /bin/sed -i "s/BUILD_MACHINE/${BUILD_MACHINE_IP}/g" ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/firewallports.dat
+
 fi
 
