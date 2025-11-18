9b829dca01a443aa7b9cfc966b80b121ce770209
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-11-18 20:06:22 +0000

    Create DeleteDatastore.sh

---
+++ providerscripts/datastore/DeleteDatastore.sh
@@ -0,0 +1,36 @@
+#!/bin/sh 
+####################################################################################
+# Author: Peter Winter
+# Date :  9/4/2016
+# Description: Delete a file from a bucket in the datastore
+#######################################################################################
+# License Agreement:
+# This file is part of The Agile Deployment Toolkit.
+# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+######################################################################################
+######################################################################################
+#set -x
+
+datastore_to_delete="${1}"
+
+S3_ACCESS_KEY="`${HOME}/utilities/config/ExtractConfigValue.sh 'S3ACCESSKEY'`"
+
+no_tokens="`/bin/echo "${S3_ACCESS_KEY}" | /usr/bin/fgrep -o '|' | /usr/bin/wc -l`"
+no_tokens="`/usr/bin/expr ${no_tokens} + 1`"
+
+count="1"
+
+while ( [ "${count}" -le "${no_tokens}" ] )
+do
+        ${HOME}/providerscripts/datastore/PerformDatastoreDelete.sh ${datastore_to_delete} ${count}
+        count="`/usr/bin/expr ${count} + 1`"
+done
