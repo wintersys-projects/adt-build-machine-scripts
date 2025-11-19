0766ca4d76f1cad74cb7a967476618b392c704cc
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-11-19 12:40:56 +0000

    Update PerformDeleteDatastore.sh

--- providerscripts/datastore/PerformDeleteDatastore.sh
+++ providerscripts/datastore/PerformDeleteDatastore.sh
@@ -20,12 +20,6 @@
 #########################################################################################
 #set -x
 
-status () {
-        /bin/echo "${1}" | /usr/bin/tee /dev/fd/3 2>/dev/null
-        script_name="`/bin/echo ${0} | /usr/bin/awk -F'/' '{print $NF}'`"
-        /bin/echo "${script_name}: ${1}" | /usr/bin/tee -a /dev/fd/4 2>/dev/null
-}
-
 datastore_to_delete="$1"
 count="$2"
 BUILD_HOME="`/bin/cat /home/buildhome.dat`"
