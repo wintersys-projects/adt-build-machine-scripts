c6919bbf9d2fe10ff870698c1775b4dfb3820027
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-11-19 14:34:13 +0000

    Update PerformDeleteFromDatastore.sh

--- providerscripts/datastore/PerformDeleteFromDatastore.sh
+++ providerscripts/datastore/PerformDeleteFromDatastore.sh
@@ -42,7 +42,8 @@ fi
 if ( [ "${datastore_tool}" = "/usr/bin/s3cmd" ] )
 then
         file_to_delete="`/bin/echo ${file_to_delete} | /bin/sed 's/\*$//g'`"
-        datastore_cmd="${datastore_tool} --recursive --force  --config=/root/.s3cfg-${count} del s3://"
+        host_base="`/bin/grep host_base /root/.s3cfg-${count} | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`" 
+        datastore_cmd="${datastore_tool} --recursive --force  --config=/root/.s3cfg-${count} --host=https://${host_base} del s3://"
 elif ( [ "${datastore_tool}" = "/usr/bin/s5cmd" ] )
 then
         host_base="`/bin/grep host_base /root/.s5cfg-${count} | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`"
