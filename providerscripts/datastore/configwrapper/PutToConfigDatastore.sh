6ade826518421569fd7056cbc8f1bb67252b9d8c
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-11-19 16:18:42 +0000

    Update PutToConfigDatastore.sh

--- providerscripts/datastore/configwrapper/PutToConfigDatastore.sh
+++ providerscripts/datastore/configwrapper/PutToConfigDatastore.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-####################################################################################
+#########################################################################################
 # Author: Peter Winter
-# Date :  24/02/2022
-# Description: This will put a particular file to the configuration datastore
-#######################################################################################
+# Date :  9/4/2016
+# Description: Get a file from a bucket in the datastore
+#########################################################################################
 # License Agreement:
 # This file is part of The Agile Deployment Toolkit.
 # The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
@@ -16,26 +16,23 @@
 # GNU General Public License for more details.
 # You should have received a copy of the GNU General Public License
 # along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
-######################################################################################
-######################################################################################
-#set -x
-
-status () {
-	/bin/echo "${1}" | /usr/bin/tee /dev/fd/3 2>/dev/null
-	script_name="`/bin/echo ${0} | /usr/bin/awk -F'/' '{print $NF}'`"
-	/bin/echo "${script_name}: ${1}" | /usr/bin/tee -a /dev/fd/4 2>/dev/null
-}
+#########################################################################################
+#########################################################################################
+set -x
 
 file_to_put="${1}"
 place_to_put="${2}"
 
 BUILD_HOME="`/bin/cat /home/buildhome.dat`"
-CLOUDHOST="`/bin/cat ${BUILD_HOME}/runtimedata/BUILD_MACHINE_CLOUDHOST`"
-BUILD_IDENTIFIER="`/bin/cat ${BUILD_HOME}/runtimedata/ACTIVE_BUILD_IDENTIFIER`"
+S3_HOST_BASE="`${BUILD_HOME}/helperscripts/GetVariableValue.sh S3_HOST_BASE`"
+CLOUDHOST="`${BUILD_HOME}/helperscripts/GetVariableValue.sh CLOUDHOST`"
+BUILD_IDENTIFIER="`${BUILD_HOME}/helperscripts/GetVariableValue.sh BUILD_IDENTIFIER`"
 SERVER_USER="`/bin/cat ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/credentials/SERVERUSER`"
 TOKEN="`/bin/echo ${SERVER_USER} | /usr/bin/fold -w 4 | /usr/bin/head -n 1 | /usr/bin/tr '[:upper:]' '[:lower:]'`"
 WEBSITE_URL="`${BUILD_HOME}/helperscripts/GetVariableValue.sh WEBSITE_URL`"
-configbucket="`/bin/echo "${WEBSITE_URL}"-config | /bin/sed 's/\./-/g'`-${TOKEN}"
+config_bucket="`/bin/echo "${WEBSITE_URL}"-config | /bin/sed 's/\./-/g'`-${TOKEN}"
+
+datastore_region="`/bin/echo "${S3_HOST_BASE}" | /bin/sed 's/|/ /g' | /usr/bin/awk '{print $1}' | /bin/sed -E 's/(.digitaloceanspaces.com|sos-|.exo.io|.linodeobjects.com|.vultrobjects.com)//g'`"
 datastore_tool=""
 
 if ( [ "`/bin/grep "^DATASTORETOOL:*" ${BUILD_HOME}/builddescriptors/buildstyles.dat | /bin/grep s3cmd`" != "" ] )
@@ -44,60 +41,71 @@ then
 elif ( [ "`/bin/grep "^DATASTORETOOL:*" ${BUILD_HOME}/builddescriptors/buildstyles.dat | /bin/grep s5cmd`" != "" ] )
 then
         datastore_tool="/usr/bin/s5cmd"
+elif ( [ "`/bin/grep "^DATASTORETOOL:*" ${BUILD_HOME}/builddescriptors/buildstyles.dat | /bin/grep rclone`" != "" ] )
+then
+        datastore_tool="/usr/bin/rclone"
 fi
 
 if ( [ "${datastore_tool}" = "/usr/bin/s3cmd" ] )
 then
-	datastore_cmd="${datastore_tool} --force --recursive --multipart-chunk-size-mb=5 put "
+        host_base="`/bin/grep host_base /root/.s3cfg-1 | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`" 
+        datastore_cmd="${datastore_tool} --config=/root/.s3cfg-1 --force -host=https://${host_base} put "
+        bucket_prefix="s3://"
 elif ( [ "${datastore_tool}" = "/usr/bin/s5cmd" ] )
 then
-	host_base="`/bin/grep host_base /root/.s5cfg | /bin/grep host_base | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`" 
-	datastore_cmd="${datastore_tool} --credentials-file /root/.s5cfg --endpoint-url https://${host_base} cp "
+        host_base="`/bin/grep host_base /root/.s5cfg-1 | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`" 
+        datastore_cmd="${datastore_tool} --credentials-file /root/.s5cfg-1 --endpoint-url https://${host_base} cp "
+        bucket_prefix="s3://"
+elif ( [ "${datastore_tool}" = "/usr/bin/rclone" ] )
+then
+        config_file="`/bin/grep -H ${datastore_region} /root/.config/rclone/rclone.conf-1 | /usr/bin/awk -F':' '{print $1}'`"
+        datastore_cmd="${datastore_tool} --config /root/.config/rclone/rclone.conf-1 copy "
+        bucket_prefix="s3:"
 fi
 
 if ( [ ! -f ${file_to_put} ] )
 then
-	path_to_file="`/bin/echo ${file_to_put} | sed 's:/[^/]*$::' | /bin/sed 's,^/,,'`"
-	file="`/bin/echo "${file_to_put}" | /bin/grep "/" | /usr/bin/awk -F'/' '{print $NF}'`"
+        path_to_file="`/bin/echo ${file_to_put} | sed 's:/[^/]*$::' | /bin/sed 's,^/,,'`"
+        file="`/bin/echo "${file_to_put}" | /bin/grep "/" | /usr/bin/awk -F'/' '{print $NF}'`"
 
-	if ( [ "`/bin/echo ${file_to_put} | /bin/grep "/"`" = "" ] )
-	then
- 		file_to_put="/tmp/${file_to_put}"
+        if ( [ "`/bin/echo ${file_to_put} | /bin/grep "/"`" = "" ] )
+        then
+                file_to_put="/tmp/${file_to_put}"
     else
-		file_to_put="/tmp/${path_to_file}/${file}"
-		dir="`/bin/echo ${file_to_put} | /bin/sed 's:/[^/]*$::'`"
-		if ( [ -d ${dir} ] )
-		then
-			/bin/mv ${dir} ${dir}.$$
-		fi
-  		/bin/mkdir -p "${dir}"
-	fi
-	/bin/touch ${file_to_put}
+                file_to_put="/tmp/${path_to_file}/${file}"
+                dir="`/bin/echo ${file_to_put} | /bin/sed 's:/[^/]*$::'`"
+                if ( [ -d ${dir} ] )
+                then
+                        /bin/mv ${dir} ${dir}.$$
+                fi
+                /bin/mkdir -p "${dir}"
+        fi
+        /bin/touch ${file_to_put}
 fi
-        
+
 if ( [ "${place_to_put}" != "" ] )
 then
-	command="${datastore_cmd} ${file_to_put} s3://${configbucket}/${place_to_put}"
+        command="${datastore_cmd} ${file_to_put} ${bucket_prefix}${config_bucket}/${place_to_put}"
 else
-	command="${datastore_cmd} ${file_to_put} s3://${configbucket}"
+        command="${datastore_cmd} ${file_to_put} ${bucket_prefix}${config_bucket}"
 fi
 
 count="0"
 satisfied="0"
 while ( [ "${count}" -lt "5" ] && [ "${satisfied}" = "0" ] )
 do
-	result="`${command} 2>&1 >/dev/null`" 
-	if ( [ "`/bin/echo ${result} | /bin/grep 'ERROR'`" != "" ] )
-	then
-		/bin/echo "An error has occured `/usr/bin/expr ${count} + 1` times in script ${0}"
-		/bin/sleep 5
-		count="`/usr/bin/expr ${count} + 1`"
-	else 
-		satisfied="1"
-	fi
-done 
+        result="`${command} 2>&1 >/dev/null`"
+        if ( [ "`/bin/echo ${result} | /bin/grep 'ERROR'`" != "" ] )
+        then
+                /bin/echo "An error has occured `/usr/bin/expr ${count} + 1` times in script ${0}"
+                /bin/sleep 5
+                count="`/usr/bin/expr ${count} + 1`"
+        else
+                satisfied="1"
+        fi
+done
 
 if ( [ -f /tmp/${file_to_put} ] )
 then
-	/bin/rm /tmp/${file_to_put}
+        /bin/rm /tmp/${file_to_put}
 fi
