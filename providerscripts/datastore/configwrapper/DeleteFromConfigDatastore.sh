99cf1465a20c2f8462f9a5b09bfff0e0b3035894
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-11-19 15:41:46 +0000

    Update DeleteFromConfigDatastore.sh

--- providerscripts/datastore/configwrapper/DeleteFromConfigDatastore.sh
+++ providerscripts/datastore/configwrapper/DeleteFromConfigDatastore.sh
@@ -49,15 +49,15 @@ fi
 if ( [ "${datastore_tool}" = "/usr/bin/s3cmd" ] )
 then
         file_to_delete="`/bin/echo ${file_to_delete} | /bin/sed 's/\*$//g'`"
-        host_base="`/bin/grep host_base /root/.s3cfg-${count} | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`" 
-        datastore_cmd="${datastore_tool} --recursive --force  --config=/root/.s3cfg-${count} --host=https://${host_base} del s3://${config_bucket}/"
+        host_base="`/bin/grep host_base /root/.s3cfg-1 | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`" 
+        datastore_cmd="${datastore_tool} --recursive --force  --config=/root/.s3cfg-1 --host=https://${host_base} del s3://${config_bucket}/"
 elif ( [ "${datastore_tool}" = "/usr/bin/s5cmd" ] )
 then
-        host_base="`/bin/grep host_base /root/.s5cfg-${count} | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`"
-        datastore_cmd="/usr/bin/s5cmd --credentials-file /root/.s5cfg-${count} --endpoint-url https://${host_base} rm s3://${config_bucket}/"
+        host_base="`/bin/grep host_base /root/.s5cfg-1 | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`"
+        datastore_cmd="/usr/bin/s5cmd --credentials-file /root/.s5cfg-1 --endpoint-url https://${host_base} rm s3://${config_bucket}/"
 elif ( [ "${datastore_tool}" = "/usr/bin/rclone" ] )
 then
-        datastore_cmd="${datastore_tool} --config /root/.config/rclone/rclone.conf-${count} purge s3:${config_bucket}/"
+        datastore_cmd="${datastore_tool} --config /root/.config/rclone/rclone.conf-1 purge s3:${config_bucket}/"
 fi
 
 ${datastore_cmd}${file_to_delete}
