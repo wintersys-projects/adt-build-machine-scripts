02e2ee099ef33d8bed57e2c1fb9329501a8a9d7b
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-11-19 18:43:03 +0000

    Update PerformDeleteDatastore.sh

--- providerscripts/datastore/PerformDeleteDatastore.sh
+++ providerscripts/datastore/PerformDeleteDatastore.sh
@@ -2,7 +2,8 @@
 #########################################################################################
 # Author: Peter Winter
 # Date :  9/4/2016
-# Description: Delete a datastore bucket which has to be already empty
+# Description: Actually delte an (empty) datastore bucket for the current S3 provider and using the
+# tool that is currently configured as active
 ##########################################################################################
 # License Agreement:
 # This file is part of The Agile Deployment Toolkit.
