be6305023157d4ca81a6aac66e2eccbfb0074bd5
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-11-19 12:22:24 +0000

    Update SyncDatastore.sh

--- providerscripts/datastore/SyncDatastore.sh
+++ providerscripts/datastore/SyncDatastore.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
-######################################################################################
+#!/bin/sh 
+####################################################################################
 # Author: Peter Winter
 # Date :  9/4/2016
-# Description: Sync one bucket to another
-######################################################################################
+# Description: Delete a file from a bucket in the datastore
+#######################################################################################
 # License Agreement:
 # This file is part of The Agile Deployment Toolkit.
 # The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
@@ -16,46 +16,21 @@
 # GNU General Public License for more details.
 # You should have received a copy of the GNU General Public License
 # along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
-#######################################################################################
-#######################################################################################
+######################################################################################
+######################################################################################
 #set -x
-
-status () {
-	/bin/echo "${1}" | /usr/bin/tee /dev/fd/3 2>/dev/null
-	script_name="`/bin/echo ${0} | /usr/bin/awk -F'/' '{print $NF}'`"
-	/bin/echo "${script_name}: ${1}" | /usr/bin/tee -a /dev/fd/4 2>/dev/null
-}
 
 original_object="$1"
 new_object="$2"
 
 BUILD_HOME="`/bin/cat /home/buildhome.dat`"
+S3_ACCESS_KEY="`${BUILD_HOME}/helperscripts/GetVariableValue.sh S3_ACCESS_KEY`"
+no_tokens="`/bin/echo "${S3_ACCESS_KEY}" | /usr/bin/fgrep -o '|' | /usr/bin/wc -l`"
+no_tokens="`/usr/bin/expr ${no_tokens} + 1`"
+count="1"
 
-datastore_tool=""
-
-if ( [ "`/bin/grep "^DATASTORETOOL:*" ${BUILD_HOME}/builddescriptors/buildstyles.dat | /bin/grep s3cmd`" != "" ] )
-then
-        datastore_tool="/usr/bin/s3cmd"
-elif ( [ "`/bin/grep "^DATASTORETOOL:*" ${BUILD_HOME}/builddescriptors/buildstyles.dat | /bin/grep s5cmd`" != "" ] )
-then
-        datastore_tool="/usr/bin/s5cmd"
-fi
-
-if ( [ "${datastore_tool}" = "/usr/bin/s3cmd" ] )
-then
-	datastore_cmd="${datastore_tool} sync "
-elif ( [ "${datastore_tool}" = "/usr/bin/s5cmd" ] )
-then
-	host_base="`/bin/grep host_base /root/.s5cfg | /bin/grep host_base | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`" 
-	datastore_cmd="${datastore_tool} --credentials-file /root/.s5cfg --endpoint-url https://${host_base} sync "
-fi
-
-if ( [ -d ${original_object} ] || [ -f ${original_object} ] )
-then
-	${datastore_cmd} ${original_object} s3://${new_object} 2>/dev/null
-elif ( [ -d ${new_object} ] || [ -f ${new_object} ] )
-then
-	${datastore_cmdl}  s3://${original_object} ${new_object} 2>/dev/null
-else
-	${datastore_cmd} s3://${original_object} s3://${new_object} 2>/dev/null
-fi
+while ( [ "${count}" -le "${no_tokens}" ] )
+do
+        ${BUILD_HOME}/providerscripts/datastore/PerformSyncDatastore.sh ${original_object} ${new_object} ${count}
+        count="`/usr/bin/expr ${count} + 1`"
+done
