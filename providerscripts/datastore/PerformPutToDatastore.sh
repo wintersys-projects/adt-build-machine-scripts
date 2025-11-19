bfd631e0b6016f849e4f3fb663df96ec94cf12bc
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-11-19 12:01:15 +0000

    Create PerformPutToDatastore.sh

---
+++ providerscripts/datastore/PerformPutToDatastore.sh
@@ -0,0 +1,77 @@
+#!/bin/sh
+#########################################################################################
+# Author: Peter Winter
+# Date :  9/4/2016
+# Description: Delete a file from a bucket in the datastore
+##########################################################################################
+# License Agreement:
+# This file is part of The Agile Deployment Toolkit.
+# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+#########################################################################################
+#########################################################################################
+#set -x
+
+file_to_put="$1"
+datastore_to_put_in="$2"
+count="$3"
+
+BUILD_HOME="`/bin/cat /home/buildhome.dat`"
+S3_HOST_BASE="`${BUILD_HOME}/helperscripts/GetVariableValue.sh S3_HOST_BASE`"
+datastore_region="`/bin/echo "${S3_HOST_BASE}" | /bin/sed 's/|/ /g' | /usr/bin/awk '{print $1}' | /bin/sed -E 's/(.digitaloceanspaces.com|sos-|.exo.io|.linodeobjects.com|.vultrobjects.com)//g'`"
+datastore_tool=""
+
+if ( [ "`/bin/grep "^DATASTORETOOL:*" ${BUILD_HOME}/builddescriptors/buildstyles.dat | /bin/grep s3cmd`" != "" ] )
+then
+        datastore_tool="/usr/bin/s3cmd"
+elif ( [ "`/bin/grep "^DATASTORETOOL:*" ${BUILD_HOME}/builddescriptors/buildstyles.dat | /bin/grep s5cmd`" != "" ] )
+then
+        datastore_tool="/usr/bin/s5cmd"
+elif ( [ "`/bin/grep "^DATASTORETOOL:*" ${BUILD_HOME}/builddescriptors/buildstyles.dat | /bin/grep rclone`" != "" ] )
+then
+        datastore_tool="/usr/bin/rclone"
+fi
+
+if ( [ "${datastore_tool}" = "/usr/bin/s3cmd" ] )
+then
+        datastore_cmd="${datastore_tool} --force --recursive --multipart-chunk-size-mb=5 --config=/root/.s3cfg-${count} put "
+        bucket_prefix="s3://"
+elif ( [ "${datastore_tool}" = "/usr/bin/s5cmd" ] )
+then
+        host_base="`/bin/grep host_base /root/.s5cfg-${count} | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`" 
+        datastore_cmd="${datastore_tool} --credentials-file /root/.s5cfg-${count} --endpoint-url https://${host_base} cp "
+        bucket_prefix="s3://"
+elif ( [ "${datastore_tool}" = "/usr/bin/rclone" ] )
+then
+        datastore_cmd="${datastore_tool} --config /root/.config/rclone/rclone.conf-${count} copy "
+        bucket_prefix="s3:"
+fi
+
+if ( [ ! -f ${file_to_put} ] )
+then
+        file_to_put="/tmp/${file_to_put}"
+        /bin/touch ${file_to_put}
+fi
+
+count="0"
+while ( [ "`${datastore_cmd} ${file_to_put} ${bucket_prefix}${datastore_to_put_in} 2>&1 >/dev/null | /bin/grep "ERROR"`" != "" ] && [ "${count}" -lt "5" ] )
+do
+        /bin/echo "An error has occured `/usr/bin/expr ${count} + 1` times in script ${0}"
+        /bin/sleep 5
+        count="`/usr/bin/expr ${count} + 1`"
+done
+
+file="`/bin/echo ${file_to_put} | /usr/bin/awk -F'/' '{print $NF}'`"
+
+if ( [ "${delete}" = "yes" ] )
+then
+        /bin/rm ${file_to_put}
+fi
