fc86941d151331736e0db7c61482ca359c46c88f
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-11-20 13:48:46 +0000

    Update MountConfigDatastore.sh

--- providerscripts/datastore/configwrapper/MountConfigDatastore.sh
+++ providerscripts/datastore/configwrapper/MountConfigDatastore.sh
@@ -54,7 +54,7 @@ then
 elif ( [ "${datastore_tool}" = "/usr/bin/rclone" ] )
 then
         host_base="`/bin/grep ^endpoint /root/.config/rclone/rclone.conf-1 | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`" 
-        datastore_cmd1="${datastore_tool} --config /root/.config/rclone/rclone.conf-${count} --s3-endpoint ${host_base} mkdir s3:${config_bucket}"
+        datastore_cmd="${datastore_tool} --config /root/.config/rclone/rclone.conf-1 --s3-endpoint ${host_base} mkdir s3:${config_bucket}"
 fi
 
 ${datastore_cmd}
