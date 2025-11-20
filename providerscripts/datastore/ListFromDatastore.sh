35bf80ee1051d49d3ad45a04f430914a647581d2
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-11-20 10:18:02 +0000

    Update ListFromDatastore.sh

--- providerscripts/datastore/ListFromDatastore.sh
+++ providerscripts/datastore/ListFromDatastore.sh
@@ -63,7 +63,7 @@ then
         fi
 elif ( [ "${datastore_tool}" = "/usr/bin/rclone" ] )
 then
-        host_base="`/bin/grep ^endpoint /root/.config/rclone/rclone.conf-1 | /bin/grep "^endpoint" | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`" 
+        host_base="`/bin/grep ^endpoint /root/.config/rclone/rclone.conf-1 | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`" 
         if ( [ "${file_to_list}" = "" ] )
         then
                 datastore_cmd="${datastore_tool} --config /root/.config/rclone/rclone.conf-1  --s3-endpoint ${host_base} lsd s3:"
