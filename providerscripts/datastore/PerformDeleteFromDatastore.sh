2cc7ae7857af374c10172a92d30f2902cea04cbd
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-11-21 16:50:52 +0000

    Update PerformDeleteFromDatastore.sh

--- providerscripts/datastore/PerformDeleteFromDatastore.sh
+++ providerscripts/datastore/PerformDeleteFromDatastore.sh
@@ -49,8 +49,13 @@ then
 elif ( [ "${datastore_tool}" = "/usr/bin/rclone" ] )
 then
         host_base="`/bin/grep ^endpoint /root/.config/rclone/rclone.conf-${count} | /bin/grep "^endpoint" | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`" 
-        datastore_cmd="${datastore_tool} --config /root/.config/rclone/rclone.conf-${count} --s3-endpoint ${host_base} delete s3:"
-        file_to_delete="`/bin/echo ${file_to_delete} | /bin/sed 's/\*$//g'`"
+        include=""
+        if ( [ "${file_to_delete}" != "" ] )
+        then
+                include="--include *${file_to_delete}*"
+        fi
+        datastore_cmd="${datastore_tool} --config /root/.config/rclone/rclone.conf-${count} --s3-endpoint ${host_base} ${include} delete s3:"
+        file_to_delete=""
 fi
 
 ${datastore_cmd}${file_to_delete}
