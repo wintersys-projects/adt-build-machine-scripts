0d8919523bb368ad71792b532ca498190d1d1852
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-11-21 16:09:51 +0000

    Update ListFromDatastore.sh

--- providerscripts/datastore/ListFromDatastore.sh
+++ providerscripts/datastore/ListFromDatastore.sh
@@ -24,7 +24,6 @@
 #set -x
 
 file_to_list="${1}"
-pattern_to_match="${2}"
 
 BUILD_HOME="`/bin/cat /home/buildhome.dat`"
 datastore_tool=""
@@ -65,9 +64,9 @@ then
         host_base="`/bin/grep ^endpoint /root/.config/rclone/rclone.conf-1 | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`" 
         
         include=""
-        if ( [ "${pattern_to_match}" != "" ] )
+        if ( [ "${file_to_list}" != "" ] )
         then
-             include='--include "*'${pattern_to_match}'*"'
+                include='--include "*'${file_to_list}'*"'
         fi
         
         if ( [ "${file_to_list}" = "" ] )
