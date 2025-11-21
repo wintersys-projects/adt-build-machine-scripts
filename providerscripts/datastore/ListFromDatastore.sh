0ecdeee946abaa3a5053ec1730350073045f1327
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-11-21 00:29:36 +0000

    Update ListFromDatastore.sh

--- providerscripts/datastore/ListFromDatastore.sh
+++ providerscripts/datastore/ListFromDatastore.sh
@@ -68,6 +68,7 @@ then
         else
                 datastore_cmd="${datastore_tool} --config /root/.config/rclone/rclone.conf-1  --s3-endpoint ${host_base} ls s3:"
         fi
+        file_to_list="`/bin/echo ${file_to_list} | /bin/sed 's/\*//g'`"
 fi
 
 if ( [ "${file_to_list}" = "" ] )
