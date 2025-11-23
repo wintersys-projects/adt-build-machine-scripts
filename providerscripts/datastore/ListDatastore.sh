449ecfb1570ebfcdda47e19fd654f804e8cf0c80
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-11-23 09:38:58 +0000

    Create ListDatastore.sh

---
+++ providerscripts/datastore/ListDatastore.sh
@@ -0,0 +1,87 @@
+#!/bin/sh
+#########################################################################################
+# Author: Peter Winter
+# Date :  9/4/2016
+# Description: List a file from a bucket in the datastore. The file is obtained from the
+# first s3 bucket in the chain of buckets when replication is being used. In other words
+# the first region listed in S3_HOST_BASE in the template is considered to be the authoritative
+# bucket when listing files
+#########################################################################################
+# License Agreement:
+# This file is part of The Agile Deployment Toolkit.
+# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+#########################################################################################
+#########################################################################################
+#set -x
+
+file_to_list="${1}"
+
+BUILD_HOME="`/bin/cat /home/buildhome.dat`"
+datastore_tool=""
+
+if ( [ "`/bin/grep "^DATASTORETOOL:*" ${BUILD_HOME}/builddescriptors/buildstyles.dat | /bin/grep s3cmd`" != "" ] )
+then
+        datastore_tool="/usr/bin/s3cmd"
+elif ( [ "`/bin/grep "^DATASTORETOOL:*" ${BUILD_HOME}/builddescriptors/buildstyles.dat | /bin/grep s5cmd`" != "" ] )
+then
+        datastore_tool="/usr/bin/s5cmd"
+elif ( [ "`/bin/grep "^DATASTORETOOL:*" ${BUILD_HOME}/builddescriptors/buildstyles.dat | /bin/grep rclone`" != "" ] )
+then
+        datastore_tool="/usr/bin/rclone"
+fi
+
+if ( [ "${datastore_tool}" = "/usr/bin/s3cmd" ] )
+then
+        host_base="`/bin/grep ^host_base /root/.s3cfg-1 | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`"
+
+        if ( [ "${file_to_list}" = "" ] )
+        then
+                datastore_cmd="${datastore_tool} --config=/root/.s3cfg-1 --recursive --host=https://${host_base} ls"
+        else
+                datastore_cmd="${datastore_tool} --config=/root/.s3cfg-1 --recursive --host=https://${host_base} ls s3://"
+        fi
+elif ( [ "${datastore_tool}" = "/usr/bin/s5cmd" ] )
+then
+        host_base="`/bin/grep ^host_base /root/.s5cfg-1 | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`"
+
+        if ( [ "${file_to_list}" = "" ] )
+        then
+                datastore_cmd="${datastore_tool} --credentials-file /root/.s5cfg-1 --endpoint-url https://${host_base} ls"
+        else
+                datastore_cmd="${datastore_tool} --credentials-file /root/.s5cfg-1 --endpoint-url https://${host_base} ls s3://"
+        fi
+elif ( [ "${datastore_tool}" = "/usr/bin/rclone" ] )
+then
+        host_base="`/bin/grep ^endpoint /root/.config/rclone/rclone.conf-1 | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`" 
+
+        include=""
+        if ( [ "${file_to_list}" != "" ] )
+        then
+                include="--include *${file_to_list}*"
+        fi
+
+        path="no"
+        if ( [ "`/bin/echo ${file_to_list} | /bin/grep '\/'`" != "" ] )
+        then
+                path="yes"
+        fi
+
+        datastore_cmd="${datastore_tool} --config /root/.config/rclone/rclone.conf-1 --s3-endpoint ${host_base} ${include} lsd s3:"
+        file_to_list=""
+fi
+
+if ( [ "${path}" = "yes" ] )
+then
+        ${datastore_cmd}${file_to_list}  | /usr/bin/awk '{print $NF}' | /usr/bin/awk -F'/' '{print $NF}' | /bin/sed '/^$/d'
+else
+        ${datastore_cmd}${file_to_list} | /usr/bin/awk '{print $NF}'
+fi
