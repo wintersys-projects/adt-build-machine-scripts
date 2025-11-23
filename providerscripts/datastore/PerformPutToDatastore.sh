7b3c779d56f4a15d4c8c41ed2920af171dfa7ce4
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-11-23 18:54:30 +0000

    Update PerformPutToDatastore.sh

--- providerscripts/datastore/PerformPutToDatastore.sh
+++ providerscripts/datastore/PerformPutToDatastore.sh
@@ -78,6 +78,6 @@ if ( [ "${delete}" = "yes" ] )
 then
         if ( [ -f ${file_to_put} ] )
         then
-                /bin/rm /tmp/${file_to_put}
+                /bin/rm ${file_to_put}
         fi
 fi
