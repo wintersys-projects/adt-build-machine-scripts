5222f6788aded562e13a89877de656e604a2faec
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-11-24 21:11:32 +0000

    Update InitialiseDatastoreConfig.sh

--- initscripts/InitialiseDatastoreConfig.sh
+++ initscripts/InitialiseDatastoreConfig.sh
@@ -276,7 +276,7 @@ if ( [ "${MULTI_REGION}" = "0" ] || ( [ "${MULTI_REGION}" = "1" ] && [ "${PRIMAR
 then
         for bucket in `${BUILD_HOME}/providerscripts/datastore/ListDatastore.sh | /bin/grep "${website_bucket}-config" | grep -Eo "${website_bucket}.*(/|$)" | /usr/bin/uniq`
         do
-                ${BUILD_HOME}/providerscripts/datastore/DeleteFromDatastore.sh ${bucket}/*
+                ${BUILD_HOME}/providerscripts/datastore/DeleteFromDatastore.sh ${bucket}/* "yes"
                 ${BUILD_HOME}/providerscripts/datastore/configwrapper/DeleteConfigDatastore.sh ${bucket}
         done
 fi
