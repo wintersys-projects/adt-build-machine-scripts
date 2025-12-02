04413d10bfd9f37528b3e0d2e774ceaaaec8082f
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-02 12:56:06 +0000

    Update GetOperatingSystemVersion.sh

--- providerscripts/server/GetOperatingSystemVersion.sh
+++ providerscripts/server/GetOperatingSystemVersion.sh
@@ -20,15 +20,11 @@
 ############################################################################################
 #set -x
 
-status () {
-	/bin/echo "${1}" | /usr/bin/tee /dev/fd/3 2>/dev/null
-	script_name="`/bin/echo ${0} | /usr/bin/awk -F'/' '{print $NF}'`"
-	/bin/echo "${script_name}: ${1}" | /usr/bin/tee -a /dev/fd/4 2>/dev/null
-}
-
 cloudhost="${1}"
 buildos="${2}"
 buildos_version="${3}"
+
+BUILD_HOME="`/bin/cat /home/buildhome.dat`"
 
 if ( [ "${cloudhost}" = "digitalocean" ] )
 then
