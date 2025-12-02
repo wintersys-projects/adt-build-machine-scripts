73b504c8a83daf85397d22f6003544437d62e829
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-02 22:56:23 +0000

    Update InstallFail2Ban.sh

--- installscripts/InstallFail2Ban.sh
+++ installscripts/InstallFail2Ban.sh
@@ -55,7 +55,7 @@ then
 		if ( [ "`/bin/grep "^FAIL2BAN:*" ${BUILD_HOME}/builddescriptors/buildstyles.dat | /bin/grep active`" != "" ] )
         then
 			eval ${install_command} fail2ban
-			/bin/cp ${BUILD_HOME}/security/firewall/config/fail2ban.conf  /etc/fail2ban/jail.local
+			/bin/cp ${BUILD_HOME}/security/firewall/config/fail2ban.conf  /etc/fail2ban/jail.d/jail.local
 		fi
 	fi
 fi
