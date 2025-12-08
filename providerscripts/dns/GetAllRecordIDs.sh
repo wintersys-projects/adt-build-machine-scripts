b94e61ae9e07ecb8c70618936f5ff4f8c04b276d
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-08 16:34:46 +0000

    Update GetAllRecordIDs.sh

--- providerscripts/dns/GetAllRecordIDs.sh
+++ providerscripts/dns/GetAllRecordIDs.sh
@@ -35,7 +35,7 @@ dns="${5}"
 
 if ( [ "${dns}" = "cloudflare" ] )
 then
-        if ( [ "`/bin/echo ${credentials} | /bin/grep ':::'`" = "" ] )
+        if ( [ "`/bin/echo ${credentials} | /bin/grep ':::'`" != "" ] )
         then
                 api_token="`/bin/echo ${credentials} | /usr/bin/awk -F':::' '{print $2}'`"
                 /usr/bin/curl -X GET "https://api.cloudflare.com/client/v4/zones/${zoneid}/dns_records?type=A&name=${websiteurl}&page=1&per_page=20&order=type&direction=desc&match=all" --header "Authorization: Bearer ${api_token}" --header "Content-Type: application/json"  | /usr/bin/jq -r '.result[].id'
