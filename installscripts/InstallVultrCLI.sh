abd07b6d73a800182a0fde951d30ca63e5ede0e3
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-21 23:51:00 +0000

    Update InstallVultrCLI.sh

--- installscripts/InstallVultrCLI.sh
+++ installscripts/InstallVultrCLI.sh
@@ -27,22 +27,30 @@ fi
 
 if ( [ "${buildos}" = "ubuntu" ] )
 then 
-    vultr_cli_version="`/usr/bin/curl -L https://api.github.com/repos/vultr/vultr-cli/releases/latest | /usr/bin/jq -r '.name'`"
-    /usr/bin/wget -c https://github.com/vultr/vultr-cli/releases/download/${vultr_cli_version}/vultr-cli_${vultr_cli_version}_linux_amd64.tar.gz -O- | /usr/bin/tar -xz -C /usr/bin
-    /bin/mv /usr/bin/vultr-cli /usr/bin/vultr
-    /bin/chown root:root /usr/bin/vultr
-
-    ${BUILD_HOME}/installscripts/InstallGo.sh ${buildos}
-    go install github.com/vultr/vultr-cli/v3@latest
+    if ( [ "`/bin/grep "^CLOUDCLITOOL:*" ${BUILD_HOME}/builddescriptors/buildstyles.dat | /bin/grep CLOUDCLITOOL:vultr:binary`" != "" ] )
+    then
+        vultr_cli_version="`/usr/bin/curl -L https://api.github.com/repos/vultr/vultr-cli/releases/latest | /usr/bin/jq -r '.name'`"
+        /usr/bin/wget -c https://github.com/vultr/vultr-cli/releases/download/${vultr_cli_version}/vultr-cli_${vultr_cli_version}_linux_amd64.tar.gz -O- | /usr/bin/tar -xz -C /usr/bin
+        /bin/mv /usr/bin/vultr-cli /usr/bin/vultr
+        /bin/chown root:root /usr/bin/vultr
+    elif ( [ "`/bin/grep "^CLOUDCLITOOL:*" ${BUILD_HOME}/builddescriptors/buildstyles.dat | /bin/grep CLOUDCLITOOL:vultr:source`" != "" ] )
+    then
+        ${BUILD_HOME}/installscripts/InstallGo.sh ${buildos}
+        /usr/bin/go install github.com/vultr/vultr-cli/v3@latest
+    fi
 fi
 
 if ( [ "${buildos}" = "debian" ] )
 then
-    vultr_cli_version="`/usr/bin/curl -L https://api.github.com/repos/vultr/vultr-cli/releases/latest | /usr/bin/jq -r '.name'`"
-    /usr/bin/wget -c https://github.com/vultr/vultr-cli/releases/download/${vultr_cli_version}/vultr-cli_${vultr_cli_version}_linux_amd64.tar.gz -O- | /usr/bin/tar -xz -C /usr/bin
-    /bin/mv /usr/bin/vultr-cli /usr/bin/vultr
-    /bin/chown root:root /usr/bin/vultr
-
-    ${BUILD_HOME}/installscripts/InstallGo.sh ${buildos}
-    go install github.com/vultr/vultr-cli/v3@latest
+    if ( [ "`/bin/grep "^CLOUDCLITOOL:*" ${BUILD_HOME}/builddescriptors/buildstyles.dat | /bin/grep CLOUDCLITOOL:vultr:binary`" != "" ] )
+    then
+        vultr_cli_version="`/usr/bin/curl -L https://api.github.com/repos/vultr/vultr-cli/releases/latest | /usr/bin/jq -r '.name'`"
+        /usr/bin/wget -c https://github.com/vultr/vultr-cli/releases/download/${vultr_cli_version}/vultr-cli_${vultr_cli_version}_linux_amd64.tar.gz -O- | /usr/bin/tar -xz -C /usr/bin
+        /bin/mv /usr/bin/vultr-cli /usr/bin/vultr
+        /bin/chown root:root /usr/bin/vultr
+    elif ( [ "`/bin/grep "^CLOUDCLITOOL:*" ${BUILD_HOME}/builddescriptors/buildstyles.dat | /bin/grep CLOUDCLITOOL:vultr:source`" != "" ] )
+    then
+        ${BUILD_HOME}/installscripts/InstallGo.sh ${buildos}
+        /usr/bin/go install github.com/vultr/vultr-cli/v3@latest
+    fi
 fi
