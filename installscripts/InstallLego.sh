96051efd9f8915731479e5286b48d0db7c6f6dac
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-21 21:32:22 +0000

    Update InstallLego.sh

--- installscripts/InstallLego.sh
+++ installscripts/InstallLego.sh
@@ -57,6 +57,7 @@ then
 			eval ${install_command} snapd
 			snap="`/usr/bin/whereis snap | /usr/bin/awk -F':' '{print $NF}' | /usr/bin/awk '{print $1}'`"
 			${snap} install lego
+			${snap} connect lego:dot-lego
 			/usr/bin/ln -s /snap/bin/lego /usr/bin/lego
 		fi
 	fi
@@ -77,6 +78,7 @@ then
 			eval ${install_command} snapd
 			snap="`/usr/bin/whereis snap | /usr/bin/awk -F':' '{print $NF}' | /usr/bin/awk '{print $1}'`"
 			${snap} install lego
+			${snap} connect lego:dot-lego
 			/usr/bin/ln -s /snap/bin/lego /usr/bin/lego
 		fi
 	fi
