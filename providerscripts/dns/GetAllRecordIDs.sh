6361a336123d5c127b5b9ab5950b0120fa76d484
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-22 00:52:26 +0000

    Update GetAllRecordIDs.sh

--- providerscripts/dns/GetAllRecordIDs.sh
+++ providerscripts/dns/GetAllRecordIDs.sh
@@ -73,7 +73,15 @@ dns="${5}"
 
 if ( [ "${dns}" = "linode" ] )
 then
-        export LINODE_CLI_CONFIG=/root/.config/dns-linode-cli
+        linode_config_file="/root/.config/dns-linode-cli"
+        
+        if ( [ -f /root/snap/linode-cli/current/.config/linode-cli ] )
+        then
+             linode_config_file="/root/snap/linode-cli/current/.config/linode-cli"
+        fi
+                        
+        export LINODE_CLI_CONFIG=${linode_config_file}
+        
         domain_id="`/usr/local/bin/linode-cli domains list --no-defaults --json | /usr/bin/jq -r '.[] | select (.domain | contains("'${domain_url}'")).id'`"
         /usr/local/bin/linode-cli domains records-list ${domain_id} --no-defaults --json | /usr/bin/jq -r '.[] | select (.name == "'${subdomain}'").id'
         unset LINODE_CLI_CONFIG
