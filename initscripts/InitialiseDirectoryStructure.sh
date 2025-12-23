cfccd2936f0327d85a24b30f1303b41333553e45
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-23 21:30:56 +0000

    Update InitialiseDirectoryStructure.sh

--- initscripts/InitialiseDirectoryStructure.sh
+++ initscripts/InitialiseDirectoryStructure.sh
@@ -20,12 +20,6 @@
 #########################################################################################
 #set -x
 
-status () {
-	/bin/echo "${1}" | /usr/bin/tee /dev/fd/3 2>/dev/null
-	script_name="`/bin/echo ${0} | /usr/bin/awk -F'/' '{print $NF}'`"
-	/bin/echo "${script_name}: ${1}" | /usr/bin/tee -a /dev/fd/4 2>/dev/null
-}
-
 BUILD_HOME="`/bin/cat /home/buildhome.dat`"
 cloudhost="${1}"
 build_identifier="${2}"
