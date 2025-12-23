23e482fc6d03aa16f6d854c064d6bd4342324cc8
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-23 21:37:56 +0000

    Update ConfigureNativeFirewall.sh

--- providerscripts/security/firewall/exoscale/ConfigureNativeFirewall.sh
+++ providerscripts/security/firewall/exoscale/ConfigureNativeFirewall.sh
@@ -39,12 +39,6 @@ exoscale_firewall_rules ()
         done
 }
 
-status () {
-        /bin/echo "${1}" | /usr/bin/tee /dev/fd/3 2>/dev/null
-        script_name="`/bin/echo ${0} | /usr/bin/awk -F'/' '{print $NF}'`"
-        /bin/echo "${script_name}: ${1}" | /usr/bin/tee -a /dev/fd/4 2>/dev/null
-}
-
 firewall_name="${1}"
 
 BUILD_HOME="`/bin/cat /home/buildhome.dat`" 
