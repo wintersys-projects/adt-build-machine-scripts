eb30dcc7673d027ef7acbf241665f1467042a130
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-23 21:39:12 +0000

    Update OverrideTemplate.sh

--- templatedconfigurations/OverrideTemplate.sh
+++ templatedconfigurations/OverrideTemplate.sh
@@ -20,12 +20,6 @@
 ####################################################################################
 #set -x
 
-status () {
-	/bin/echo "${1}" | /usr/bin/tee /dev/fd/3 2>/dev/null
-	script_name="`/bin/echo ${0} | /usr/bin/awk -F'/' '{print $NF}'`"
-	/bin/echo "${script_name}: ${1}" | /usr/bin/tee -a /dev/fd/4 2>/dev/null
-}
-
 BUILD_HOME="`/bin/cat /home/buildhome.dat`"
 CLOUDHOST="`${BUILD_HOME}/helperscripts/GetVariableValue.sh CLOUDHOST`"
 
