04c2932f2b19778492f12dc2c49ddca41c20a020
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-23 21:31:37 +0000

    Update InitialiseCloudInit.sh

--- initscripts/InitialiseCloudInit.sh
+++ initscripts/InitialiseCloudInit.sh
@@ -23,12 +23,6 @@
 ####################################################################################
 #set -x
 
-status () {
-	/bin/echo "${1}" | /usr/bin/tee /dev/fd/3 2>/dev/null
-	script_name="`/bin/echo ${0} | /usr/bin/awk -F'/' '{print $NF}'`"
-	/bin/echo "${script_name}: ${1}" | /usr/bin/tee -a /dev/fd/4 2>/dev/null
-}
-
 BUILD_HOME="`/bin/cat /home/buildhome.dat`"
 NO_AUTHENTICATORS="`${BUILD_HOME}/helperscripts/GetVariableValue.sh NO_AUTHENTICATORS`"
 PRODUCTION="`${BUILD_HOME}/helperscripts/GetVariableValue.sh PRODUCTION`"
