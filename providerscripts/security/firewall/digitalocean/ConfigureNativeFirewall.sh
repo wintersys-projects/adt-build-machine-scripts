def3e3f806ca2327b08682fe21ab4e0e33fdbd02
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-23 21:37:43 +0000

    Update ConfigureNativeFirewall.sh

--- providerscripts/security/firewall/digitalocean/ConfigureNativeFirewall.sh
+++ providerscripts/security/firewall/digitalocean/ConfigureNativeFirewall.sh
@@ -41,13 +41,6 @@ digitalocean_firewall_rules ()
         /bin/echo "${firewall_rules}"
 }
 
-
-status () {
-        /bin/echo "${1}" | /usr/bin/tee /dev/fd/3 2>/dev/null
-        script_name="`/bin/echo ${0} | /usr/bin/awk -F'/' '{print $NF}'`"
-        /bin/echo "${script_name}: ${1}" | /usr/bin/tee -a /dev/fd/4 2>/dev/null
-}
-
 firewall_name="${1}"
 
 BUILD_HOME="`/bin/cat /home/buildhome.dat`" 
