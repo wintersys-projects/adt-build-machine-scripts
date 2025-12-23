b0312cbab83b3757d726e45a7740e3e0ef701761
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-23 19:46:07 +0000

    Create InstallSSMTP.sh

---
+++ installscripts/InstallSSMTP.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+######################################################################################################
+# Description: This script will install curl
+# Author: Peter Winter
+# Date: 17/01/2017
+#######################################################################################################
+# License Agreement:
+# This file is part of The Agile Deployment Toolkit.
+# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+#######################################################################################################
+#######################################################################################################
+#set -x
+
+if ( [ "${1}" != "" ] )
+then
+	buildos="${1}"
+fi
+
+apt=""
+if ( [ "`/bin/grep "^PACKAGEMANAGER:*" ${BUILD_HOME}/builddescriptors/buildstyles.dat | /usr/bin/awk -F':' '{print $NF}'`" = "apt" ] )
+then
+	apt="/usr/bin/apt"
+elif ( [ "`/bin/grep "^PACKAGEMANAGER:*" ${BUILD_HOME}/builddescriptors/buildstyles.dat | /usr/bin/awk -F':' '{print $NF}'`" = "apt-get" ] )
+then
+	apt="/usr/bin/apt-get"
+fi
+
+export DEBIAN_FRONTEND=noninteractive 
+install_command="${apt} -o DPkg::Lock::Timeout=-1 -o Dpkg::Use-Pty=0 -qq -y install " 
+
+if ( [ "${apt}" != "" ] )
+then
+	if ( [ "${buildos}" = "ubuntu" ] )
+	then
+			eval ${install_command} ssmtp	
+			eval ${install_command} mailutils	
+	fi
+
+	if ( [ "${buildos}" = "debian" ] )
+	then
+			eval ${install_command} ssmtp	
+			eval ${install_command} mailutils	
+	fi
+fi
