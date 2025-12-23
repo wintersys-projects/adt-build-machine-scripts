3f997caf617d4b1de4c48b76d799aa77a513b45d
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-23 21:23:15 +0000

    Update BuildAuthenticator.sh

--- buildscripts/BuildAuthenticator.sh
+++ buildscripts/BuildAuthenticator.sh
@@ -20,7 +20,7 @@
 # along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
 #########################################################################################
 #########################################################################################
-set -x
+#set -x
 finished=0 #This will be set to 1 if the build is valid
 counter="0" #This counts how many attempted builds there have been
 
