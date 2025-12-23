cf3bd671155d304e9de8f096ccc4a3bde27b074f
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-23 20:06:24 +0000

    Create InstallEmailUtils.sh

---
+++ installscripts/InstallEmailUtils.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+#####################################################################################
+# Author: Peter Winter
+# Date :  9/4/2016
+# Description: Install the email utilities
+#####################################################################################
+# License Agreement:
+# This file is part of The Agile Deployment Toolkit.
+# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+####################################################################################
+####################################################################################
+#set -x
+
+if ( [ "$1" != "" ] )
+then
+	buildos="${1}"
+fi
+
+BUILD_HOME="`/bin/cat /home/buildhome.dat`"
+
+if ( [ "`/bin/grep "^DATASTORETOOL:*" ${BUILD_HOME}/builddescriptors/buildstyles.dat | /bin/grep s3cmd`" != "" ] )
+then
+        ${HOME}/installscripts/InstallSendEmail.sh ${BUILDOS}
+fi
+
+if ( [ "`/bin/grep "^DATASTORETOOL:*" ${BUILD_HOME}/builddescriptors/buildstyles.dat | /bin/grep s3cmd`" != "" ] )
+then
+        ${HOME}/installscripts/InstallSSMTP.sh ${BUILDOS}
+fi
