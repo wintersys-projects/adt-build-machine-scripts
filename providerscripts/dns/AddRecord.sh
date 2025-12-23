3edd7f71d9dc266077c6ca507018db7636f6b28b
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-23 21:34:33 +0000

    Update AddRecord.sh

--- providerscripts/dns/AddRecord.sh
+++ providerscripts/dns/AddRecord.sh
@@ -20,12 +20,6 @@
 ################################################################################
 #set -x
 
-status () {
-        /bin/echo "${1}" | /usr/bin/tee /dev/fd/3 2>/dev/null
-        script_name="`/bin/echo ${0} | /usr/bin/awk -F'/' '{print $NF}'`"
-        /bin/echo "${script_name}: ${1}" | /usr/bin/tee -a /dev/fd/4 2>/dev/null
-}
-
 zoneid="${1}"
 email="${2}"
 credentials="${3}"
