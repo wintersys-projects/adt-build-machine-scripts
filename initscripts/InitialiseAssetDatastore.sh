dccae6d1ab30644c329f0363c1768a8d5b46a308
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-23 21:29:34 +0000

    Update InitialiseAssetDatastore.sh

--- initscripts/InitialiseAssetDatastore.sh
+++ initscripts/InitialiseAssetDatastore.sh
@@ -22,12 +22,6 @@
 ####################################################################################
 #set -x
 
-status () {
-        /bin/echo "${1}" | /usr/bin/tee /dev/fd/3 2>/dev/null
-        script_name="`/bin/echo ${0} | /usr/bin/awk -F'/' '{print $NF}'`"
-        /bin/echo "${script_name}: ${1}" | /usr/bin/tee -a /dev/fd/4 2>/dev/null
-}
-
 BUILD_HOME="`/bin/cat /home/buildhome.dat`"
 DIRECTORIES_TO_MOUNT="`${BUILD_HOME}/helperscripts/GetVariableValue.sh DIRECTORIES_TO_MOUNT | /bin/sed 's/:/ /g'`"
 PERSIST_ASSETS_TO_DATASTORE="`${BUILD_HOME}/helperscripts/GetVariableValue.sh PERSIST_ASSETS_TO_DATASTORE`"
