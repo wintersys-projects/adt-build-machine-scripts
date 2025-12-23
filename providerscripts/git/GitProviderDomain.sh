db3e71be862075da82113809b9b487c669b65bff
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-23 21:35:58 +0000

    Update GitProviderDomain.sh

--- providerscripts/git/GitProviderDomain.sh
+++ providerscripts/git/GitProviderDomain.sh
@@ -20,13 +20,6 @@
 #####################################################################################
 #set -x
 
-
-status () {
-	/bin/echo "${1}" | /usr/bin/tee /dev/fd/3 2>/dev/null
-	script_name="`/bin/echo ${0} | /usr/bin/awk -F'/' '{print $NF}'`"
-	/bin/echo "${script_name}: ${1}" | /usr/bin/tee -a /dev/fd/4 2>/dev/null
-}
-
 infrastructure_repository_provider="${1}"
 
 if ( [ "${infrastructure_repository_provider}" = "github" ] )
