2ec2046b4593a2662bf6c5e9f1889144e08c353e
Winty <160187405+wintersys-projects@users.noreply.github.com>
2025-12-26 23:14:40 +0000

    Create VirusScan.sh

---
+++ helperscripts/VirusScan.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+######################################################################################
+# Author : Peter Winter
+# Date   : 11/08/2021
+# Description: This script will scan your filesystem for viruses 
+#######################################################################################
+# License Agreement:
+# This file is part of The Agile Deployment Toolkit.
+# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+########################################################################################
+########################################################################################
+#set -x
+
+/usr/bin/freshclam
+
+if ( [ "$?" != "0" ] )
+then
+  ${BUILD_HOME}/providerscripts/email/SendEmail.sh "TROUBLE UPDATING VIRUS DATABASE" "Failed to update the virus database successfully" "ERROR"
+fi
+
+if ( [ ! -d ${BUILD_HOME}/runtimedata/virus_report ] )
+then
+  /bin/mkdir -p ${BUILD_HOME}/runtimedata/virus_report
+fi
+
+/usr/bin/clamscan --max-filesize=2000M --max-scansize=2000M --recursive=yes --infected / > ${BUILD_HOME}/runtimedata/virus_report/latest.log  2>/dev/null
+
+if ( [ ! -f ${BUILD_HOME}/runtimedata/virus_report/latest.log ] || [ "`/usr/bin/find ${BUILD_HOME}/runtimedata/virus_report/virus_report/latest.log -cmin -5`" = "" ] )
+then
+        ${BUILD_HOME}/providerscripts/email/SendEmail.sh "TROUBLE PERFORMING PRODUCING VIRUS SCAN REPORT" "Failed to perform virus scan correctly" "ERROR"
+else
+  message="`/bin/cat ${BUILD_HOME}/runtimedata/virus_report/latest.log  | /bin/grep "Infected files"`"
+  machine="`/usr/bin/hostname`"
+  message="\n\n`/bin/cat ${BUILD_HOME}/runtimedata/virus_report/latest.log on machine ${machine}`"
+
+  ${BUILD_HOME}/providerscripts/email/SendEmail.sh "VIRUS SCAN REPORT FOR ${machine}" "${message}" "MANDATORY"
+fi
