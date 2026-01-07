f32a089102ce64ac0448167982b66abf9b16cde4
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-01-07 23:52:42 +0000

    Update PutToConfigDatastore.sh

--- providerscripts/datastore/configwrapper/PutToConfigDatastore.sh
+++ providerscripts/datastore/configwrapper/PutToConfigDatastore.sh
@@ -62,8 +62,8 @@ then
 elif ( [ "${datastore_tool}" = "/usr/bin/s5cmd" ] )
 then
         host_base="`/bin/grep ^host_base /root/.s5cfg-1 | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`" 
-        datastore_cmd="${datastore_tool} --credentials-file /root/.s5cfg-1 --endpoint-url https://${host_base} cp  --metadata 'CreationDate=`/usr/bin/date`'"
-        bucket_prefix="s3://"
+        now="`/usr/bin/date +'%Y-%m-%dT%H:%M:%S'`"
+        datastore_cmd="${datastore_tool} --credentials-file /root/.s5cfg-1 --endpoint-url https://${host_base} cp --metadata 'CreationDate=${now}'"        bucket_prefix="s3://"
         if ( [ "${place_to_put}" = "" ] )
         then
                 slasher=""
