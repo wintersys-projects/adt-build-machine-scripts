082395165c69fd92907912406cf904c55c6d618a
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-02-02 17:01:00 +0000

    Update PerformPutToDatastore.sh

--- providerscripts/datastore/operations/PerformPutToDatastore.sh
+++ providerscripts/datastore/operations/PerformPutToDatastore.sh
@@ -33,17 +33,18 @@ then
         place_to_put=""
 fi
 
-datastore_tool=""
 datastore_cmd=""
 datastore_cmd1=""
 
-if ( [ "`${HOME}/utilities/config/CheckBuildStyle.sh 'DATASTORETOOL:s3cmd'`" = "1" ] )
+datastore_tool=""
+
+if ( [ "`/bin/grep "^DATASTORETOOL:*" ${BUILD_HOME}/builddescriptors/buildstyles.dat | /bin/grep s3cmd`" != "" ] )
 then
         datastore_tool="/usr/bin/s3cmd"
-elif ( [ "`${HOME}/utilities/config/CheckBuildStyle.sh 'DATASTORETOOL:s5cmd'`" = "1" ]  )
+elif ( [ "`/bin/grep "^DATASTORETOOL:*" ${BUILD_HOME}/builddescriptors/buildstyles.dat | /bin/grep s5cmd`" != "" ] )
 then
         datastore_tool="/usr/bin/s5cmd"
-elif ( [ "`${HOME}/utilities/config/CheckBuildStyle.sh 'DATASTORETOOL:rclone'`" = "1" ]  )
+elif ( [ "`/bin/grep "^DATASTORETOOL:*" ${BUILD_HOME}/builddescriptors/buildstyles.dat | /bin/grep rclone`" != "" ] )
 then
         datastore_tool="/usr/bin/rclone"
 fi
