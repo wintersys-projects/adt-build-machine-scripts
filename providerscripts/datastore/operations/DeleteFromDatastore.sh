3b3b960a6ff78b1ae101d56197f57155c1bb8372
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-02-02 16:25:50 +0000

    Update DeleteFromDatastore.sh

--- providerscripts/datastore/operations/DeleteFromDatastore.sh
+++ providerscripts/datastore/operations/DeleteFromDatastore.sh
@@ -1,3 +1,27 @@
+#!/bin/sh 
+####################################################################################
+# Author: Peter Winter
+# Date :  9/4/2016
+# Description: Delete a file from a bucket in the datastore. This can be called in 
+# local or distributed mode. Local mode is when your servers are operating in single
+# region mode and distributed mode is what is used if you are operating in multi-region mode. 
+#######################################################################################
+# License Agreement:
+# This file is part of The Agile Deployment Toolkit.
+# The Agile Deployment Toolkit is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# The Agile Deployment Toolkit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with The Agile Deployment Toolkit.  If not, see <http://www.gnu.org/licenses/>.
+######################################################################################
+######################################################################################
+#set -x
+
 bucket_type="${1}"
 file_to_delete="${2}"
 mode="${3}"
