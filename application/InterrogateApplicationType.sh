e65a413d18d1ac3949d7f58ab5382f4592fa2931
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-02-02 21:23:37 +0000

    Update InterrogateApplicationType.sh

--- application/InterrogateApplicationType.sh
+++ application/InterrogateApplicationType.sh
@@ -25,9 +25,9 @@
 #set -x
 
 status () {
-	/bin/echo "${1}" | /usr/bin/tee /dev/fd/3 2>/dev/null
-	script_name="`/bin/echo ${0} | /usr/bin/awk -F'/' '{print $NF}'`"
-	/bin/echo "${script_name}: ${1}" | /usr/bin/tee -a /dev/fd/4 2>/dev/null
+        /bin/echo "${1}" | /usr/bin/tee /dev/fd/3 2>/dev/null
+        script_name="`/bin/echo ${0} | /usr/bin/awk -F'/' '{print $NF}'`"
+        /bin/echo "${script_name}: ${1}" | /usr/bin/tee -a /dev/fd/4 2>/dev/null
 }
 
 BUILD_HOME="`/bin/cat /home/buildhome.dat`"
@@ -46,12 +46,12 @@ interrogation_home="${BUILD_HOME}/interrogation"
 
 if ( [ ! -d ${interrogation_home} ] )
 then
-	/bin/mkdir -p ${interrogation_home}
+        /bin/mkdir -p ${interrogation_home}
 fi
 
 if ( [ -d ${interrogation_home} ] )
 then
-	/bin/rm -r ${interrogation_home}/* 1>/dev/null 2>/dev/null
+        /bin/rm -r ${interrogation_home}/* 1>/dev/null 2>/dev/null
 fi
 
 cd ${interrogation_home}
@@ -61,84 +61,75 @@ girepo=""
 #If we are a basseline check then we expect to be able to find the baseline repository available
 if ( [ "${BUILD_ARCHIVE_CHOICE}" = "baseline" ] )
 then
-	gitrepo="0"
+        gitrepo="0"
 
-	if ( [ "`${BUILD_HOME}/providerscripts/git/GitLSRemote.sh ${APPLICATION_REPOSITORY_PROVIDER} ${APPLICATION_REPOSITORY_USERNAME} ${APPLICATION_REPOSITORY_OWNER} ${APPLICATION_BASELINE_SOURCECODE_REPOSITORY} ${APPLICATION_REPOSITORY_TOKEN} 2>/dev/null`" = "" ] )
-	then
-		status "Sorry, could not find the baseline repository for you application when I was expecting to, will have to exit..."
-		/bin/touch /tmp/END_IT_ALL
-	else
-		status "I have found potentially usable webroot baseline sourcecode in your git repo."
-		gitrepo="1"
-	fi
+        if ( [ "`${BUILD_HOME}/providerscripts/git/GitLSRemote.sh ${APPLICATION_REPOSITORY_PROVIDER} ${APPLICATION_REPOSITORY_USERNAME} ${APPLICATION_REPOSITORY_OWNER} ${APPLICATION_BASELINE_SOURCECODE_REPOSITORY} ${APPLICATION_REPOSITORY_TOKEN} 2>/dev/null`" = "" ] )
+        then
+                status "Sorry, could not find the baseline repository for you application when I was expecting to, will have to exit..."
+                /bin/touch /tmp/END_IT_ALL
+        else
+                status "I have found potentially usable webroot baseline sourcecode in your git repo."
+                gitrepo="1"
+        fi
 
-	if ( [ "`${BUILD_HOME}/providerscripts/git/GitLSRemote.sh ${APPLICATION_REPOSITORY_PROVIDER} ${APPLICATION_REPOSITORY_USERNAME} ${APPLICATION_REPOSITORY_OWNER} ${BASELINE_DB_REPOSITORY} ${APPLICATION_REPOSITORY_TOKEN} 2>/dev/null`" = "" ] )
-	then
-		status "Sorry, could not find the baseline repository for you application when I was expecting to, will have to exit..."
-		/bin/touch /tmp/END_IT_ALL
-	else
-		status "I have found potentially usable database baseline sourcecode in your git repo."
-	fi
- 
-	status "The build assets have been verified suitable for the build to proceed"
+        if ( [ "`${BUILD_HOME}/providerscripts/git/GitLSRemote.sh ${APPLICATION_REPOSITORY_PROVIDER} ${APPLICATION_REPOSITORY_USERNAME} ${APPLICATION_REPOSITORY_OWNER} ${BASELINE_DB_REPOSITORY} ${APPLICATION_REPOSITORY_TOKEN} 2>/dev/null`" = "" ] )
+        then
+                status "Sorry, could not find the baseline repository for you application when I was expecting to, will have to exit..."
+                /bin/touch /tmp/END_IT_ALL
+        else
+                status "I have found potentially usable database baseline sourcecode in your git repo."
+        fi
+
+        status "The build assets have been verified suitable for the build to proceed"
 fi
 
 #If we are a temporal build, check then we expect to be able to find a backup in the datastore
 periodicity=""
 if ( [ "${BUILD_ARCHIVE_CHOICE}" != "baseline" ] && [ "${BUILD_ARCHIVE_CHOICE}" != "virgin" ] )
 then
-	if ( [ "`/bin/echo 'hourly daily weekly monthly bimonthly' | /bin/grep ${BUILD_ARCHIVE_CHOICE}`" != "" ] )
-	then
-		backuparchive="`/bin/echo ${WEBSITE_URL} | /bin/sed 's/\./-/g'`-${BUILD_ARCHIVE_CHOICE}/applicationsourcecode.tar.gz"
-		backupdbarchive="`/bin/echo ${WEBSITE_URL} | /bin/sed 's/\./-/g'`-db-${BUILD_ARCHIVE_CHOICE}/${WEBSITE_NAME}-DB-backup.tar.gz"
-	else
-		status "Your build kit doesn't seem to have a valid periodicity set"
-		/bin/touch /tmp/END_IT_ALL
-	fi
+        datastorebucket="0"
+        datastoredbbucket="0"
+        ${BUILD_HOME}/providerscripts/datastore/operations/GetFromDatastore.sh "backup-web" "applicationsourcecode.tar.gz" "." "${BUILD_ARCHIVE_CHOICE}"
+        ${BUILD_HOME}/providerscripts/datastore/operations/GetFromDatastore.sh "backup-db" "${WEBSITE_NAME}-DB-backup.tar.gz" "." "${BUILD_ARCHIVE_CHOICE}"
+        archive="${interrogation_home}/applicationsourcecode.tar.gz"
+        archivedb="${interrogation_home}/${WEBSITE_NAME}-DB-backup.tar.gz"
 
-	datastorebucket="0"
-	datastoredbbucket="0"
-	${BUILD_HOME}/providerscripts/datastore/operations/GetFromDatastore.sh ${backuparchive}
-	${BUILD_HOME}/providerscripts/datastore/operations/GetFromDatastore.sh ${backupdbarchive}
-	archive="${interrogation_home}/applicationsourcecode.tar.gz"
-	archivedb="${interrogation_home}/${WEBSITE_NAME}-DB-backup.tar.gz"
+        if ( [  -f ${archive} ] )
+        then
+                status "I have found potentially usable backup webroot sourcecode in your datastore. The build can proceed"
+                status ""
+                datastorebucket="1"
+        else
+                status "Did not find candidate sourcecode in your datastore"
+                /bin/touch /tmp/END_IT_ALL
+        fi
 
-	if ( [  -f ${archive} ] )
-	then
-		status "I have found potentially usable backup webroot sourcecode in your datastore. The build can proceed"
-		status ""
-		datastorebucket="1"
-	else
-		status "Did not find candidate sourcecode in your datastore"
-		/bin/touch /tmp/END_IT_ALL
-	fi
-
-	if ( [  -f ${archivedb} ] )
-	then
-		status "I have found potentially usable backup db dump in your datastore. The build can proceed"
-		status ""
-		datastoredbbucket="1"
-	else
-		status "Notice: did not find candidate db sourcecode in your datastore"
-		status "Press <enter> to acknowledge and continue, otherwise, ctrl-c to exit"
-		read x
-	fi
+        if ( [  -f ${archivedb} ] )
+        then
+                status "I have found potentially usable backup db dump in your datastore. The build can proceed"
+                status ""
+                datastoredbbucket="1"
+        else
+                status "Notice: did not find candidate db sourcecode in your datastore"
+                status "Press <enter> to acknowledge and continue, otherwise, ctrl-c to exit"
+                read x
+        fi
 fi
 
 #If we successfully have found the a repository for our baseline then have a go at finding which application it is by baseline
 if ( [ "${gitrepo}" = "1" ] )
 then
-	${BUILD_HOME}/providerscripts/git/GitClone.sh ${APPLICATION_REPOSITORY_PROVIDER} ${APPLICATION_REPOSITORY_USERNAME} ${APPLICATION_REPOSITORY_OWNER} ${APPLICATION_BASELINE_SOURCECODE_REPOSITORY} ${APPLICATION_REPOSITORY_TOKEN}
-	${BUILD_HOME}/providerscripts/git/GitClone.sh ${APPLICATION_REPOSITORY_PROVIDER} ${APPLICATION_REPOSITORY_USERNAME} ${APPLICATION_REPOSITORY_OWNER} ${BASELINE_DB_REPOSITORY} ${APPLICATION_REPOSITORY_TOKEN}
-	${BUILD_HOME}/application/WhichApplicationByGitAndBaseline.sh
+        ${BUILD_HOME}/providerscripts/git/GitClone.sh ${APPLICATION_REPOSITORY_PROVIDER} ${APPLICATION_REPOSITORY_USERNAME} ${APPLICATION_REPOSITORY_OWNER} ${APPLICATION_BASELINE_SOURCECODE_REPOSITORY} ${APPLICATION_REPOSITORY_TOKEN}
+        ${BUILD_HOME}/providerscripts/git/GitClone.sh ${APPLICATION_REPOSITORY_PROVIDER} ${APPLICATION_REPOSITORY_USERNAME} ${APPLICATION_REPOSITORY_OWNER} ${BASELINE_DB_REPOSITORY} ${APPLICATION_REPOSITORY_TOKEN}
+        ${BUILD_HOME}/application/WhichApplicationByGitAndBaseline.sh
 fi
 
 #If we successfully have found the a backup for our temporal backup then have a go at finding which application it is by backup
 if ( [ "${datastorebucket}" = "1" ] )
 then
-	/bin/tar xvfz ${archive} -C ${interrogation_home}
-	/bin/tar xvfz ${archivedb} -C ${interrogation_home}
-	${BUILD_HOME}/application/WhichApplicationByDatastoreAndBackup.sh
+        /bin/tar xvfz ${archive} -C ${interrogation_home}
+        /bin/tar xvfz ${archivedb} -C ${interrogation_home}
+        ${BUILD_HOME}/application/WhichApplicationByDatastoreAndBackup.sh
 fi
 
 cd ${BUILD_HOME}
