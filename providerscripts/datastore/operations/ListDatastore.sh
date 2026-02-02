9208ac5a5df9d5bb8dc251d76962e5d124718603
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-02-02 17:47:15 +0000

    Update ListDatastore.sh

--- providerscripts/datastore/operations/ListDatastore.sh
+++ providerscripts/datastore/operations/ListDatastore.sh
@@ -81,22 +81,22 @@ if ( [ "${datastore_tool}" = "/usr/bin/s3cmd" ] )
 then
         host_base="`/bin/grep ^host_base /root/.s3cfg-1 | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`"
 
-        if ( [ "${file_to_list}" = "" ] )
-        then
+     #   if ( [ "${file_to_list}" = "" ] )
+     #   then
                 datastore_cmd="${datastore_tool} --config=/root/.s3cfg-1 --host=https://${host_base} ls"
-        else
-                datastore_cmd="${datastore_tool} --config=/root/.s3cfg-1 --host=https://${host_base} ls s3://"
-        fi
+     #   else
+     #           datastore_cmd="${datastore_tool} --config=/root/.s3cfg-1 --host=https://${host_base} ls s3://"
+     #   fi
 elif ( [ "${datastore_tool}" = "/usr/bin/s5cmd" ] )
 then
         host_base="`/bin/grep ^host_base /root/.s5cfg-1 | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`"
 
-        if ( [ "${file_to_list}" = "" ] )
-        then
+      #  if ( [ "${file_to_list}" = "" ] )
+      #  then
                 datastore_cmd="${datastore_tool} --credentials-file /root/.s5cfg-1 --endpoint-url https://${host_base} ls"
-        else
-                datastore_cmd="${datastore_tool} --credentials-file /root/.s5cfg-1 --endpoint-url https://${host_base} ls s3://"
-        fi
+      #  else
+      #          datastore_cmd="${datastore_tool} --credentials-file /root/.s5cfg-1 --endpoint-url https://${host_base} ls s3://"
+      #  fi
 elif ( [ "${datastore_tool}" = "/usr/bin/rclone" ] )
 then
         host_base="`/bin/grep ^endpoint /root/.config/rclone/rclone.conf-1 | /usr/bin/awk -F'=' '{print  $NF}' | /bin/sed 's/ //g'`" 
@@ -111,5 +111,5 @@ then
         file_to_list=""
 fi
 
-${datastore_cmd}${active_bucket}  | /usr/bin/awk '{print $NF}' | /bin/sed '/^$/d'
+${datastore_cmd}  | /bin/grep -wo "${active_bucket}"
 
