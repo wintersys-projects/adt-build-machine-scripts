d7faa4ce6e4f655e569f36992568ae387b02069d
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-02-02 18:48:54 +0000

    Update SyncDatastore.sh

--- providerscripts/datastore/operations/SyncDatastore.sh
+++ providerscripts/datastore/operations/SyncDatastore.sh
@@ -64,6 +64,9 @@ then
 elif ( [ "${bucket_type}" = "backup" ] )
 then
         active_bucket="`/bin/echo ${WEBSITE_URL} | /bin/sed 's/\./-/g'`-${additional_specifier}"
+elif ( [ "${bucket_type}" = "auth" ] )
+then
+        active_bucket="authip-adt-allowed-${additional_specifier}"
 fi
 
 no_tokens="`/bin/echo "${S3_ACCESS_KEY}" | /usr/bin/fgrep -o '|' | /usr/bin/wc -l`"
