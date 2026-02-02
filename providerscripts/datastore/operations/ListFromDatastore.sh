96306b0baf5428a5d0a2c256a4ffcae82597b4f8
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-02-02 16:45:42 +0000

    Update ListFromDatastore.sh

--- providerscripts/datastore/operations/ListFromDatastore.sh
+++ providerscripts/datastore/operations/ListFromDatastore.sh
@@ -25,10 +25,11 @@ bucket_type="${1}"
 file_to_list="${2}"
 additional_specifier="${3}"
 
-WEBSITE_URL="`${HOME}/utilities/config/ExtractConfigValue.sh 'WEBSITEURL'`"
-DNS_CHOICE="`${HOME}/utilities/config/ExtractConfigValue.sh 'DNSCHOICE'`"
-SSL_GENERATION_SERVICE="`${HOME}/utilities/config/ExtractConfigValue.sh 'SSLGENERATIONSERVICE'`"
-SERVER_USER="`${HOME}/utilities/config/ExtractConfigValue.sh 'SERVERUSER'`"
+BUILD_HOME="`/bin/cat /home/buildhome.dat`"
+WEBSITE_URL="`${BUILD_HOME}/helperscripts/GetVariableValue.sh WEBSITE_URL`"
+DNS_CHOICE="`${BUILD_HOME}/helperscripts/GetVariableValue.sh DNS_CHOICE`"
+SSL_GENERATION_SERVICE="`${BUILD_HOME}/helperscripts/GetVariableValue.sh SSL_GENERATION_SERVICE`"
+SERVER_USER="`${BUILD_HOME}/helperscripts/GetVariableValue.sh SERVER_USER`"
 TOKEN="`/bin/echo ${SERVER_USER} | /usr/bin/fold -w 4 | /usr/bin/head -n 1 | /usr/bin/tr '[:upper:]' '[:lower:]'`"
 
 if ( [ "${bucket_type}" = "ssl" ] )
