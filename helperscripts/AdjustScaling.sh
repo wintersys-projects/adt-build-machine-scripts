87cd989263d644ae3416b85c3c981205b70ac191
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-02-03 09:10:18 +0000

    Update AdjustScaling.sh

--- helperscripts/AdjustScaling.sh
+++ helperscripts/AdjustScaling.sh
@@ -23,8 +23,8 @@
 
 if ( [ ! -f  ./AdjustScaling.sh ] )
 then
-	/bin/echo "Sorry, this script has to be run from the helperscripts subdirectory"
-	exit
+        /bin/echo "Sorry, this script has to be run from the helperscripts subdirectory"
+        exit
 fi
 
 /bin/echo "Remember, scaling takes 5 minutes to come online after the completion of an initial build. If less than 5 minutes has passed, wait a bit, then adjust scaling"
@@ -43,30 +43,30 @@ fi
 read response
 if ( [ "${response}" = "1" ] )
 then
-	CLOUDHOST="digitalocean"
+        CLOUDHOST="digitalocean"
 elif ( [ "${response}" = "2" ] )
 then
-	CLOUDHOST="exoscale"
+        CLOUDHOST="exoscale"
 elif ( [ "${response}" = "3" ] )
 then
-	CLOUDHOST="linode"
+        CLOUDHOST="linode"
 elif ( [ "${response}" = "4" ] )
 then
-	CLOUDHOST="vultr"
+        CLOUDHOST="vultr"
 else
-	/bin/echo "Unrecognised  cloudhost. Exiting ...."
-	exit
+        /bin/echo "Unrecognised  cloudhost. Exiting ...."
+        exit
 fi
 
 if ( [ "${CLOUDHOST}" != "`/bin/cat ${BUILD_HOME}/runtimedata/ACTIVE_CLOUDHOST`" ] )
 then
-	/bin/echo "Your chosen cloudhost provider is different to your active cloudhost provider on this build machine"
-	/bin/echo "Do you want to set your chosen cloudhost to be the active cloudhost provider (Y|y)"
-	read response
-	if ( [ "${response}" = "Y" ] || [ "${response}" = "y" ] )
-	then
-		/bin/echo "${CLOUDHOST}" > ${BUILD_HOME}/runtimedata/ACTIVE_CLOUDHOST
-	fi
+        /bin/echo "Your chosen cloudhost provider is different to your active cloudhost provider on this build machine"
+        /bin/echo "Do you want to set your chosen cloudhost to be the active cloudhost provider (Y|y)"
+        read response
+        if ( [ "${response}" = "Y" ] || [ "${response}" = "y" ] )
+        then
+                /bin/echo "${CLOUDHOST}" > ${BUILD_HOME}/runtimedata/ACTIVE_CLOUDHOST
+        fi
 fi
 
 /bin/echo "What is the build identifier you want to adjust scaling for?"
@@ -88,12 +88,12 @@ original_scale_value="0"
 
 for value in ${stripped_scaling_profile}
 do
-	original_scale_value="`/usr/bin/expr ${original_scale_value} + ${value}`"
+        original_scale_value="`/usr/bin/expr ${original_scale_value} + ${value}`"
 done
 
 if ( [ "${original_scale_value}" != "" ] )
 then
-	/bin/echo "Scaling value is currently set to ${original_scale_value} webservers"
+        /bin/echo "Scaling value is currently set to ${original_scale_value} webservers"
 fi
 
 /bin/echo "Please enter the number of webservers that you want to scale to"
@@ -101,8 +101,8 @@ read new_scale_value
 
 while ( ! [ "${new_scale_value}" -eq "${new_scale_value}" ] || [ "${new_scale_value}" -lt "2" ] ) 2> /dev/null
 do
-	/bin/echo "Sorry integers 2 or higher only"
-	read new_scale_value
+        /bin/echo "Sorry integers 2 or higher only"
+        read new_scale_value
 done
 
 /bin/echo ""
@@ -112,7 +112,7 @@ read response
 
 if ( [ "${response}" != "Y" ] && [ "${response}" != "y" ] )
 then
-	exit
+        exit
 fi
 
 REGION="`${BUILD_HOME}/helperscripts/GetVariableValue.sh REGION`"
@@ -120,8 +120,8 @@ number_of_autoscalers="`${BUILD_HOME}/providerscripts/server/NumberOfServers.sh 
 
 if ( [ "${number_of_autoscalers}" = "0" ] )
 then
-	/bin/echo "There doesn't seem to be any autoscalers running it's pointless trying to set a scaling value"
-	exit
+        /bin/echo "There doesn't seem to be any autoscalers running it's pointless trying to set a scaling value"
+        exit
 fi
 
 number_of_webservers="${new_scale_value}"
@@ -135,13 +135,13 @@ additional_number_of_webservers="`/usr/bin/expr ${number_of_webservers} - ${tota
 new_scale_values="STATIC_SCALE"
 for autoscaler_no in `printf "%d\n" $(seq 1 ${number_of_autoscalers})`
 do
-	if ( [ "${additional_number_of_webservers}" -gt "0" ] )
-	then
-		new_scale_values="${new_scale_values}:`/usr/bin/expr ${base_number_of_webservers} + 1`"
-		additional_number_of_webservers="`/usr/bin/expr ${additional_number_of_webservers} - 1`"
-	else
-		new_scale_values="${new_scale_values}:${base_number_of_webservers}"
-	fi
+        if ( [ "${additional_number_of_webservers}" -gt "0" ] )
+        then
+                new_scale_values="${new_scale_values}:`/usr/bin/expr ${base_number_of_webservers} + 1`"
+                additional_number_of_webservers="`/usr/bin/expr ${additional_number_of_webservers} - 1`"
+        else
+                new_scale_values="${new_scale_values}:${base_number_of_webservers}"
+        fi
 done
 
 /bin/echo "Deleting existing Scaling Profile from datastore"
@@ -149,25 +149,24 @@ ${BUILD_HOME}/providerscripts/datastore/config/toolkit/DeleteFromConfigDatastore
 
 if ( [ -f ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/STATIC_SCALE:* ] )
 then
-	/bin/rm ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/STATIC_SCALE:*
+        /bin/rm ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/STATIC_SCALE:*
 fi
 
 /bin/touch ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/${new_scale_values}
-${BUILD_HOME}/providerscripts/datastore/config/toolkit/PutToConfigDatastore.sh ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/${new_scale_values} "root" "no"
+${BUILD_HOME}/providerscripts/datastore/operations/PutToDatastore.sh "config" "${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/${new_scale_values}" "root" "local" "no"
 
-
-if ( [ "`${BUILD_HOME}/providerscripts/datastore/config/toolkit/ListFromConfigDatastore.sh ${new_scale_values}`" != "" ] )
+if ( [ "`${BUILD_HOME}/providerscripts/datastore/operations/ListFromDatastore.sh "config" "${new_scale_values}"`" != "" ] )
 then
         /bin/echo "New Scaling Profile is present in the datastore : ${new_scale_values}"
 fi
 
-scaling_profile="`${BUILD_HOME}/providerscripts/datastore/config/toolkit/ListFromConfigDatastore.sh STATIC_SCALE:*`"
+scaling_profile="`${BUILD_HOME}/providerscripts/datastore/operations/ListFromDatastore.sh "config" "STATIC_SCALE:*"`"
 stripped_scaling_profile="`/bin/echo ${scaling_profile} | /bin/sed 's/.*STATIC_SCALE://g' | /bin/sed 's/:/ /g'`"
 total_number_of_webservers="0"
 
 for value in ${stripped_scaling_profile}
 do
-	total_number_of_webservers="`/usr/bin/expr ${total_number_of_webservers} + ${value}`"
+        total_number_of_webservers="`/usr/bin/expr ${total_number_of_webservers} + ${value}`"
 done
 
 /bin/echo ""
