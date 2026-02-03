097102e32ce2c1f1304e6165b24d765f88ca1c2b
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-02-03 10:05:26 +0000

    Update ListDatastore.sh

--- providerscripts/datastore/operations/ListDatastore.sh
+++ providerscripts/datastore/operations/ListDatastore.sh
@@ -59,9 +59,6 @@ then
 elif ( [ "${bucket_type}" = "config-all" ] )
 then
         active_bucket="`/bin/echo "${WEBSITE_URL}"-config | /bin/sed 's/\./-/g'`"
-elif ( [ "${bucket_type}" = "asset" ] )
-then
-        active_bucket="`/bin/echo "${WEBSITE_URL}-assets-${additional_specifier}" | /bin/sed -e 's/\./-/g' -e 's;/;-;g' -e 's/--/-/g'`"
 elif ( [ "${bucket_type}" = "backup" ] )
 then
         active_bucket="`/bin/echo ${WEBSITE_URL} | /bin/sed 's/\./-/g'`-${additional_specifier}"
