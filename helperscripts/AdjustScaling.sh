a8091b06ad80eba9ce091c5715b2a810f1072b4b
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-02-05 11:05:13 +0000

    Update AdjustScaling.sh

--- helperscripts/AdjustScaling.sh
+++ helperscripts/AdjustScaling.sh
@@ -58,6 +58,9 @@ else
         exit
 fi
 
+/bin/echo "Please enter the name of the build of the server you wish to connect with"
+read BUILD_IDENTIFIER
+
 /bin/echo "Please enter the full URL of the website you want to alter the scaling configuration for, for example, www.testwebsite.uk"
 
 read website_url
