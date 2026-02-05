5f6f11ae2d328aedc935ef04d65d5bc56da9a115
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-02-05 16:15:16 +0000

    Update InitialiseScalingProfile.sh

--- initscripts/InitialiseScalingProfile.sh
+++ initscripts/InitialiseScalingProfile.sh
@@ -34,48 +34,43 @@ REGION="`${BUILD_HOME}/helperscripts/GetVariableValue.sh REGION`"
 NO_AUTOSCALERS="`${BUILD_HOME}/helperscripts/GetVariableValue.sh NO_AUTOSCALERS`"
 BUILD_IDENTIFIER="`${BUILD_HOME}/helperscripts/GetVariableValue.sh BUILD_IDENTIFIER`"
 NO_WEBSERVERS="`${BUILD_HOME}/helperscripts/GetVariableValue.sh NO_WEBSERVERS`"
+WEBSERVER_URL="`${BUILD_HOME}/helperscripts/GetVariableValue.sh WEBSERVER_URL`"
 
 if ( [ "${PRODUCTION}" = "1" ] && [ "${DEVELOPMENT}" = "0" ] )
 then
-        if ( [ -f ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/STATIC_SCALE:* ] )
-        then
-                /bin/rm ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/STATIC_SCALE:*
-        fi
+	if ( [ -f ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/STATIC_SCALE:* ] )
+	then
+		/bin/rm ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/STATIC_SCALE:*
+	fi
 
-        status ""
-        status "#############################################################"
-        status "Setting scaling value for number of webservers to ${NO_WEBSERVERS}"
-        status "#############################################################"
-        status ""
+	status ""
+	status "#############################################################"
+	status "Setting scaling value for number of webservers to ${NO_WEBSERVERS}"
+	status "#############################################################"
+	status ""
 
-        ####added
+	${BUILD_HOME}/providerscripts/datastore/operations/MountDatastore.sh "scaling" "local" "scaling-${CLOUDHOST}-${REGION}"
+	${BUILD_HOME}/providerscripts/datastore/operations/DeleteFromDatastore.sh "scaling" "autoscaler-*/STATIC_SCALE:*" "local" "${WEBSITE_URL}-scaling-${CLOUDHOST}-${REGION}"
 
-       # /bin/touch ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/STATIC_SCALE:${NO_WEBSERVERS}
-        ${BUILD_HOME}/providerscripts/datastore/operations/MountDatastore.sh "scaling" "local" "scaling-${CLOUDHOST}-${REGION}"
+	no_autoscaler="1"
+	while ( [ "${no_autoscaler}" -le "${NO_AUTOSCALERS}" ] )
+	do
+		if ( [ ! -d ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/scaling/autoscaler-${no_autoscaler} ] )
+		then
+			/bin/mkdir -p ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/scaling/autoscaler-${no_autoscaler}
+		fi
+		/bin/touch ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/scaling/autoscaler-${no_autoscaler}/STATIC_SCALE:${NO_WEBSERVERS}
+		${BUILD_HOME}/providerscripts/datastore/operations/DeleteFromDatastore.sh "scaling" "*autoscaler-${no_autoscaler}*" "local" "scaling-${CLOUDHOST}-${REGION}"
+		${BUILD_HOME}/providerscripts/datastore/operations/PutToDatastore.sh "scaling" "${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/scaling/autoscaler-${no_autoscaler}/STATIC_SCALE:${NO_WEBSERVERS}" "autoscaler-${no_autoscaler}" "local" "no" "scaling-${CLOUDHOST}-${REGION}"
+		no_autoscaler="`/usr/bin/expr ${no_autoscaler} + 1`"
+	done
 
-        no_autoscaler="1"
-        while ( [ "${no_autoscaler}" -le "${NO_AUTOSCALERS}" ] )
-        do
-                if ( [ ! -d ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/scaling/autoscaler-${no_autoscaler} ] )
-                then
-                        /bin/mkdir -p ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/scaling/autoscaler-${no_autoscaler}
-                fi
-                /bin/touch ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/scaling/autoscaler-${no_autoscaler}/STATIC_SCALE:${NO_WEBSERVERS}
-                ${BUILD_HOME}/providerscripts/datastore/operations/DeleteFromDatastore.sh "scaling" "*autoscaler-${no_autoscaler}*" "local" "scaling-${CLOUDHOST}-${REGION}"
-                ${BUILD_HOME}/providerscripts/datastore/operations/PutToDatastore.sh "scaling" "${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/scaling/autoscaler-${no_autoscaler}/STATIC_SCALE:${NO_WEBSERVERS}" "autoscaler-${no_autoscaler}" "local" "no" "scaling-${CLOUDHOST}-${REGION}"
-                no_autoscaler="`/usr/bin/expr ${no_autoscaler} + 1`"
-        done
-        ####added
-
-     #   /bin/touch ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/STATIC_SCALE:${NO_WEBSERVERS}
-     #   ${BUILD_HOME}/providerscripts/datastore/operations/PutToDatastore.sh "config" "${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/STATIC_SCALE:${NO_WEBSERVERS}" "root" "local" "no"
-
-        if ( [ "`${BUILD_HOME}/helperscripts/IsHardcoreBuild.sh`" != "1" ] )
-        then
-                status ""
-                status "Press <enter> to accept these values"
-                status ""
-                read x
-        fi
+	if ( [ "`${BUILD_HOME}/helperscripts/IsHardcoreBuild.sh`" != "1" ] )
+	then
+		status ""
+		status "Press <enter> to accept these values"
+		status ""
+		read x
+	fi
 fi
 
