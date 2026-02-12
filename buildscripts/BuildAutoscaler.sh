417f4e0c98d372b2e56b94b4da1301f2d892f411
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-02-12 18:31:37 +0000

    Update BuildAutoscaler.sh

--- buildscripts/BuildAutoscaler.sh
+++ buildscripts/BuildAutoscaler.sh
@@ -146,11 +146,11 @@ do
 		then
 			if ( [ "`${BUILD_HOME}/providerscripts/datastore/operations/ListFromDatastore.sh "config" "autoscalerpublicips/*"`" != "" ] )
 			then
-				${BUILD_HOME}/providerscripts/datastore/operations/DeleteFromDatastore.sh "config" "autoscalerpublicips/*"
+				${BUILD_HOME}/providerscripts/datastore/operations/DeleteFromDatastore.sh "config" "autoscalerpublicips" "local"
 			fi
 			if ( [ "`${BUILD_HOME}/providerscripts/datastore/operations/ListFromDatastore.sh "config" "autoscalerips/*"`" != "" ] )
 			then
-				${BUILD_HOME}/providerscripts/datastore/operations/DeleteFromDatastore.sh "cofig" "autoscalerips/*"
+				${BUILD_HOME}/providerscripts/datastore/operations/DeleteFromDatastore.sh "config" "autoscalerips" "local"
 			fi
 		fi
 		${BUILD_HOME}/providerscripts/datastore/operations/PutToDatastore.sh "config" "${ip}" "autoscalerpublicips" "local" "no"
@@ -217,8 +217,8 @@ do
 			fi
 
 			#Delete the autoscaler IP addresses from the S3 datastore because they were clearly not needed because of failure
-			${BUILD_HOME}/providerscripts/datastore/operations/DeleteFromDatastore.sh "config" "autoscalerpublicip" "root" "local"
-			${BUILD_HOME}/providerscripts/datastore/operations/DeleteFromDatastore.sh "config" "autoscalerip" "root" "local"
+			${BUILD_HOME}/providerscripts/datastore/operations/DeleteFromDatastore.sh "config" "autoscalerpublicips" "local"
+			${BUILD_HOME}/providerscripts/datastore/operations/DeleteFromDatastore.sh "config" "autoscalerips" "local"
 			${BUILD_HOME}/providerscripts/server/DestroyServer.sh ${ip} ${CLOUDHOST}
 
 			#Wait until we are sure that the autoscaler server(s) are destroyed because of a faulty build
