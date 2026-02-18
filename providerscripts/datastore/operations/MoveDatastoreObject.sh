1bbe5aa6ad27dbe7b801987245631518abfb2dae
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-02-18 08:34:43 +0000

    Update MoveDatastoreObject.sh

--- providerscripts/datastore/operations/MoveDatastoreObject.sh
+++ providerscripts/datastore/operations/MoveDatastoreObject.sh
@@ -59,6 +59,10 @@ then
 elif ( [ "${bucket_type}" = "config" ] )
 then
         active_bucket="`/bin/echo "${WEBSITE_URL}"-config | /bin/sed 's/\./-/g'`-${TOKEN}"
+elif ( [ "${bucket_type}" = "auth-config" ] )
+then
+        WEBSITE_URL="`${BUILD_HOME}/helperscripts/GetVariableValue.sh AUTH_SERVER_URL`"
+        active_bucket="`/bin/echo "${WEBSITE_URL}"-config | /bin/sed 's/\./-/g'`-${additional_specifier}"  
 elif ( [ "${bucket_type}" = "asset" ] )
 then
         active_bucket="`/bin/echo "${WEBSITE_URL}-assets-${additional_specifier}" | /bin/sed -e 's/\./-/g' -e 's;/;-;g' -e 's/--/-/g'`"
