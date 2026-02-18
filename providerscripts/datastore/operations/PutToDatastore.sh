1c8883d535a48c4be15d180c965fd2d57445b56c
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-02-18 08:36:36 +0000

    Fix active_bucket assignment for auth-config type

--- providerscripts/datastore/operations/PutToDatastore.sh
+++ providerscripts/datastore/operations/PutToDatastore.sh
@@ -81,6 +81,7 @@ then
 elif ( [ "${bucket_type}" = "auth-config" ] )
 then
         WEBSITE_URL="`${BUILD_HOME}/helperscripts/GetVariableValue.sh AUTH_SERVER_URL`"
+        active_bucket="`/bin/echo "${WEBSITE_URL}"-config | /bin/sed 's/\./-/g'`-${TOKEN}"
 elif ( [ "${bucket_type}" = "asset" ] )
 then
         active_bucket="`/bin/echo "${WEBSITE_URL}-assets-${additional_specifier}" | /bin/sed -e 's/\./-/g' -e 's;/;-;g' -e 's/--/-/g'`"
