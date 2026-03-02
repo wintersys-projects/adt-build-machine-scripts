febfa632d389c6e90bc9b0ce2d9890e327cfd2a2
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-03-02 19:29:02 +0000

    Update DeleteDatastore.sh

--- providerscripts/datastore/operations/DeleteDatastore.sh
+++ providerscripts/datastore/operations/DeleteDatastore.sh
@@ -87,6 +87,12 @@ elif ( [ "${bucket_type}" = "dbaas" ] )
 then
         active_bucket="`/bin/echo ${WEBSITE_URL} | /bin/sed 's/\./-/g'`"
         active_bucket="${active_bucket}-${DNS_CHOICE}-dbaas"
+elif ( [ "${bucket_type}" = "firewall-auth-laptop-ips" ] )
+then
+        active_bucket="`/bin/echo ${WEBSITE_URL} | /bin/sed 's/\./-/g'`-firewall-auth-laptop-ips"
+elif ( [ "${bucket_type}" = "basic-auth-credentials" ] )
+then
+        active_bucket="`/bin/echo ${WEBSITE_URL} | /bin/sed 's/\./-/g'`-basic-auth-credentials"
 elif ( [ "${bucket_type}" = "snap" ] )
 then
         active_bucket="`/bin/echo ${WEBSITE_URL} | /bin/sed 's/\./-/g'`"
