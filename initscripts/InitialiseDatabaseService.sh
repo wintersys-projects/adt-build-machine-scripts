b7c680ed204b7e7ea36247a77938b1eff6b40df0
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-03-08 13:05:03 +0000

    Update InitialiseDatabaseService.sh

--- initscripts/InitialiseDatabaseService.sh
+++ initscripts/InitialiseDatabaseService.sh
@@ -593,7 +593,7 @@ fi
 
 if ( [ "${MULTI_REGION}" = "1" ] && [ "${PRIMARY_REGION}" = "0" ] ) 
 then
-        #multi_region_bucket="`/bin/echo "${WEBSITE_URL}" | /bin/sed 's/\./-/g'`-multi-region"
+        ${BUILD_HOME}/providerscripts/datastore/operations/MountDatastore.sh "multi-region" "distributed"
         if ( [ "`${BUILD_HOME}/providerscripts/datastore/operations/ListFromDatastore.sh "multi-region" "credentials.dat"`" != "" ] )
         then
                 ${BUILD_HOME}/providerscripts/datastore/operations/GetFromDatastore.sh "multi-region" "credentials.dat" "${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}"
