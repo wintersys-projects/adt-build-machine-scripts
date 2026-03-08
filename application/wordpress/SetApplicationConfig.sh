10a3abdc6a10480b2ae20d5b98f921d0f4977fec
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-03-08 21:07:11 +0000

    Update SetApplicationConfig.sh

--- application/wordpress/SetApplicationConfig.sh
+++ application/wordpress/SetApplicationConfig.sh
@@ -28,6 +28,55 @@
 #WEBSITE_TITLE:XXXXWEBSITE_TITLEXXXX
 
 
+if ( [ ! -d  ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/application ] )
+then
+        /bin/mkdir -p ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/application
+fi
+
+/bin/cp ${BUILD_HOME}/application/descriptors/${APPLICATION}.dat ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/application/${APPLICATION}.dat
+
+if ( [ "${DATABASE_INSTALLATION_TYPE}" = "Postgres" ] || ( [ "${DATABASE_INSTALLATION_TYPE}" = "DBaaS" ] && [ "`/bin/echo ${DATABASE_DBaaS_INSTALLATION_TYPE} | /bin/grep 'Postgres'`" != "" ] ) )
+then
+        DB_TYPE="pgsql"
+else
+        DB_TYPE="mysqli"
+fi
+
+if ( [ "${SYSTEM_EMAIL_PROVIDER}" = "1" ] )
+then
+        smtp_port="2525"
+        smtp_host="smtp-pulse.com"
+elif ( [ "${SYSTEM_EMAIL_PROVIDER}" = "2" ] )
+then
+        smtp_port="587"
+        smtp_host="in.mailjet.com"
+elif ( [ "${SYSTEM_EMAIL_PROVIDER}" = "3" ] )
+then
+        smtp_port="2525"
+        smtp_host="email-smtp.eu-west-1.amazonaws.com"
+fi
+
+
+
+SYSTEM_FROMEMAIL_ADDRESS="`${BUILD_HOME}/helperscripts/GetVariableValue.sh 'SYSTEM_FROMEMAIL_ADDRESS' | /bin/sed 's/_/ /g'`"
+SYSTEM_TOEMAIL_ADDRESS="`${BUILD_HOME}/helperscripts/GetVariableValue.sh 'SYSTEM_TOEMAIL_ADDRESS' | /bin/sed 's/_/ /g'`"
+WEBSITE_DISPLAY_NAME="`${BUILD_HOME}/helperscripts/GetVariableValue.sh 'WEBSITE_DISPLAY_NAME'` Webmaster"
+SYSTEM_EMAIL_USERNAME="`${BUILD_HOME}/helperscripts/GetVariableValue.sh 'SYSTEM_EMAIL_USERNAME'`"
+SYSTEM_EMAIL_PASSWORD="`${BUILD_HOME}/helperscripts/GetVariableValue.sh 'SYSTEM_EMAIL_PASSWORD'`"
+
+#APPLICATION_BASELINE_SOURCECODE_REPOSITORY="`${BUILD_HOME}/helperscripts/GetVariableValue.sh 'APPLICATION_BASELINE_SOURCECODE_REPOSITORY'`"
+#joomla_version="`/bin/echo "${APPLICATION_BASELINE_SOURCECODE_REPOSITORY}" | /usr/bin/awk -F':' '{print $NF}'`"
+
+#/bin/sed -i "s/XXXXAPPLICATION_VERSIONXXXX/${joomla_version}/g" ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/application/${APPLICATION}.dat
+/bin/sed -i "s/XXXXAPPLICATION_USERNAMEXXXX/${DB_USERNAME}/" ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/application/${APPLICATION}.dat
+/bin/sed -i "s/XXXXAPPLICATION_PASSWORDXXXX/${DB_PASSWORD}/" ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/application/${APPLICATION}.dat
+/bin/sed -i "s/XXXXAPPLICATION_DATABASEXXXX/${DB_NAME}/" ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/application/${APPLICATION}.dat
+/bin/sed -i "s/XXXXAPPLICATION_DB_HOSTXXXX/${DB_IDENTIFIER}/" ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/application/${APPLICATION}.dat
+/bin/sed -i "s/XXXXAPPLICATION_DB_PORTXXXX/${DB_PORT}/" ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/application/${APPLICATION}.dat
+/bin/sed -i "s/XXXXAPPLICATION_DB_TYPEXXXX/${DB_TYPE}/" ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/application/${APPLICATION}.dat
+
+
+
 
 
 
