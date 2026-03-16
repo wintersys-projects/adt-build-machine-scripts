69348295dc1a1e64cdc5622ad2a0cafb05892cd5
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-03-16 09:20:29 +0000

    Update WebserverCloudInit.sh

--- initscripts/cloud-init/WebserverCloudInit.sh
+++ initscripts/cloud-init/WebserverCloudInit.sh
@@ -91,25 +91,25 @@ fi
 APPLICATION_LANGUAGE="`${BUILD_HOME}/helperscripts/GetVariableValue.sh APPLICATION_LANGUAGE`"
 PHP_VERSION="`${BUILD_HOME}/helperscripts/GetVariableValue.sh PHP_VERSION`"
 
-if ( [ "${APPLICATION_LANGUAGE}" = "PHP" ] )
-then
-	if ( [ "${BUILDOS}" = "ubuntu" ] )
-	then
-		/bin/sed  -i 's/#XXXXPHPUBUNTUXXXX//g' ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/cloud-init/webserver.yaml
-	elif ( [ "${BUILDOS}" = "debian" ] )
-	then
-		/bin/sed  -i 's/#XXXXPHPDEBIANXXXX//g' ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/cloud-init/webserver.yaml
-	fi
-
-	/bin/sed  -i "s/XXXXPHP_VERSIONXXXX/${PHP_VERSION}/g" ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/cloud-init/webserver.yaml
-
-	php_modules="`/bin/grep ^PHP ${BUILD_HOME}/builddescriptors/buildstyles.dat | /bin/sed 's/^PHP:cloud-init://g' | /usr/bin/awk -F'|' '{print $1}' | /bin/sed 's/:/ /g'`"
-	php_module_list=""
-	for php_module in ${php_modules}
-	do
-		php_modules_list="${php_modules_list} php${PHP_VERSION}-${php_module}"
-	done
-fi
+#if ( [ "${APPLICATION_LANGUAGE}" = "PHP" ] )
+#then
+#	if ( [ "${BUILDOS}" = "ubuntu" ] )
+#	then
+#		/bin/sed  -i 's/#XXXXPHPUBUNTUXXXX//g' ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/cloud-init/webserver.yaml
+#	elif ( [ "${BUILDOS}" = "debian" ] )
+#	then
+#		/bin/sed  -i 's/#XXXXPHPDEBIANXXXX//g' ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/cloud-init/webserver.yaml
+#	fi
+#
+#	/bin/sed  -i "s/XXXXPHP_VERSIONXXXX/${PHP_VERSION}/g" ${BUILD_HOME}/runtimedata/${CLOUDHOST}/${BUILD_IDENTIFIER}/cloud-init/webserver.yaml
+#
+#	php_modules="`/bin/grep ^PHP ${BUILD_HOME}/builddescriptors/buildstyles.dat | /bin/sed 's/^PHP:cloud-init://g' | /usr/bin/awk -F'|' '{print $1}' | /bin/sed 's/:/ /g'`"
+#	php_module_list=""
+#	for php_module in ${php_modules}
+#	do
+#		php_modules_list="${php_modules_list} php${PHP_VERSION}-${php_module}"
+#	done
+#fi
 
 #the way this works is that installation method for each webserver type is commented out with a placeholder token
 #to enable the installastion of the webserver type that you are installing the blocking placeholder is commented out/removed
