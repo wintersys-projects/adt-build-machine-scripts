c087613eec05ba664aa35830a1fac070be62fc64
Winty <160187405+wintersys-projects@users.noreply.github.com>
2026-03-22 19:29:21 +0000

    Update php-debian-installer.sh

--- installscripts/php-debian-installer.sh
+++ installscripts/php-debian-installer.sh
@@ -15,8 +15,6 @@ ${SUDO} /usr/bin/apt-get update
 #its possible we might fail and die, I have seen it happen due to networking glitches therefore php deserves a second chance just like everyone else
 while ( [ -f /usr/bin/php ] )
 do
-    ${SUDO} /usr/bin/os-prober
-    ${SUDO} /usr/sbin/update-grub
     ${SUDO} /usr/bin/apt-get update
     ${SUDO} /usr/bin/apt-get -y install lsb-release ca-certificates curl
     ${SUDO} /usr/bin/curl -sSLo /tmp/debsuryorg-archive-keyring.deb https://packages.sury.org/debsuryorg-archive-keyring.deb
